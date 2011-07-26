55b725d6e6259ea3945243c076ebc6275979bff2
Feng Yang <fyang@redhat.com>
2011-07-26 13:00:33 +0000

    KVM Test: Make virt_test_utils could run tests script under ../virt/tests
    Signed-off-by: Feng Yang <fyang@redhat.com>
    Acked-by: Qingtang Zhou <qzhou@redhat.com>
    Acked-by: Yiqiao Pu <ypu@redhat.com>

--- virt_test_utils.py
+++ virt_test_utils.py
@@ -1341,7 +1341,20 @@ def run_sub_test(test, params, env, sub_type=None):
     """
     if sub_type is None:
         raise error.TestError("No sub test is found")
-    subtest_dir = os.path.join(test.bindir, "tests")
+    virt_dir = os.path.dirname(virt_utils.__file__)
+    subtest_dir_virt = os.path.join(virt_dir, "tests")
+    subtest_dir_kvm = os.path.join(test.bindir, "tests")
+    subtest_dir = None
+    for d in [subtest_dir_kvm, subtest_dir_virt]:
+        module_path = os.path.join(d, "%s.py" % sub_type)
+        if os.path.isfile(module_path):
+            subtest_dir = d
+            break
+    if subtest_dir is None:
+        raise error.TestError("Could not find test file %s.py "
+                              "on either %s or %s directory" % (sub_type,
+                              subtest_dir_kvm, subtest_dir_virt))
+
     f, p, d = imp.find_module(sub_type, [subtest_dir])
     test_module = imp.load_module(sub_type, f, p, d)
     f.close()
