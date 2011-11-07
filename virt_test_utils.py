fbb9a873ffb1104057ec45f069a23e9391d248ac
Qingtang Zhou <qzhou@redhat.com>
2011-11-07 19:59:34 +0800

    KVM Test: virt_test_utils.py: Get guest status with verify_status method of monitor
    Autotest gets guest's status by checking whether 'paused' string is in the output of
    monitor cmd 'info status', but this way doesn't work well when we use QMPMonitor.
    This patch will use monitor.verify_status() method instead.
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- virt_test_utils.py
+++ virt_test_utils.py
@@ -327,7 +327,7 @@ def migrate(vm, env=None, mig_timeout=3600, mig_protocol="tcp",
         raise error.TestFail("Migration end with stauts: %s" % status)
 
     if dest_host == 'localhost':
-        if "paused" in dest_vm.monitor.info("status"):
+        if dest_vm.monitor.verify_status("paused"):
             logging.debug("Destination VM is paused, resuming it")
             dest_vm.monitor.cmd("cont")
 
