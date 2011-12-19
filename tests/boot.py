e154ad4949236b56450ed2796dfb840952863370
Qingtang Zhou <qzhou@redhat.com>
2011-12-19 10:02:07 +0000

    Virt Test: boot.py: Verify usb deivce after boot
    This request adds some verification steps in
    boot_with_usb and reboot_with_usb case, to make
    sure the usb devices work well in guest.
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>
    (cherry picked from commit 0ed12d25ceffea62dab5cedbc9794821d41c0d97)
    
    Conflicts:
    
    	client/tests/kvm/subtests.cfg.sample
    	client/virt/tests/boot.py

--- tests/boot.py
+++ tests/boot.py
@@ -91,10 +91,15 @@ def run_boot(test, params, env):
         error.context("Reboot guest.")
         if params["reboot_method"] == "system_reset":
             time.sleep(int(params.get("sleep_before_reset", 10)))
-        session = vm.reboot(session, params["reboot_method"], 0, timeout)
+        try:
+            # Reboot the VM
+            for i in range(int(params.get("reboot_count", 1))):
+                session = vm.reboot(session, params["reboot_method"], 0,
+                                                                timeout)
+        finally:
+            session.close()
+
 
         if check_func:
             error.context("Verify device(s) after rebooting.")
             _check_device(check_func)
-
-    session.close()
