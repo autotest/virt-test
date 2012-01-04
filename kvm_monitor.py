autotest/autotest

virt: kvm_monitor.py: Implement set_link() in Monitor
humanmonitor: set_link idKIXPDK on/off
qmpmonitor: {'execute': 'set_link', 'arguments': {'name': 'idKIXPDK',
             'up': False}, 'id': 'nlqdqdq4'}

Update:
 - Get on/off parameter string from monitor output in Human monitor.


Signed-off-by: Amos Kong <akong@redhat.com>
Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

client/virt/kvm_monitor.py
@@ -391,6 +391,32 @@ class HumanMonitor(Monitor):
         return self.cmd(command="screendump %s" % filename, debug=debug)
 
 
+    def set_link(self, name, up):
+        """
+        Set link up/down.
+
+        @param name: Link name
+        @param up: Bool value, True=set up this link, False=Set down this link
+        @return: The response to the command
+        """
+        set_link_cmd = "set_link"
+
+        # set_link in RHEL5 host use "up|down" instead of "on|off" which is
+        # used in RHEL6 host and Fedora host. So here find out the string
+        # this monitor accept.
+        o = self.cmd("help %s" % set_link_cmd)
+        try:
+            on_str, off_str = re.findall("(\w+)\|(\w+)", o)[0]
+        except Exception:
+            # take a default value if can't get on/off string from monitor.
+            on_str, off_str = "on", "off"
+
+        status = off_str
+        if up:
+            status = on_str
+        return self.cmd("%s %s %s" % (set_link_cmd, name, status))
+
+
     def migrate(self, uri, full_copy=False, incremental_copy=False, wait=False):
         """
         Migrate.
@@ -929,3 +955,15 @@ class QMPMonitor(Monitor):
         """
         args = {"value": value}
         return self.cmd("migrate_set_speed", args)
+
+
+    def set_link(self, name, up):
+        """
+        Set link up/down.
+
+        @param name: Link name
+        @param up: Bool value, True=set up this link, False=Set down this link
+
+        @return: The response to the command
+        """
+        return self.send_args_cmd("set_link name=%s,up=%s" % (name, str(up)))

autotest/virt-test

kvm_monitor.py
@@ -394,6 +394,32 @@ class HumanMonitor(Monitor):
         return self.cmd(command="screendump %s" % filename, debug=debug)
 
 
+    def set_link(self, name, up):
+        """
+        Set link up/down.
+
+        @param name: Link name
+        @param up: Bool value, True=set up this link, False=Set down this link
+        @return: The response to the command
+        """
+        set_link_cmd = "set_link"
+
+        # set_link in RHEL5 host use "up|down" instead of "on|off" which is
+        # used in RHEL6 host and Fedora host. So here find out the string
+        # this monitor accept.
+        o = self.cmd("help %s" % set_link_cmd)
+        try:
+            on_str, off_str = re.findall("(\w+)\|(\w+)", o)[0]
+        except IndexError:
+            # take a default value if can't get on/off string from monitor.
+            on_str, off_str = "on", "off"
+
+        status = off_str
+        if up:
+            status = on_str
+        return self.cmd("%s %s %s" % (set_link_cmd, name, status))
+
+
     def migrate(self, uri, full_copy=False, incremental_copy=False, wait=False):
         """
         Migrate.
@@ -942,3 +968,15 @@ class QMPMonitor(Monitor):
         """
         args = {"value": value}
         return self.cmd("migrate_set_speed", args)
+
+
+    def set_link(self, name, up):
+        """
+        Set link up/down.
+
+        @param name: Link name
+        @param up: Bool value, True=set up this link, False=Set down this link
+
+        @return: The response to the command
+        """
+        return self.send_args_cmd("set_link name=%s,up=%s" % (name, str(up)))