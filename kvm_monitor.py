3144b822267f8dea1ed2315bcef78f70cb63ea56
Feng Yang <fyang@redhat.com>
2012-01-18 13:21:04 +0000

    virt: kvm_monitor.py: Convent float number string to float type
    send_arg_cmds can convent some types of string to python type.
    This patch makes it convent float number string to float type.
    
    Changes from V1:
     - Update regex string as ldoktor's advice.
    
    Signed-off-by: Feng Yang <fyang@redhat.com>
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>
    Acked-by: Xiaoqing Wei <xwei@redhat.com>

--- kvm_monitor.py
+++ kvm_monitor.py
@@ -957,7 +957,7 @@ class QMPMonitor(Monitor):
                     try:
                         if re.match("^[0-9]+$", opt[1]):
                             value = int(opt[1])
-                        elif re.match("^[0-9.]+$", opt[1]):
+                        elif re.match("^[0-9]+.[0-9]*$", opt[1]):
                             value = float(opt[1])
                         elif "True" in opt[1] or "true" in opt[1]:
                             value = True
