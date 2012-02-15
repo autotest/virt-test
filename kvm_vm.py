257db8b73ff6220027031dd675c8d01616a895ef
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-02-15 14:42:05 -0200

    kvm_vm: Amend b69c24346f912bb00ad25a6f6206b8c3afb2a865
    There was yet another place that needed to be replaced
    with the variable cd_format.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- kvm_vm.py
+++ kvm_vm.py
@@ -881,69 +881,9 @@ class VM(virt_vm.BaseVM):
                 qemu_cmd += " -device virtio-scsi,id=virtio_scsi_pci"
                 have_virtio_scsi = True
             if iso:
-                iso = virt_utils.get_path(root_dir, iso)
-                if params.get("index_enable") == "yes":
-                    drive_index = cdrom_params.get("drive_index")
-                    if drive_index:
-                        index = drive_index
-                    else:
-                        index_global = get_index(index_global)
-                        index = str(index_global)
-                        index_global += 1
-                else:
-                    index = None
-                if has_option(help, "device"):
-                    qemu_cmd += add_drive(help, iso, index, "ide", media="cdrom",
-                                          ide_bus=ide_bus, ide_unit=ide_unit)
-                else:
-                    qemu_cmd += add_cdrom(help, iso, index)
-                if ide_unit == 1:
-                    ide_bus += 1
-                ide_unit ^= 1
-
-        cpu_model = params.get("cpu_model", "qemu64")
-        if "2.6.32" in commands.getoutput("uname -r"):
-            cpu_model = "cpu64-rhel6"
-        flags = params.get("extra_flags")
-        x2apic = params.get("enable_x2apic")
-
-        if "el6" in commands.getoutput("uname -r") and x2apic=="yes":
-            flags += ",+x2apic"
-
-        qemu_cmd += add_cpu_flags(help, cpu_model, flags,
-                                  params.get("cpu_vendor_id"),
-                                  params.get("family"))
-
-        soundhw = params.get("soundcards")
-        if soundhw:
-            if "2.6.32" not in commands.getoutput("uname -r"):
-                qemu_cmd += " -soundhw %s" % soundhw
-
-        # We may want to add the floppy using the "-drive -global " format
-        # and the current script allow the nums of the floppies to be 2
-        # Readonly floppy is supported by adding the parameter of floppy_readonly
-        floppies = params.get("floppy")
-        floppies_readonly = params.get("floppy_readonly")
-        if floppies:
-            for floppy in floppies.split():
-                floppy = virt_utils.get_path(root_dir, floppy)
-                qemu_cmd += add_floppy(help, floppy)
-        if floppies and floppies_readonly:
-            floppy_list = floppies.split()
-            fl_readonly_list = floppies_readonly.split()
-            for index in range(len(fl_readonly_list)):
-                fl_readonly_list[index] = eval(fl_readonly_list[index])
-            if len(floppy_list) > 2 :
-                raise error.TestError("Only the maximum of 2 floppies"
-                                      " can be supported here")
-            for (floppy,fl_readonly) in zip(floppy_list,fl_readonly_list):
-                floppy = virt_utils.get_path(root_dir, floppy)
-                if has_option(help,"global"):
-                    qemu_cmd += add_drive(help,floppy,media="floppy",
-                        floppy_unit=floppy_unit,readonly=fl_readonly)
-                else:
-                    qemu_cmd += add_floppy(help, floppy)
-                floppy_unit ^= 1
+                qemu_cmd += add_cdrom(help, virt_utils.get_path(root_dir, iso),
+                                      cdrom_params.get("drive_index"),
+                                      cd_format)
 
         # Add usb devices
         for usb_dev in params.objects("usb_devices"):
