21e8490d00e61635ecfa562b079d6e992c13d06b
Feng Yang <fyang@redhat.com>
2012-05-03 14:49:44 +0000

    virt test: Configure guest to save and analyze bsod in installation
    put command in finish.bat, because do not want to always add command
    to unattended install anster file.
    
    reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v AutoReboot /d 0 /t REG_DWORD /f
    reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v CrashDumpEnabled /d 2 /t REG_DWORD /f
    reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v NMICrashDump  /d 1 /t REG_DWORD /f
    reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v DumpFile /d %SystemRoot%\Memory.dmp /t REG_EXPAND_SZ /f
    reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v bsod /d "D:\autoit3.exe D:\dump_control.au3" /t REG_SZ /f
    reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v AlwaysKeepMemoryDump /d 1 /t REG_DWORD /f
    25G.
    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\Windows Error Reporting" /v Disabled /d 1 /t REG_DWORD /f
    
    Signed-off-by: Feng Yang <fyang@redhat.com>

--- shared/deps/finish.bat
+++ shared/deps/finish.bat
@@ -19,4 +19,12 @@ if errorlevel 1 goto end
 if errorlevel 0 goto check_process
 
 :end
+reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v AutoReboot /d 0 /t REG_DWORD /f
+reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v CrashDumpEnabled /d 2 /t REG_DWORD /f
+reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v NMICrashDump  /d 1 /t REG_DWORD /f
+reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v DumpFile /d %SystemRoot%\Memory.dmp /t REG_EXPAND_SZ /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v bsod /d "D:\autoit3.exe D:\dump_control.au3" /t REG_SZ /f
+reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v AlwaysKeepMemoryDump /d 1 /t REG_DWORD /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\Windows Error Reporting" /v Disabled /d 1 /t REG_DWORD /f
+
 echo Post set up finished>  COM1
