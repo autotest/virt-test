e40e529e5c1f749cce6d1f1c550bf9af66392d25
Yiqiao Pu <ypu@redhat.com>
2012-05-09 14:18:16 +0000

    virt-test: Update qemu-io to upstream version
    Upstream version qemu-io have some changes:
      - typo fix
      - Add function get_cmd_line to reduce the reduplicate code
      - remove useless lines
      - Use qemu_io_binary to get qemu_io from params
    
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>
    Acked-by: Suqin Huang <shuang@redhat.com>

--- qemu_io.py
+++ qemu_io.py
@@ -1,13 +1,12 @@
 import re, logging
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.bin import utils
-from autotest_lib.client.common_lib import logging_manager
 import aexpect
 import virt_utils
 
-class QemuIOError(Exception):
+class QemuIOParamError(Exception):
     """
-    Base exception for qemu-io command.
+    Parameter Error for qemu-io command
     """
     pass
 
@@ -16,9 +15,9 @@ class QemuIO():
     """
     A class for execute qemu-io command
     """
-
-    def __init__(self, test, image_name, blkdebug_cfg="",
+    def __init__(self, test, params, image_name, blkdebug_cfg="",
                  prompt=r"qemu-io>\s*$", log_filename=None, io_options=""):
+        self.type = ""
         if log_filename:
             log_filename += "-" + virt_utils.generate_random_string(4)
             self.output_func = virt_utils.log_line
@@ -30,7 +29,51 @@ class QemuIO():
         self.prompt=prompt
         self.blkdebug_cfg=blkdebug_cfg
 
-        self.qemu_io_cmd = virt_utils.get_path(test.bindir, "qemu-io")
+        self.qemu_io_cmd = virt_utils.get_path(test.bindir,
+                                               params.get("qemu_io_binary",
+                                                          "qemu-io"))
+        self.io_options = io_options
+        self.run_command = False
+        self.image_name = image_name
+        self.blkdebug_cfg = blkdebug_cfg
+
+
+    def get_cmd_line(self, ignore_option=[], essential_option=[],
+                     forbid_option=[]):
+        """
+        Generate the command line for qemu-io from the parameters
+        @params ignore_option: list for the options should not in command
+        @params essential_option: list for the essential options
+        @params forbid_option: list for the option should not in command
+        @return: qemu-io command line
+        """
+        warning_flag = True
+        essential_flag = False
+
+        qemu_io_cmd = self.qemu_io_cmd
+        if self.io_options:
+            for io_option in re.split(",", self.io_options):
+                if io_option in ignore_option:
+                    pass
+                elif io_option in forbid_option:
+                    raise QemuIOParamError
+                else:
+                    if not essential_flag and io_option in essential_option:
+                        essential_flag = True
+                    if len(io_option) == 1:
+                        qemu_io_cmd += " -%s" % io_option
+                    else:
+                        qemu_io_cmd += " --%s" % io_option
+            if not essential_flag:
+                raise QemuIOParamError
+
+        if self.image_name:
+            qemu_io_cmd += " "
+            if self.blkdebug_cfg:
+                qemu_io_cmd += "blkdebug:%s:" %  self.blkdebug_cfg
+            qemu_io_cmd += self.image_name
+
+        return qemu_io_cmd
 
     def cmd_output(self, command):
         """
@@ -48,33 +91,16 @@ class QemuIOShellSession(QemuIO):
     """
     Use a shell session to execute qemu-io command
     """
-    def __init__(self, test, image_name, blkdebug_cfg="",
-                 prompt=r"qemu-io>\s*$", log_filename=None, io_options=""):
-        QemuIO.__init__(self, test, image_name, blkdebug_cfg, prompt,
+    def __init__(self, test, params, image_name, blkdebug_cfg="",
+                 prompt=r"qemu+-io>\s*$", log_filename=None, io_options=""):
+        QemuIO.__init__(self, test, params, image_name, blkdebug_cfg, prompt,
                         log_filename, io_options)
 
         self.type = "shell"
-        ignore_option = ["h", "help", "V", "version", "c", "cmd"]
+        forbid_option = ["h", "help", "V", "version", "c", "cmd"]
         qemu_io_cmd = self.qemu_io_cmd
 
-        if io_options:
-            for io_option in re.split(",", io_options):
-                if io_option in ignore_option:
-                    raise QemuIOError("Opion %s should not be here."
-                                      % io_option)
-                else:
-                    if len(io_option) == 1:
-                        qemu_io_cmd += " -%s" % io_option
-                    else:
-                        qemu_io_cmd += " --%s" % io_option
-
-        if image_name:
-            qemu_io_cmd += " "
-            if blkdebug_cfg:
-                qemu_io_cmd += "blkdebug:%s:" %  blkdebug_cfg
-            qemu_io_cmd += image_name
-
-        self.qemu_io_cmd = qemu_io_cmd
+        self.qemu_io_cmd = self.get_cmd_line(forbid_option=forbid_option)
         self.create_session = True
         self.session = None
 
@@ -94,10 +120,15 @@ class QemuIOShellSession(QemuIO):
         output_prefix = self.output_prefix
         if self.create_session:
             error.context("Running command: %s" % qemu_io_cmd, logging.info)
-            self.session = aexpect.ShellSession(qemu_io_cmd, prompt=prompt,
-                                                 output_func=output_func,
-                                                 output_params=output_params,
-                                                 output_prefix=output_prefix)
+            self.session = aexpect.ShellSession(qemu_io_cmd, echo=True,
+                                                prompt=prompt,
+                                                output_func=output_func,
+                                                output_params=output_params,
+                                                output_prefix=output_prefix)
+            # Record the command line in log file
+            params = self.output_params + (qemu_io_cmd, )
+            self.output_func(*params)
+
             self.create_session = False
             # Get the reaction from session
             self.session.cmd_output("\n")
@@ -117,39 +148,16 @@ class QemuIOSystem(QemuIO):
     """
     Run qemu-io with a command line which will return immediately
     """
-    def __init__(self, test, image_name, blkdebug_cfg="",
+    def __init__(self, test, params, image_name, blkdebug_cfg="",
                  prompt=r"qemu-io>\s*$", log_filename=None, io_options=""):
-        QemuIO.__init__(self, test, image_name, blkdebug_cfg, prompt,
+        QemuIO.__init__(self, test, params, image_name, blkdebug_cfg, prompt,
                         log_filename, io_options)
-        no_warning_option = ["h", "help", "V", "version", "c", "cmd"]
         ignore_option = ["c", "cmd"]
+        essential_option = ["h", "help", "V", "version", "c", "cmd"]
         qemu_io_cmd = self.qemu_io_cmd
 
-        warning_flag = True
-        self.run_command = False
-        for io_option in re.split(",", io_options):
-            if io_option in no_warning_option:
-                warning_flag = False
-            if io_option in ignore_option:
-                self.run_command = True
-            else:
-                if len(io_option) == 1:
-                    qemu_io_cmd += " -%s" % io_option
-                else:
-                    qemu_io_cmd += " --%s" % io_option
-
-        if warning_flag:
-            raise error.QemuIOError("The qemu-io command will not return"
-                                    " immedirately with this "
-                                    "option %s" % io_options)
-
-        if image_name:
-            qemu_io_cmd += " "
-            if blkdebug_cfg:
-                qemu_io_cmd += "blkdebug:%s:" %  blkdebug_cfg
-            qemu_io_cmd += image_name
-
-        self.qemu_io_cmd = qemu_io_cmd
+        self.qemu_io_cmd = self.get_cmd_line(ignore_option=ignore_option,
+                                             essential_option=essential_option)
 
     @error.context_aware
     def cmd_output(self, command, timeout=60):
@@ -160,11 +168,15 @@ class QemuIOSystem(QemuIO):
         @param timeout: timeout for execute the command
         """
         qemu_io_cmd = self.qemu_io_cmd
-        if self.run_command:
+        if command:
             qemu_io_cmd += " -c '%s'" % command
 
         error.context("Running command: %s" % qemu_io_cmd, logging.info)
         output = utils.system_output(qemu_io_cmd, timeout=timeout)
+
+        # Record command line in log file
+        params = self.output_params + (qemu_io_cmd,)
+        self.output_func(*params)
 
         params = self.output_params + (output,)
         self.output_func(*params)
