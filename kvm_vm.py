875d25f6be1656ca39d261438342ff038a44b901
Yiqiao Pu <ypu@redhat.com>
2012-06-11 17:15:27 +0800

    virt: Add virt storage module
    Put all storage related functions into one module and put the
    image related function such as create and romve into one class.
    This exports:
      - two functions for get image/blkdebug filename
      - one class for image operates
        - options:
            - image file name and format
            - base image file name and format(option)
            - snapshot image file name and format(option)
        - functions:
            - create
            - remove
            - clone_image
            - rm_clone_image
            - check_image
            - backup_image
            - convert(new)
            - rebase(new)
            - commit(new)
            - snapshot_create(new)
    
    change from v1:
     - Make a base module and two separated module for libvirt and kvm.
       Now the libvirt side functions are not implemented yet, so use
       kvm side function temporarily
     - Fix code style and docstring problems
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

--- kvm_vm.py
+++ kvm_vm.py
@@ -9,6 +9,7 @@ import virt_test_utils
 from autotest_lib.client.common_lib import error, cartesian_config
 from autotest_lib.client.bin import utils
 import virt_utils, virt_vm, virt_test_setup, kvm_monitor, aexpect
+import virt_storage
 
 
 class VM(virt_vm.BaseVM):
@@ -1029,37 +1030,28 @@ class VM(virt_vm.BaseVM):
                     virtio_scsi_pcis.append("virtio_scsi_pci%d" % i)
 
             qemu_cmd += add_drive(help,
-                  virt_vm.get_image_filename(image_params, root_dir),
-                  index,
-                  image_params.get("drive_format"),
-                  image_params.get("drive_cache"),
-                  image_params.get("drive_werror"),
-                  image_params.get("drive_rerror"),
-                  image_params.get("drive_serial"),
-                  image_params.get("image_snapshot") == "yes",
-                  image_params.get("image_boot") == "yes",
-                  virt_vm.get_image_blkdebug_filename(image_params, root_dir),
-                  image_params.get("image_format"),
-                  image_params.get("image_aio", "native"),
-                  "disk", ide_bus, ide_unit, vdisk,
-                  image_params.get("drive_pci_addr"),
-                  physical_block_size=image_params.get("physical_block_size"),
-                  logical_block_size=image_params.get("logical_block_size"),
-                  bus=bus, port=port,
-                  bootindex=image_params.get("bootindex"),
-                  removable=image_params.get("removable"),
-                  min_io_size=image_params.get("min_io_size"),
-                  opt_io_size=image_params.get("opt_io_size"),
-                  scsi=image_params.get("virtio-blk-pci_scsi"),
-                  readonly=image_params.get("image_readonly"))
-
-            # increase the bus and unit no for ide device
-            if params.get("drive_format") == "ide":
-                if ide_unit == 1:
-                    ide_bus += 1
-                ide_unit ^= 1
-            else:
-                vdisk += 1
+                    virt_storage.get_image_filename(image_params, root_dir),
+                    image_params.get("drive_index"),
+                    image_params.get("drive_format"),
+                    image_params.get("drive_cache"),
+                    image_params.get("drive_werror"),
+                    image_params.get("drive_rerror"),
+                    image_params.get("drive_serial"),
+                    image_params.get("image_snapshot"),
+                    image_params.get("image_boot"),
+                    virt_storage.get_image_blkdebug_filename(image_params,
+                                                           self.virt_dir),
+                    bus,
+                    port,
+                    image_params.get("bootindex"),
+                    image_params.get("removable"),
+                    image_params.get("min_io_size"),
+                    image_params.get("opt_io_size"),
+                    image_params.get("physical_block_size"),
+                    image_params.get("logical_block_size"),
+                    image_params.get("image_readonly"),
+                    image_params.get("drive_scsiid"),
+                    image_params.get("drive_lun"))
 
         # Networking
         redirs = []
