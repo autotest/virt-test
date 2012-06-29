cbf17e8726f1f21bbeda4c5c071b25c32e692f60
Yiqiao Pu <ypu@redhat.com>
2012-06-29 18:47:43 +0800

    virt: Add virt storage module
    Put all storage related functions into one module and put the
    image related function such as create and romve into one class.
    This exports:
      - two functions for get image/blkdebug filename
      - one class for image operates
        - options:
            - image file name and format
            - base image file name and format(option)
            - snapshot image file name and format(option)
        - functions:
            - create
            - remove
            - clone_image
            - rm_clone_image
            - check_image
            - backup_image
            - convert(new)
            - rebase(new)
            - commit(new)
            - snapshot_create(new)
    
    change from v1:
     - Make a base module and two separated module for libvirt and kvm.
       Now the libvirt side functions are not implemented yet, so use
       kvm side function temporarily
     - Fix code style and docstring problems
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>
    Acked-by: Feng Yang <fyang@redhat.com>

--- virt_env_process.py
+++ virt_env_process.py
@@ -24,7 +24,7 @@ def preprocess_image(test, params):
     @param params: A dict containing image preprocessing parameters.
     @note: Currently this function just creates an image if requested.
     """
-    image_filename = virt_utils.get_image_filename(params, test.bindir)
+    image_filename = virt_vm.get_image_filename(params, test.bindir)
 
     create_image = False
 
@@ -35,7 +35,7 @@ def preprocess_image(test, params):
           os.path.exists(image_filename)):
         create_image = True
 
-    if create_image and not virt_utils.create_image(params, test.bindir):
+    if create_image and not virt_vm.create_image(params, test.bindir):
         raise error.TestError("Could not create image")
 
 
@@ -118,13 +118,13 @@ def postprocess_image(test, params):
     """
     if params.get("check_image") == "yes":
         try:
-            virt_utils.check_image(params, test.bindir)
+            virt_vm.check_image(params, test.bindir)
         except Exception, e:
             if params.get("restore_image_on_check_error", "no") == "yes":
-                virt_utils.backup_image(params, test.bindir, 'restore', True)
+                virt_vm.backup_image(params, test.bindir, 'restore', True)
             raise e
     if params.get("remove_image") == "yes":
-        virt_utils.remove_image(params, test.bindir)
+        virt_vm.remove_image(params, test.bindir)
 
 
 def postprocess_vm(test, params, env, name):
