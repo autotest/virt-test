57a32ad886147a7dd05f605b8ba87628ca34fed6
Yiqiao Pu <ypu@redhat.com>
2012-08-22 11:33:23 +0000

    KVM-test: Add essential_option check in qemu_io
    Add essential option check in qemu_io to ignore the wrong error
    raised when the essential_option is empty.
    
    
    change from v1:
      - fix the wrong judgement
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>
    Acked-by: Suqin Huang <shuang@redhat.com>

--- qemu_io.py
+++ qemu_io.py
@@ -66,7 +66,7 @@ class QemuIO(object):
                         qemu_io_cmd += " -%s" % io_option
                     else:
                         qemu_io_cmd += " --%s" % io_option
-            if not essential_flag:
+            if essential_option and not essential_flag:
                 raise QemuIOParamError
 
         if self.image_name:
