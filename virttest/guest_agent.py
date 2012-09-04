db9ea260a2e880095546e94f710561d43aed2e7a
Qingtang Zhou <qzhou@redhat.com>
2012-09-04 17:41:24 +0800

    virt.guest_agent: Add shutdown parameter to shutdown method
    The 'guest-shutdown' cmd in qemu guest agent has 3 modes:
     - 'powerdown': The default action for this command.
     - 'reboot': Reboot guest.
     - 'halt': halt guest directly.
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- virttest/guest_agent.py
+++ virttest/guest_agent.py
@@ -4,7 +4,11 @@ Interfaces to the virt agent.
 @copyright: 2008-2012 Red Hat Inc.
 """
 
+<<<<<<< HEAD
 import socket, time, logging, random
+=======
+import socket, time, logging
+>>>>>>> virt.guest_agent: Add shutdown parameter to shutdown method
 from autotest.client.shared import error
 from kvm_monitor import Monitor, MonitorError
 
