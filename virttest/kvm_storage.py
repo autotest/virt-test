35d84e07002e67dc8d4fcc46a6ad5ed87abdd5f1
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-09-04 21:00:52 -0300

    virt.kvm_storage: Reduce verbosity of operations
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- virttest/kvm_storage.py
+++ virttest/kvm_storage.py
@@ -97,7 +97,7 @@ class QemuImg(storage.QemuImg):
         try:
             msg = "Create image by command: %s" % qemu_img_cmd
             error.context(msg, logging.info)
-            result = utils.system_output(qemu_img_cmd, verbose=False)
+            result = utils.run(qemu_img_cmd, verbose=False)
         except error.CmdError, e:
             logging.error("Could not create image, failed with error message:"
                             "%s", str(e))
@@ -326,7 +326,8 @@ class QemuImg(storage.QemuImg):
         image_is_qcow2 = self.image_format == 'qcow2'
         if os.path.exists(image_filename) and image_is_qcow2:
             # Verifying if qemu-img supports 'check'
-            q_result = utils.run(qemu_img_cmd, ignore_status=True)
+            q_result = utils.run(qemu_img_cmd, ignore_status=True,
+                                 verbose=False)
             q_output = q_result.stdout
             check_img = True
             if not "check" in q_output:
@@ -339,15 +340,15 @@ class QemuImg(storage.QemuImg):
                 check_img = False
             if check_img:
                 try:
-                    utils.system("%s info %s" % (qemu_img_cmd,
-                                                 image_filename))
+                    utils.run("%s info %s" % (qemu_img_cmd, image_filename),
+                              verbose=False)
                 except error.CmdError:
                     logging.error("Error getting info from image %s",
                                   image_filename)
 
                 cmd_result = utils.run("%s check %s" %
                                        (qemu_img_cmd, image_filename),
-                                       ignore_status=True)
+                                       ignore_status=True, verbose=False)
                 # Error check, large chances of a non-fatal problem.
                 # There are chances that bad data was skipped though
                 if cmd_result.exit_status == 1:
