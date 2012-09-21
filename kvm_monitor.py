bbd356bdd824354d15c84ba1faee5a75ebdec941
Shuping Cui <scui@redhat.com>
2012-09-21 13:47:05 +0000

    KVM_Test: Update check QMP monitor command.
    Signed-off-by: Shuping Cui <scui@redhat.com>
    Acked-by: Qingtang Zhou <qzhou@redhat.com>

--- kvm_monitor.py
+++ kvm_monitor.py
@@ -1068,7 +1068,7 @@ class QMPMonitor(Monitor):
         cmd_output = []
         for cmdline in cmdlines.split(";"):
             command = cmdline.split()[0]
-            if self._has_command(command):
+            if not self._has_command(command):
                 if "=" in cmdline:
                     command = cmdline.split()[0]
                     cmdargs = " ".join(cmdline.split()[1:]).split(",")
