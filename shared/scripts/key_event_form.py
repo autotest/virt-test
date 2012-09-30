a62a82c47533059860765456d680eb01827a82b5
Marian Krcmarik <mkrcmari@redhat.com>
2012-09-30 17:19:11 +0200

    shared/scripts: Simple wxPython form catching keyEvents
    Form catches keyEvents and store them into a text file.
    
    Signed-off-by: Marian Krcmarik <mkrcmari@redhat.com>

---
+++ shared/scripts/key_event_form.py
@@ -0,0 +1,33 @@
+import wx
+
+class TestForm(wx.Frame):
+
+    def __init__(self):
+        wx.Frame.__init__(self, None, wx.ID_ANY, "Test")
+
+        # Add a panel so it looks correct on all platforms
+        panel = wx.Panel(self, wx.ID_ANY)
+        btn = wx.TextCtrl(panel, value="")
+
+        btn.Bind(wx.EVT_CHAR, self.on_char_event)
+        btn.SetFocus()
+
+        # Clean text file
+        input_file = open("/tmp/autotest-rv_input", "w")
+        input_file.write("")
+        input_file.close()
+
+    def on_char_event(self, event):
+        keycode = event.GetKeyCode()
+
+        # Store caught keycodes into text file
+        input_file = open("/tmp/autotest-rv_input", "a")
+        input_file.write("%s," % str(keycode))
+        input_file.close()
+        event.Skip()
+
+if __name__ == "__main__":
+    APP = wx.PySimpleApp()
+    FRAME = TestForm()
+    FRAME.Show()
+    APP.MainLoop()
