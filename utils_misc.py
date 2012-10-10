9008a448cbaee4fee4da20b1aba5f24dc799c885
Yunping Zheng <yunzheng@redhat.com>
2012-10-10 14:34:20 +0000

    KVM Test: Add systemtap profiler
    This patch adds a systemtap profiler to autotest. Now systemtap
    can run during your test, providing useful debug data.
    
    When you use this profiler on virt tests, you should add
        'profilers = systemtap'
     and
        'stap_script_file = file_path_systemtap_script'
    
    On your test cfg file. If the 'file_path_systemtap_script' you
    assigned is a relative path,the script will try to find the file
    under 'test.autodir/profilers/systemtap/scripts'.
    
    Signed-off-by: Yunping Zheng <yunzheng@redhat.com>

--- utils_misc.py
+++ utils_misc.py
@@ -1672,7 +1672,7 @@ def run_tests(parser, job):
             # Setting up profilers during test execution.
             profilers = dict.get("profilers", "").split()
             for profiler in profilers:
-                job.profilers.add(profiler)
+                job.profilers.add(profiler, **dict)
             # We need only one execution, profiled, hence we're passing
             # the profile_only parameter to job.run_test().
             profile_only = bool(profilers) or None
