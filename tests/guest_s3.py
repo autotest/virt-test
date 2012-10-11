a47772000260deebcfd499ecec5d254def65cdaf
Qingtang Zhou <qzhou@redhat.com>
2012-10-11 11:32:35 +0800

    tests: Add a new test case guest_s3
    This is another version of guest s3 test, it supports
    both linux and windows guest.
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

---
+++ tests/guest_s3.py
@@ -0,0 +1,16 @@
+from autotest.client.shared import error
+from virttest.utils_test import GuestSuspend
+
+
+@error.context_aware
+def run_guest_s3(test, params, env):
+    """
+    Suspend guest to memory, supports both Linux and Windows.
+
+    @param test: kvm test object.
+    @param params: Dictionary with test parameters.
+    @param env: Dictionary with the test environment.
+    """
+    vm = env.get_vm(params["main_vm"])
+    vm.verify_alive()
+    GuestSuspend.guest_suspend_mem(params, vm)
