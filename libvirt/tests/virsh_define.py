6a1a581d533fe9ca5eaa28367628529d7856a9c5
Chris Evich <cevich@redhat.com>
2012-10-22 09:55:37 -0200

    virt-libvirt: Add virsh_define (and undefine) test
    Thanks to Yu Mingfei <yumingfei@cn.fujitsu.com> for the original
    virsh_rename_domain test which this is based upon. Exercises
    virsh define and virsh_undefine plus auto-generated uuids.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

---
+++ libvirt/tests/virsh_define.py
@@ -0,0 +1,68 @@
+import logging
+from autotest.client.shared import error
+from virttest import libvirt_xml
+
+def run_virsh_define(test, params, env):
+    """
+    Test defining/undefining domain by dumping xml, changing it, and re-adding.
+
+    (1) Get name and uuid of existing vm
+    (2) Extract XML, undefine domain
+    (3) Change name & uuid in XML
+    (4) Define domain
+    (5) Verify domain start
+    (6) Change name & uuid back to original
+    """
+
+    def do_rename(vm, new_name, uuid=None):
+        if vm.is_alive():
+            vm.destroy(gracefully=True)
+
+        vmxml = libvirt_xml.VMXML()
+        vmxml.new_from_dumpxml(vm.name)
+        backup = vmxml.copy()
+        # can't do in-place rename, must operate on XML
+        try:
+            vmxml.undefine()
+            # All failures trip a single exception
+        except libvirt_xml.LibvirtXMLError, detail:
+            del vmxml # clean up temporary files
+            raise error.TestFail("Error reported while undefining VM:" + detail)
+        # Alter the XML
+        vmxml.vm_name = new_name
+        if uuid is None:
+            # invalidate uuid so libvirt will regenerate
+            del vmxml.uuid
+            vm.uuid = None
+        else:
+            vmxml.uuid = uuid
+            vm.uuid = uuid
+        # Re-define XML to libvirt
+        logging.info("Test rename %s to %s.", vm.name, new_name)
+        try:
+            vmxml.define()
+        except libvirt_xml.LibvirtXMLError:
+            del vmxml # clean up temporary files
+            # Allow exceptions thrown here since state will be undefined
+            backup.define()
+        # Keep names uniform
+        vm.name = new_name
+        # Exercize the defined XML
+        if vm.start():
+            logging.info("Start new guest %s succeed.", new_name)
+        vm.destroy()
+        # vmxml and backup go out of scope, tmp files auto-removed
+
+    # Run test
+    vm_name = params.get("main_vm")
+    vm = env.get_vm(params["main_vm"])
+    new_name = params.get("new_name", "test")
+    uuid = vm.get_uuid()
+    logging.info("Original uuid: %s" % vm.get_uuid())
+    assert uuid is not None
+    do_rename(vm, new_name) # give it a new uuid
+    logging.info("Generated uuid: %s" % vm.get_uuid())
+    assert vm.uuid != uuid
+    # Rename back to original to maintain known-state
+    do_rename(vm, vm_name, uuid) # restore original uuid
+    logging.info("Final uuid: %s" % vm.get_uuid())
