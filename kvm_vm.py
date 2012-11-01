088bf16e5ba8c20dc0aa8d2d1e123ac1dea5b696
Feng Yang <fyang@redhat.com>
2012-11-01 12:23:16 +0000

    virt-kvm/libvirt: Inherit needs_restart() metod
    upstream commit id:
    c405cccb0db6ac83042e67bb74f5d2653f7e7de9
    
        virt-kvm/libvirt: Inherit needs_restart() metod
    
        + Both libvirt and KVM had nearly identical needs_restart
        implimentations.  This patch allows them to inherit common checks while
        allowing specialty checks via subclass overrides.
    
        + Fix networking bug in needs_restart() for virt-kvm and virt-libvirt.
        Before each test, preprocess_vm() checks any VM instances stored in
        env file against requested parameters, using vm.needs_restart().  However,
        as implemented, only parameters encoded on the qemu or virt-install
        command-line are tested.  This patch adds a check to also compare networking
        parameters.
    
        N.B. It does NOT verify non-networking related hot-added devices.
    
        Signed-off-by: Chris Evich <cevich@redhat.com>
    
    Signed-off-by: Feng Yang <fyang@redhat.com>
    Acked-by: Yiqiao Pu <ypu@redhat.com>

--- kvm_vm.py
+++ kvm_vm.py
@@ -164,7 +164,7 @@ class VM(virt_vm.BaseVM):
         return VM(name, params, root_dir, address_cache, state)
 
 
-    def __make_qemu_command(self, name=None, params=None, root_dir=None):
+    def make_create_command(self, name=None, params=None, root_dir=None):
         """
         Generate a qemu command line. All parameters are optional. If a
         parameter is not supplied, the corresponding value stored in the
@@ -1751,7 +1751,7 @@ class VM(virt_vm.BaseVM):
                         raise virt_vm.VMBadPATypeError(pa_type)
                 else:
                     # fill in key values, validate nettype
-                    # note: __make_qemu_command() calls vm.add_nic (i.e. on a copy)
+                    # note: make_create_command() calls vm.add_nic (i.e. on a copy)
                     if nic_params.get('netdst') == 'private':
                         nic.netdst = (test_setup.
                                       PrivateBridgeConfig(nic_params).brname)
@@ -1808,7 +1808,7 @@ class VM(virt_vm.BaseVM):
                     raise virt_vm.VMPAError(pa_type)
 
             # Make qemu command
-            qemu_command = self.__make_qemu_command()
+            qemu_command = self.make_create_command()
 
             # Add migration parameters if required
             if migration_mode == "tcp":
@@ -2878,18 +2878,6 @@ class VM(virt_vm.BaseVM):
                     migration_exec_cmd="cat "+path, mac_source=self)
         self.verify_status('running') # Throws exception if not
 
-    def needs_restart(self, name, params, basedir):
-        """
-        Verifies whether the current qemu commandline matches the requested
-        one, based on the test parameters.
-        """
-        if (self.__make_qemu_command() !=
-                self.__make_qemu_command(name, params, basedir)):
-            logging.debug("VM params in env don't match requested, restarting.")
-            return True
-        else:
-            logging.debug("VM params in env do match requested, continuing.")
-            return False
 
     def pause(self):
         """
