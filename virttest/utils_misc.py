7853b4391726ec13fdb50b38c8d0d21bc52f1001
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-11-28 11:29:58 -0200

    download_file: Skip question to check file SHA1SUM
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- virttest/utils_misc.py
+++ virttest/utils_misc.py
@@ -4659,11 +4659,8 @@ def download_file(url, destination, sha1_url, interactive=False):
             logging.warning("Missing file %s", path)
     else:
         logging.info("Found %s", path)
-        if interactive:
-            if sha1 is None:
-                answer = 'n'
-            else:
-                answer = utils.ask("Would you like to check %s SHA1 sum?" % path)
+        if sha1 is None:
+            answer = 'n'
         else:
             answer = 'y'
 
