f596dd3c251bbd401b4b5a2526489f29e7ff2434
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-11-30 11:11:15 -0200

    create_config_files: Log diff output as INFO
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- virttest/utils_misc.py
+++ virttest/utils_misc.py
@@ -4574,7 +4574,7 @@ def create_config_files(test_dir, shared_dir, interactive, step=None):
             diff_result = utils.run("diff -Naur %s %s" % (dst_file, src_file),
                                     ignore_status=True, verbose=False)
             if diff_result.exit_status != 0:
-                logging.debug("%s result:\n %s" %
+                logging.info("%s result:\n %s" %
                               (diff_result.command, diff_result.stdout))
                 if interactive:
                     answer = utils.ask("Config file  %s differs from %s."
