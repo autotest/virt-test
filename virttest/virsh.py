autotest/virt-test

commit 8f464022f845e958c4777f943384179b66428eec

virt-libvirt: Added debug logging for virsh.define()
Signed-off-by: Chris Evich <cevich@redhat.com>

virttest/virsh.py
@@ -787,6 +787,7 @@ def define(xml_path, **dargs):
     dargs['ignore_status'] = False
     try:
         command("define --file %s" % xml_path, **dargs)
+        logging.debug("defined VM %s", name)
         return True
     except error.CmdError:
         logging.error("Define %s failed.", xml_path)

commit 7501a6395c3d40f8a044accb514d570b77abd202

virt-libvirt: Fix debug log message typo.
* Bug introduced in 8f464022f845e958c4777f943384179b66428eec
* Fix 'name' -> 'xml_path'

Signed-off-by: Chris Evich <cevich@redhat.com>

virttest/virsh.py
@@ -787,7 +787,7 @@ def define(xml_path, **dargs):
     dargs['ignore_status'] = False
     try:
         command("define --file %s" % xml_path, **dargs)
-        logging.debug("defined VM %s", name)
+        logging.debug("Defined VM from %s", xml_path)
         return True
     except error.CmdError:
         logging.error("Define %s failed.", xml_path)