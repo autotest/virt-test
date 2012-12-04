d003211c59a0b7f4b9fe49316fc70f237724c51f
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-12-04 21:52:24 -0200

    virttest: Add libraries utils_env and utils_params
    Containing the Env and Params classes.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ virttest/utils_params.py
@@ -0,0 +1,37 @@
+import UserDict
+
+
+class Params(UserDict.IterableUserDict):
+    """
+    A dict-like object passed to every test.
+    """
+    def objects(self, key):
+        """
+        Return the names of objects defined using a given key.
+
+        @param key: The name of the key whose value lists the objects
+                (e.g. 'nics').
+        """
+        return self.get(key, "").split()
+
+
+    def object_params(self, obj_name):
+        """
+        Return a dict-like object containing the parameters of an individual
+        object.
+
+        This method behaves as follows: the suffix '_' + obj_name is removed
+        from all key names that have it.  Other key names are left unchanged.
+        The values of keys with the suffix overwrite the values of their
+        suffixless versions.
+
+        @param obj_name: The name of the object (objects are listed by the
+                objects() method).
+        """
+        suffix = "_" + obj_name
+        new_dict = self.copy()
+        for key in self:
+            if key.endswith(suffix):
+                new_key = key.split(suffix)[0]
+                new_dict[new_key] = self[key]
+        return new_dict
