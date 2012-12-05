af3f8f11c1743675c5ca40ef0799e587d27297de
Feng Yang <fyang@redhat.com>
2012-12-05 12:38:07 +0000

    kvm_vm.py Redesign sr-iov part code
    After this patch, we can configure sr-iov device in the
    same way as nic device. So older test case could run
    with sr-iov device.
    
    Changes from v1:
    Recreate the patch accroding latest next tree.
    
    Signed-off-by: Feng Yang <fyang@redhat.com>

--- virttest/kvm_vm.py
+++ virttest/kvm_vm.py
@@ -1419,7 +1419,7 @@ class VM(virt_vm.BaseVM):
                 else:
                     tftp = None
                 nettype = nic.get("nettype", "bridge")
-                # don't force conversion of add_nic()/add_net() optional parameter
+                # don't force conversion add_nic()/add_net() optional parameter
                 if nic.has_key('tapfd'):
                     tapfd = int(nic.tapfd)
                 else:
@@ -1704,12 +1704,6 @@ class VM(virt_vm.BaseVM):
         if params.get("disable_hpet") == "yes":
             qemu_cmd += add_no_hpet(help_text)
 
-        # If the PCI assignment step went OK, add each one of the PCI assigned
-        # devices to the qemu command line.
-        if vm.pci_assignable:
-            for pci_id in vm.pa_pci_ids:
-                qemu_cmd += add_pcidevice(help_text, pci_id)
-
         qemu_cmd += add_rtc(help_text)
 
         if has_option(help_text, "boot"):
@@ -1903,13 +1897,13 @@ class VM(virt_vm.BaseVM):
                 pa_type = nic_params.get("pci_assignable")
                 if pa_type and pa_type != "no":
                     if self.pci_assignable is None:
-                        self.pci_assignable = test_setup.PciAssignable(
-                            driver=params.get("driver"),
-                            driver_option=params.get("driver_option"),
-                            host_set_flag = params.get("host_setup_flag"),
-                            kvm_params = params.get("kvm_default"),
-                            vf_filter_re = params.get("vf_filter_re"),
-                            pf_filter_re = params.get("pf_filter_re"))
+                       self.pci_assignable = test_setup.PciAssignable(
+                           driver=params.get("driver"),
+                           driver_option=params.get("driver_option"),
+                           host_set_flag = params.get("host_setup_flag"),
+                           kvm_params = params.get("kvm_default"),
+                           vf_filter_re = params.get("vf_filter_re"),
+                           pf_filter_re = params.get("pf_filter_re"))
                     # Virtual Functions (VF) assignable devices
                     if pa_type == "vf":
                         self.pci_assignable.add_device(device_type=pa_type)
@@ -1930,7 +1924,7 @@ class VM(virt_vm.BaseVM):
                         # Will raise exception if source doesn't
                         # have cooresponding nic
                         logging.debug("Copying mac for nic %s from VM %s"
-                                        % (nic.nic_name, mac_source.name))
+                                       % (nic.nic_name, mac_source.name))
                         nic.mac = mac_source.get_mac_address(nic.nic_name)
                     if nic.nettype == 'bridge' or nic.nettype == 'network':
                         try:
