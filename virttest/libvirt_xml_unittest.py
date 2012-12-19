fbf6293420d87bb170f0645f834f50e81cef1fb1
Chris Evich <cevich@redhat.com>
2012-12-19 13:10:31 -0500

    virt-libvirt: Add test cases for libvirt_xml NetworkXML classes
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- virttest/libvirt_xml_unittest.py
+++ virttest/libvirt_xml_unittest.py
@@ -50,5 +50,31 @@ class TestVMXML(LibvirtXMLTestBase):
         self.assertEqual(test_xtf.find('vcpu').text, '4')
 
 
+class testNetworkXML(LibvirtXMLTestBase):
+
+    def _from_scratch(self):
+        netxml = libvirt_xml.NetworkXML(network_name = 'test0',
+                                        virsh_instance = self.dummy_virsh)
+        self.assertEqual(netxml.name, 'test0')
+        netxml.name = 'test1'
+        netxml.uuid = 'test2'
+        netxml.bridge = {'test3':'test4'}
+        return netxml
+
+    def test_getters(self):
+        netxml = self._from_scratch()
+        self.assertEqual(netxml.name, 'test1')
+        self.assertEqual(netxml.uuid, 'test2')
+        self.assertEqual(netxml.bridge, {'test3':'test4'})
+
+
+    def test_valid_xml(self):
+        netxml = self._from_scratch()
+        test_xtf = xml_utils.XMLTreeFile(netxml.xml) # re-parse from filename
+        self.assertEqual(test_xtf.find('name').text, 'test1')
+        self.assertEqual(test_xtf.find('uuid').text, 'test2')
+        self.assertEqual(test_xtf.find('bridge').get('test3'), 'test4')
+
+
 if __name__ == "__main__":
     unittest.main()
