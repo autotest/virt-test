01705b902c3ed1677d0e7221c9bb6e114052fe02
Xiaoqing Wei <xwei@redhat.com>
2012-12-27 14:02:17 +0000

    KVM Test: remove the unnecessary double quote on uri when migrate using qmp monitor
    Changelog from V1:
    now fixing the kvm_monitor.py instead of the kvm_vm.py, so no need to check the monitor type.
    
    
    Signed-off-by: Xiaoqing Wei <xwei@redhat.com>
    Acked-by: Yiqiao Pu <ypu@redhat.com>

--- kvm_monitor.py
+++ kvm_monitor.py
@@ -1274,6 +1274,7 @@ class QMPMonitor(Monitor):
         args = {"uri": uri,
                 "blk": full_copy,
                 "inc": incremental_copy}
+        args['uri'] = re.sub('"', "", args['uri'])
         try:
             return self.cmd("migrate", args)
         except QMPCmdError, e:
