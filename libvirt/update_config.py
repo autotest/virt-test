f0fead1ae891febc184fd6ab903975ff7deebfc5
Lucas Meneghel Rodrigues <lmr@redhat.com>
2013-01-07 18:23:33 -0200

    update_config: Also include subtests.cfg creation
    So that the config is fully functional.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- libvirt/update_config.py
+++ libvirt/update_config.py
@@ -10,8 +10,10 @@ from virttest import data_dir, bootstrap
 
 test_dir = os.path.dirname(sys.modules[__name__].__file__)
 test_dir = os.path.abspath(test_dir)
+t_type = os.path.basename(test_dir)
 shared_dir = os.path.join(data_dir.get_root_dir(), "shared")
 
 if __name__ == "__main__":
     bootstrap.create_config_files(test_dir, shared_dir, interactive=False,
                                   force_update=True)
+    bootstrap.create_subtests_cfg(t_type)
