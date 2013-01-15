a42ad213866df0a28280696be493ff50caeeb357
Yiqiao Pu <ypu@redhat.com>
2013-01-15 14:47:15 +0000

    KVM-test: Add tray open status check script
    For checking tray status for cdrom tray lock case.
    
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

---
+++ deps/tray_open.py
@@ -0,0 +1,11 @@
+import CDROM, fcntl, os, sys
+
+if len(sys.argv) > 1:
+    fd = os.open(sys.argv[1], os.O_RDONLY | os.O_NONBLOCK)
+
+    if CDROM.CDS_TRAY_OPEN == fcntl.ioctl(fd, CDROM.CDROM_DRIVE_STATUS):
+       print "cdrom is open"
+    else:
+       print "cdrom is close"
+
+    os.close(fd)
