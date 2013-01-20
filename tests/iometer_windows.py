a23c90abe944f0c34e6372f804b0924d1400b70d
Amos Kong <akong@redhat.com>
2013-01-20 16:16:21 +0000

    add iometer test for Windows guest
    Iometer is an I/O subsystem measurement and characterization
    tool for single and clustered systems. We use it to test block I/O
    performance of virtual machine.
    
    Need to add some util files to winutils.iso/Iometer:
     http://kongove.fedorapeople.org/autotest/Iometer.tar.gz
    
    This patch just added the testcase, I will change this case to generate
    RHS format result, then we can use regression.py to compare the results.
    
    Signed-off-by: Amos Kong <akong@redhat.com>

--- tests/iometer_windows.py
+++ tests/iometer_windows.py
@@ -1,15 +1,20 @@
+"""
+@author: Golita Yue <gyue@redhat.com>
+@author: Amos Kong <akong@redhat.com>
+"""
 import logging, time, re
 from autotest.client.shared import error
-from autotest.client.virt import virt_vm
 
 
+@error.context_aware
 def run_iometer_windows(test, params, env):
     """
-    Run Iometer for windows on a windows guest:
-    1) Boot guest with second disk
-    2) Log into a guest
-    3) Install Iometer
-    4) Execute the Iometer test contained in the winutils.iso
+    Run Iometer for Windows on a Windows guest:
+
+    1) Boot guest with additional disk
+    2) Install Iometer
+    3) Execute the Iometer test contained in the winutils.iso
+    4) Copy result to host
 
     @param test: kvm test object
     @param params: Dictionary with the test parameters
@@ -20,14 +25,17 @@ def run_iometer_windows(test, params, env):
         cdrom_chk_cmd += " cmd && diskpart /s cmd"
         vols = []
         start_time = time.time()
+
         while time.time() - start_time < timeout:
             vols_str = session.cmd(cdrom_chk_cmd)
             logging.info("vols_str is %s" % vols_str)
-            if len(re.findall("CDFS.*Healthy", vols_str)) >= 1:
-                vols = re.findall(".*CDFS.*?Healthy.*\n", vols_str)
+
+            if len(re.findall("CDFS.*CD-ROM", vols_str)) >= 1:
+                vols = re.findall(".*CDFS.*?CD-ROM.*\n", vols_str)
                 logging.info("vols is %s" % vols)
                 break
         return vols
+
 
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
@@ -35,8 +43,10 @@ def run_iometer_windows(test, params, env):
     session = vm.wait_for_login(timeout=timeout)
     cmd_timeout = int(params.get("cmd_timeout",1200))
 
-    # Create a partition on disk
+    logging.info("Sleep 120 seconds, and create a partition on second disk")
     time.sleep(120)
+
+    error.context("Creating partition")
     create_partition_cmd = params.get("create_partition_cmd")
     (s, o) = session.get_command_status_output(cmd=create_partition_cmd,
                                                 timeout=cmd_timeout)
@@ -46,6 +56,7 @@ def run_iometer_windows(test, params, env):
 
     # Format the disk
     format_cmd = params.get("format_cmd")
+    error.context("Formating second disk")
     (s, o) = session.get_command_status_output(cmd=format_cmd,
                                                 timeout=cmd_timeout)
     if s != 0:
@@ -59,6 +70,7 @@ def run_iometer_windows(test, params, env):
 
     install_iometer_cmd = params.get("iometer_installation_cmd")
     install_iometer_cmd = re.sub("WIN_UTILS", vol_utils, install_iometer_cmd)
+    error.context("Installing iometer")
     (s, o) = session.get_command_status_output(cmd=install_iometer_cmd,
                                                 timeout=cmd_timeout)
     if s !=0:
@@ -69,23 +81,26 @@ def run_iometer_windows(test, params, env):
     # Run Iometer
     cmd_reg = params.get("iometer_reg")
     cmd_run = params.get("iometer_run")
-    t = int(params.get("iometer_timeout"))
+    t = int(params.get("iometer_timeout", 1000))
     cmd_reg = re.sub("WIN_UTILS", vol_utils, cmd_reg)
     cmd_run = re.sub("WIN_UTILS", vol_utils, cmd_run)
 
-    logging.info("Register Iometer on guest, timeout %ss", cmd_timeout)
+    error.context("Registering Iometer on guest, timeout %ss" % cmd_timeout)
     (s, o) = session.cmd_status_output(cmd=cmd_reg, timeout=cmd_timeout)
     if s !=0:
         raise error.TestFail("Failed to register iometer with error: %s" % o)
     else:
         logging.info("Complete iometer register")
-    logging.info("Running Iometer command on guest, timeout %ss", cmd_timeout)
+
+    error.context("Running Iometer command on guest, timeout %ss" % cmd_timeout)
     (s, o) = session.cmd_status_output(cmd=cmd_run, timeout=t)
     if s !=0:
         raise error.TestFail("Failed to run iometer with error: %s" % o)
     else:
         logging.info("Completed iometer testing")
+
     guest_path = params.get("guest_path")
+    error.context("Copying result '%s' to host" % guest_path)
     vm.copy_files_from(guest_path,test.resultsdir)
 
     session.close()
