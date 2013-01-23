5f1995be9930cab8d03a8f5d8a9a8fafd71db26a
Igor Mammedov <imammedo@redhat.com>
2013-01-23 15:32:02 +0100

    cpuid: add simplistic kernel that dumps cpuid to serial port
    ... and test utilities to parse its output
    
    CPUID leaves are capped by reported level, xlevel, x2level values.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    ---
      v3:
        * dump CPUID(EAX=3), CPUID(EAX=4,ECX=3), CPUID(EAX={0xB, 0xC})
        * dump CPUID(EAX=0x80000007) and known paravirt cpuid leaves if
          detected
        * make target name specific to function cpuid_dump_kernel.bin,
          so we could later add another targets for other tests
      v2:
        * log cpuid dump on parsing failure
        * cap by reported level, xlevel, x2level

---
+++ shared/deps/cpuid_test_kernel/main.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright Red Hat, Inc. 2013
+ *
+ * Authors:
+ *  Igor Mammedov <imammedo@redhat.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+void printf (const char *format, ...);
+void test();
+
+#endif
