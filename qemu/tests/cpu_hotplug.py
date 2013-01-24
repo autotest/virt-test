b3f88e560633ddaade468285895a17f4af921992
Xiaoqing Wei <xwei@redhat.com>
2013-01-24 10:36:57 +0000

    qemu.tests:add sub case add max CPUs for cpu hotplug
    hotplug 159 vcpus and check vcpu number inside and reboot guest
    
    Changelog from V3:
    1) removed open/close session code.
    2) use vm.monitor.send_args_cmd to auto choose monitor
    3) use utils_misc.wait_for() to replace time.sleep()
    Changelog from V2:
    1) add all error.context with logging.info
    Changelog from V1:
    1) fixed the hardcode log
    2) make the error.context with 'logging.info' argument
    3) update the docstring
    4) add try / except to the hotplug code
    5) sync the log with test steps
    6) added 10 Secs sleep time as Win guest needs it
    
    Signed-off-by: Xiaoqing Wei <xwei@redhat.com>
    Acked-by: Feng Yang <fyang@redhat.com>
    
    Merge the old scripts into cpu_hotplug
    
    And cpu hotplug may support in qemu 1.5, so remove the manually
    set cpu online inside guest related code.
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

--- qemu/tests/cpu_hotplug.py
+++ qemu/tests/cpu_hotplug.py
@@ -1,6 +1,6 @@
 import os, logging, re
 from autotest.client.shared import error
-from virttest import utils_test
+from virttest import utils_test, utils_misc
 
 
 @error.context_aware
@@ -8,90 +8,89 @@ def run_cpu_hotplug(test, params, env):
     """
     Runs CPU hotplug test:
 
-    1) Pick up a living guest
-    2) Send the monitor command cpu_set [cpu id] for each cpu we wish to have
-    3) Verify if guest has the additional CPUs showing up under
-        /sys/devices/system/cpu
-    4) Try to bring them online by writing 1 to the 'online' file inside that dir
-    5) Run the CPU Hotplug test suite shipped with autotest inside guest
+    1) Boot the vm with -smp X,maxcpus=Y
+    2) After logged into the vm, check CPUs number
+    3) Send the monitor command cpu_set [cpu id] for each cpu we wish to have
+    4) Verify if guest has the additional CPUs showing up
+    5) reboot the vm
+    6) recheck guest get hot-pluged CPUs
+    7) Try to bring them online by writing 1 to the 'online' file inside
+       that dir(Linux guest only)
+    8) Run the CPU Hotplug test suite shipped with autotest inside guest
+       (Linux guest only)
 
     @param test: QEMU test object.
     @param params: Dictionary with test parameters.
     @param env: Dictionary with the test environment.
     """
+    error.context("boot the vm, with '-smp X,maxcpus=Y' option,"
+                  "thus allow hotplug vcpu", logging.info)
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
+
     timeout = int(params.get("login_timeout", 360))
     session = vm.wait_for_login(timeout=timeout)
 
     n_cpus_add = int(params.get("n_cpus_add", 1))
+    maxcpus = int(params.get("maxcpus", 160))
     current_cpus = int(params.get("smp", 1))
     onoff_iterations = int(params.get("onoff_iterations", 20))
-    total_cpus = current_cpus + n_cpus_add
 
-    error.context("cleaning guest dmesg before addition")
-    session.cmd("dmesg -c")
+    if n_cpus_add + current_cpus > maxcpus:
+        error.TestWarn("CPU quantity more than maxcpus, set it to maxcpus")
+        total_cpus = maxcpus
+    else:
+        total_cpus = current_cpus + n_cpus_add
 
-    error.context("Adding %d CPUs to guest" % n_cpus_add)
-    for i in range(total_cpus):
-        vm.monitor.cmd("cpu_set %s online" % i)
+    error.context("check if CPUs in guest matches qemu cmd "
+                  "before hot-plug", logging.info)
+    if not utils_misc.check_if_vm_vcpu_match(current_cpus, vm):
+        raise error.TestError("CPU quantity mismatch cmd before hotplug !")
 
-    output = vm.monitor.cmd("info cpus")
-    logging.debug("Output of info cpus:\n%s", output)
+    for i in range(current_cpus, total_cpus):
+        error.context("hot-pluging vCPU %s" % i, logging.info)
+        vm.monitor.send_args_cmd("cpu_set %s online" % i)
+
+    output = vm.monitor.send_args_cmd("info cpus")
+    logging.debug("Output of info CPUs:\n%s", output)
 
     cpu_regexp = re.compile("CPU #(\d+)")
     total_cpus_monitor = len(cpu_regexp.findall(output))
     if total_cpus_monitor != total_cpus:
-        raise error.TestFail("Monitor reports %s CPUs, when VM should have %s" %
-                             (total_cpus_monitor, total_cpus))
+        raise error.TestFail("Monitor reports %s CPUs, when VM should have"
+                             " %s" % (total_cpus_monitor, total_cpus))
+    # Windows is a little bit lazy that needs more secs to recognize.
+    error.context("hotplugging finished, let's wait a few sec and"
+                  " check CPUs quantity in guest.", logging.info)
+    if not utils_misc.wait_for(lambda: utils_misc.check_if_vm_vcpu_match(
+                                                              total_cpus, vm),
+                               60, first=10, step=5.0, text="retry later"):
+        raise error.TestFail("CPU quantity mismatch cmd after hotplug !")
+    error.context("rebooting the vm and check CPU quantity !", logging.info)
+    session = vm.reboot()
+    if not utils_misc.check_if_vm_vcpu_match(total_cpus, vm):
+        raise error.TestFail("CPU quantity mismatch cmd after hotplug "
+                             "and reboot !")
 
-    dmesg_after = session.cmd("dmesg")
-    logging.debug("Guest dmesg output after CPU add:\n%s" % dmesg_after)
-
-    # Verify whether the new cpus are showing up on /sys
-    error.context("verifying if new CPUs are showing on guest's /sys dir")
-    n_cmd = 'find /sys/devices/system/cpu/cpu[0-99] -maxdepth 0 -type d | wc -l'
-    output = session.cmd(n_cmd)
-    logging.debug("List of cpus on /sys:\n%s" % output)
-    try:
-        cpus_after_addition = int(output)
-    except ValueError:
-        logging.error("Output of '%s': %s", n_cmd, output)
-        raise error.TestFail("Unable to get CPU count after CPU addition")
-
-    if cpus_after_addition != total_cpus:
-        raise error.TestFail("%s CPUs are showing up under "
-                             "/sys/devices/system/cpu, was expecting %s" %
-                             (cpus_after_addition, total_cpus))
+    # Window guest doesn't support online/offline test
+    if params['os_type'] == "windows":
+        return
 
     error.context("locating online files for guest's new CPUs")
-    r_cmd = 'find /sys/devices/system/cpu/cpu[1-99]/online -maxdepth 0 -type f'
+    r_cmd = 'find /sys/devices/system/cpu/cpu*/online -maxdepth 0 -type f'
     online_files = session.cmd(r_cmd)
+    # Sometimes the return value include command line itself
+    if "find" in online_files:
+        online_files = " ".join(online_files.strip().split("\n")[1:])
     logging.debug("CPU online files detected: %s", online_files)
-    online_files = online_files.split().sort()
+    online_files = online_files.split()
+    online_files.sort()
 
     if not online_files:
         raise error.TestFail("Could not find CPUs that can be "
                              "enabled/disabled on guest")
 
-    for online_file in online_files:
-        cpu_regexp = re.compile("cpu(\d+)", re.IGNORECASE)
-        cpu_id = cpu_regexp.findall(online_file)[0]
-        error.context("changing online status for CPU %s" % cpu_id)
-        check_online_status = session.cmd("cat %s" % online_file)
-        try:
-            check_online_status = int(check_online_status)
-        except ValueError:
-            raise error.TestFail("Unable to get online status from CPU %s" %
-                                 cpu_id)
-        assert(check_online_status in [0, 1])
-        if check_online_status == 0:
-            error.context("Bringing CPU %s online" % cpu_id)
-            session.cmd("echo 1 > %s" % online_file)
-
-    # Now that all CPUs were onlined, let's execute the
-    # autotest CPU Hotplug test
-    control_path = os.path.join(test.virtdir, "autotest_control",
+    control_path = os.path.join(test.virtdir, "control",
                                 "cpu_hotplug.control")
 
     timeout = int(params.get("cpu_hotplug_timeout", 300))
