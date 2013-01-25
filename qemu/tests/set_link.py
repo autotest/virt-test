c7109e331dba5e351798d54c6adc4db47875a4ae
Feng Yang <fyang@redhat.com>
2013-01-25 11:03:22 +0000

    set_link.py: Bug fix and add error.context
    Now vm do not have attribute 'netdev_id'. Change to use
    vm.virtnet[0].netdev_id.
    Waiting for guest network up again after set_link up.
    
    Also add error.context.
    
    Signed-off-by: Feng Yang <fyang@redhat.com>

--- qemu/tests/set_link.py
+++ qemu/tests/set_link.py
@@ -2,15 +2,21 @@ import logging
 from autotest.client.shared import error
 from virttest import utils_test
 
-
+@error.context_aware
 def run_set_link(test, params, env):
     """
     KVM guest link test:
     1) Boot up guest with one nic
-    2) Ping guest from host
-    3) Disable guest link and ping guest from host
-    4) Re-enable guest link and ping guest from host
-    5) Do file transfer test
+    2) Disable guest link by set_link
+    3) Ping guest from host
+    4) Re-enable guest link by set_link
+    5) Ping guest from host
+    6) Call utils_test.run_file_transfer function to test file transfer.
+       It will do following steps:
+       6.1) Create a large file by dd on host.
+       6.2) Copy this file from host to guest.
+       6.3) Copy this file from guest to host.
+       6.4) Check if file transfers ended good.
 
     @param test: kvm test object
     @param params: Dictionary with the test parameters
@@ -18,7 +24,10 @@ def run_set_link(test, params, env):
     """
     vm = utils_test.get_living_vm(env, params.get("main_vm"))
     timeout = float(params.get("login_timeout", 360))
-    session = utils_test.wait_for_login(vm, 0, timeout, 0, 2)
+    # Waiting for guest boot up.
+    session = vm.wait_for_login(timeout=timeout)
+    session.close()
+
 
     def set_link_test(linkid):
         """
@@ -27,27 +36,32 @@ def run_set_link(test, params, env):
         @param linkid: id of netdev or devices to be tested
         """
         ip = vm.get_address(0)
-
+        error.context("Disable guest link by set_link", logging.info)
         vm.set_link(linkid, up=False)
-        _, o = utils_test.ping(ip, count=10, timeout=20)
+        error.context("Ping guest from host", logging.info)
+        s, o = utils_test.ping(ip, count=10, timeout=20)
         if utils_test.get_loss_ratio(o) != 100:
             raise error.TestFail("Still can ping the %s after down %s" %
                                  (ip, linkid))
 
+        error.context("Re-enable guest link by set_link", logging.info)
         vm.set_link(linkid, up=True)
-        _, o = utils_test.ping(ip, count=10, timeout=20)
+        # Waiting for guest network up again.
+        session = vm.wait_for_login(timeout=timeout)
+        session.close()
+        error.context("Ping guest from host", logging.info)
+        s, o = utils_test.ping(ip, count=10, timeout=20)
         # we use 100% here as the notification of link status changed may be
         # delayed in guest driver
         if utils_test.get_loss_ratio(o) == 100:
             raise error.TestFail("Packet loss during ping %s after up %s" %
                                  (ip, linkid))
 
-    netdev_id = vm.netdev_id[0]
-    device_id = vm.get_peer(netdev_id)
+    netdev_id = vm.virtnet[0].netdev_id
+    device_id = vm.virtnet[0].device_id
     logging.info("Issue set_link commands for netdevs")
     set_link_test(netdev_id)
     logging.info("Issue set_link commands for network devics")
     set_link_test(device_id)
 
     utils_test.run_file_transfer(test, params, env)
-    session.close()
