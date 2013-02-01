1eb5631850f5c2228a04239d14cc795ed5415773
Yunping Zheng <yunzheng@redhat.com>
2013-02-01 10:52:42 +0000

    KVM TEST : Win guest unattended_install support latest driver
    This patch modify win8 and win2012 using their own driver.
    This patch also make windows unattended_install support
    scsi disk and scsi cdrom.
    
    
    Signed-off-by: Yunping Zheng <yunzheng@redhat.com>
    Acked-by: Suqin Huang <shuang@redhat.com>

--- tests/unattended_install.py
+++ tests/unattended_install.py
@@ -373,9 +373,15 @@ class UnattendedInstallConfig(object):
         if self.install_virtio == 'yes':
             v_attributes = ['virtio_floppy', 'virtio_storage_path',
                             'virtio_network_path', 'virtio_oemsetup_id',
-                            'virtio_network_installer']
-            for va in v_attributes:
-                setattr(self, va, params.get(va, ''))
+                            'virtio_network_installer_path',
+                            'virtio_balloon_installer_path',
+                            'virtio_qxl_installer_path',
+                            'virtio_scsi_cdrom']
+        else:
+            v_attributes = ['virtio_balloon_installer_path',
+                            'virtio_qxl_installer_path']
+        for va in v_attributes:
+            setattr(self, va, params.get(va, ''))
 
         self.tmpdir = test.tmpdir
 
@@ -546,6 +552,8 @@ class UnattendedInstallConfig(object):
         # component PnpCustomizationsWinPE Element Node
         if self.install_virtio == 'yes':
             paths = doc.getElementsByTagName("Path")
+            if self.virtio_scsi_cdrom == 'yes':
+                self.virtio_network_path = self.virtio_storage_path
             values = [self.virtio_storage_path, self.virtio_network_path]
             for path, value in zip(paths, values):
                 path_text = path.childNodes[0]
