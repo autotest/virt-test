3b3ddb00df2b5ec870ba4bc29098cbd2f637d27f
Amos Kong <akong@redhat.com>
2013-02-28 11:19:02 +0000

    netperf: introduce an agent to launch client sessions
    Currently we use many threads/heavy control sessions to launch many
    clients, Autotest takes too much host resouse.
    
    This patch introduced an agent script, we execute it insider client
    system to launch multiple sessions, and Autotest only need to use one
    background thread to control clients.
    
    We can consider to process the raw results in this agent script in
    future, if it can help to simplify the case.
    
    Signed-off-by: Amos Kong <akong@redhat.com>

---
+++ shared/scripts/netperf_agent.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+@author Amos Kong <akong@redhat.com>
+
+"""
+import os, sys, time
+
+if len(sys.argv) < 4:
+    print """ netperf agent usage:
+    %s [session_number] [netperf_path] [netperf_parameters_str]
+
+    $session_number: number of client sessions
+    $netperf_path: client path
+    $netperf_parameter_str: netperf parameters string""" % sys.argv[0]
+    sys.exit()
+
+n = int(sys.argv[1])
+path = sys.argv[2]
+params = " ".join(sys.argv[3:])
+
+for i in range(n - 1):
+    os.system("%s %s &" % (path, params))
+os.system("%s %s" % (path, params))
