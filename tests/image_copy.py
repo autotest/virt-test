8237acc680e7625a2fe4f9825bb94a81e0096c48
Paolo Bonzini <pbonzini@redhat.com>
2013-02-28 03:25:38 +0100

    image_copy: use assets as the source
    Use the asset library to download images if the images_good parameter
    is not set.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- tests/image_copy.py
+++ tests/image_copy.py
@@ -1,7 +1,7 @@
 import os, logging
 from autotest.client.shared import error
 from autotest.client import utils
-from virttest import utils_misc, data_dir, utils_test
+from virttest import utils_misc, data_dir, utils_test, asset
 
 
 def run_image_copy(test, params, env):
@@ -19,22 +19,10 @@ def run_image_copy(test, params, env):
     if vm is not None:
         vm.destroy()
 
-    mount_dest_dir = params.get('dst_dir', '/mnt/images')
-    if not os.path.exists(mount_dest_dir):
-        try:
-            os.makedirs(mount_dest_dir)
-        except OSError, err:
-            logging.warning('mkdir %s error:\n%s', mount_dest_dir, err)
-
-    if not os.path.exists(mount_dest_dir):
-        raise error.TestError('Failed to create NFS share dir %s' %
-                              mount_dest_dir)
-
     src = params.get('images_good')
-    image = '%s.%s' % (os.path.split(params['image_name'])[1],
-                       params['image_format'])
-    src_path = os.path.join(mount_dest_dir, image)
-    dst_path = '%s/%s.%s' % (data_dir.get_data_dir(), params['image_name'], params['image_format'])
+    asset_name = '%s' % (os.path.split(params['image_name'])[1])
+    image = '%s.%s' % (params['image_name'], params['image_format'])
+    dst_path = '%s/%s' % (data_dir.get_data_dir(), image)
     pwd = os.path.join(test.bindir, "images")
     if params.get("rename_error_image", "no") == "yes":
         error_image = os.path.basename(params['image_name']) + "-error"
@@ -43,27 +31,32 @@ def run_image_copy(test, params, env):
         mv_cmd = "/bin/mv %s %s" % (dst_path, error_dst_path)
         utils.system(mv_cmd, timeout=360, ignore_status=True)
 
-    try:
+    if src:
+        mount_dest_dir = params.get('dst_dir', '/mnt/images')
+        if not os.path.exists(mount_dest_dir):
+            try:
+                os.makedirs(mount_dest_dir)
+            except OSError, err:
+                logging.warning('mkdir %s error:\n%s', mount_dest_dir, err)
+
+        if not os.path.exists(mount_dest_dir):
+            raise error.TestError('Failed to create NFS share dir %s' %
+                                  mount_dest_dir)
+
         error.context("Mount the NFS share directory")
         if not utils_misc.mount(src, mount_dest_dir, 'nfs', 'ro'):
             raise error.TestError('Could not mount NFS share %s to %s' %
                                   (src, mount_dest_dir))
+        src_path = '%s/%s.%s' % (mount_dest_dir, asset_name, params['image_format'])
+        asset_info = asset.get_file_asset(asset_name, src_path, dst_path)
+        if asset_info is None:
+            raise error.TestError('Could not find %s' % image)
+    else:
+        asset_info = asset.get_asset_info(asset_name)
 
-        # Check the existence of source image
-        error.context("Check the existence of source image")
-        if os.path.exists("%s.xz" % src_path):
-            logging.debug('Copying image %s (from xz) ...', image)
-            cmd = "xz -cd %s.xz > %s" % (src_path, dst_path)
-        elif os.path.exists("%s.gz" % src_path):
-            logging.debug('Copying image %s (from gzip) ...', image)
-            cmd = "gzip -cd %s.gz > %s" % (src_path, dst_path)
-        elif os.path.exists(src_path):
-            logging.debug('Copying image %s (uncompressed) ...', image)
-            cmd = 'cp %s %s' % (src_path, dst_path)
-        else:
-            raise error.TestError('Could not find %s in NFS share' % src_path)
+    try:
+        asset.download_file(asset_info, interactive=False, force=True)
 
-        utils.system(cmd)
     finally:
         if params.get("sub_type"):
             params['image_name'] += "-error"
