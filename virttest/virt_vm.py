8523e8fcfe7b9fccea5a5a21ed82ad1342b4d4c5
Lucas Meneghel Rodrigues <lmr@redhat.com>
2013-03-14 00:58:31 -0300

    virt_vm: Plug some more fd leaks
    Close log files that are opened one time, such as the
    ones opened for file transfers. This helps to reduce
    even further the amount of files opened after a given
    test job, specially if it involves a lot of file transfers.
    
    With this patch, this test set:
    
    ./run -t qemu -g RHEL.6.3.x86_64 --nettype bridge --tests "migrate.with_autotest"
    
    Comes from 94 files opened at the end to 43.
    
    CC: Lei Yang <yanglei.fage@gmail.com>
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- virttest/virt_vm.py
+++ virttest/virt_vm.py
@@ -889,6 +889,7 @@ class BaseVM(object):
         remote.copy_files_to(address, client, username, password, port,
                                   host_path, guest_path, limit, log_filename,
                                   verbose, timeout)
+        utils_misc.close_log_file(log_filename)
 
 
     @error.context_aware
@@ -920,6 +921,7 @@ class BaseVM(object):
         remote.copy_files_from(address, client, username, password, port,
                                     guest_path, host_path, limit, log_filename,
                                     verbose, timeout)
+        utils_misc.close_log_file(log_filename)
 
 
     @error.context_aware
