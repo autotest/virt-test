e61a063451d61319e066a32ade5a75db161bf347
Yiqiao Pu <ypu@redhat.com>
2013-03-14 21:26:02 +0000

    qemu.tests: Use funcatexit to handle the clean up in transparent hugepage test
    Use funcatexit to handle the debugfs clean up in transparent hugepage test
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

--- tests/trans_hugepage.py
+++ tests/trans_hugepage.py
@@ -1,7 +1,18 @@
 import logging, os, re
 from autotest.client.shared import error
 from autotest.client.shared import utils
-from virttest import utils_test
+from virttest import utils_test, funcatexit
+
+
+def cleanup(debugfs_path, session):
+    """
+    Umount the debugfs and close the session
+    """
+    if os.path.ismount(debugfs_path):
+        utils.run("umount %s" % debugfs_path)
+    if os.path.isdir(debugfs_path):
+        os.removedirs(debugfs_path)
+    session.close()
 
 
 @error.context_aware
@@ -45,76 +56,70 @@ def run_trans_hugepage(test, params, env):
     vm = utils_test.get_living_vm(env, params.get("main_vm"))
     session = utils_test.wait_for_login(vm, timeout=login_timeout)
 
+    funcatexit.register(env, params.get("type"), cleanup, debugfs_path,
+                        session)
+
+    logging.info("Smoke test start")
+    error.context("smoke test")
+
+    nr_ah_before = int(get_mem_status('AnonHugePages', 'host'))
+    if nr_ah_before <= 0:
+        e_msg = 'smoke: Host is not using THP'
+        logging.error(e_msg)
+        failures.append(e_msg)
+
+    # Protect system from oom killer
+    if int(get_mem_status('MemFree', 'guest')) / 1024 < mem :
+        mem = int(get_mem_status('MemFree', 'guest')) / 1024
+
+    session.cmd("mkdir -p %s" % mem_path)
+
+    session.cmd("mount -t tmpfs -o size=%sM none %s" % (str(mem), mem_path))
+
+    count = mem / 4
+    session.cmd("dd if=/dev/zero of=%s/1 bs=4000000 count=%s" %
+                (mem_path, count), timeout=dd_timeout)
+
+    nr_ah_after = int(get_mem_status('AnonHugePages', 'host'))
+
+    if nr_ah_after <= nr_ah_before:
+        e_msg = ('smoke: Host did not use new THP during dd')
+        logging.error(e_msg)
+        failures.append(e_msg)
+
+    if debugfs_flag == 1:
+        if int(open('%s/kvm/largepages' % debugfs_path, 'r').read()) <= 0:
+            e_msg = 'smoke: KVM is not using THP'
+            logging.error(e_msg)
+            failures.append(e_msg)
+
+    logging.info("Smoke test finished")
+
+    # Use parallel dd as stress for memory
+    count = count / 3
+    logging.info("Stress test start")
+    error.context("stress test")
+    cmd = "rm -rf %s/*; for i in `seq %s`; do dd " % (mem_path, count)
+    cmd += "if=/dev/zero of=%s/$i bs=4000000 count=1& done;wait" % mem_path
+    output = session.cmd_output(cmd, timeout=dd_timeout)
+
+    if len(re.findall("No space", output)) > count * 0.05:
+        e_msg = "stress: Too many dd instances failed in guest"
+        logging.error(e_msg)
+        failures.append(e_msg)
+
     try:
-        logging.info("Smoke test start")
-        error.context("smoke test")
+        output = session.cmd('pidof dd')
+    except Exception:
+        output = None
 
-        nr_ah_before = int(get_mem_status('AnonHugePages', 'host'))
-        if nr_ah_before <= 0:
-            e_msg = 'smoke: Host is not using THP'
-            logging.error(e_msg)
-            failures.append(e_msg)
+    if output is not None:
+        for i in re.split('\n+', output):
+            session.cmd('kill -9 %s' % i)
 
-        # Protect system from oom killer
-        if int(get_mem_status('MemFree', 'guest')) / 1024 < mem :
-            mem = int(get_mem_status('MemFree', 'guest')) / 1024
+    session.cmd("umount %s" % mem_path)
 
-        session.cmd("mkdir -p %s" % mem_path)
-
-        session.cmd("mount -t tmpfs -o size=%sM none %s" % (str(mem), mem_path))
-
-        count = mem / 4
-        session.cmd("dd if=/dev/zero of=%s/1 bs=4000000 count=%s" %
-                    (mem_path, count), timeout=dd_timeout)
-
-        nr_ah_after = int(get_mem_status('AnonHugePages', 'host'))
-
-        if nr_ah_after <= nr_ah_before:
-            e_msg = ('smoke: Host did not use new THP during dd')
-            logging.error(e_msg)
-            failures.append(e_msg)
-
-        if debugfs_flag == 1:
-            if int(open('%s/kvm/largepages' % debugfs_path, 'r').read()) <= 0:
-                e_msg = 'smoke: KVM is not using THP'
-                logging.error(e_msg)
-                failures.append(e_msg)
-
-        logging.info("Smoke test finished")
-
-        # Use parallel dd as stress for memory
-        count = count / 3
-        logging.info("Stress test start")
-        error.context("stress test")
-        cmd = "rm -rf %s/*; for i in `seq %s`; do dd " % (mem_path, count)
-        cmd += "if=/dev/zero of=%s/$i bs=4000000 count=1& done;wait" % mem_path
-        output = session.cmd_output(cmd, timeout=dd_timeout)
-
-        if len(re.findall("No space", output)) > count * 0.05:
-            e_msg = "stress: Too many dd instances failed in guest"
-            logging.error(e_msg)
-            failures.append(e_msg)
-
-        try:
-            output = session.cmd('pidof dd')
-        except Exception:
-            output = None
-
-        if output is not None:
-            for i in re.split('\n+', output):
-                session.cmd('kill -9 %s' % i)
-
-        session.cmd("umount %s" % mem_path)
-
-        logging.info("Stress test finished")
-
-    finally:
-        error.context("all tests cleanup")
-        if os.path.ismount(debugfs_path):
-            utils.run("umount %s" % debugfs_path)
-        if os.path.isdir(debugfs_path):
-            os.removedirs(debugfs_path)
-        session.close()
+    logging.info("Stress test finished")
 
     error.context("")
     if failures:
