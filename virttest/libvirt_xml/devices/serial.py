5bb167d2e10666f09a38f4361351a20cb0f9c08b
Chris Evich <cevich@redhat.com>
2013-03-15 13:54:12 -0400

    fixup! virt: Improve vm_xml device support + unittests
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- virttest/libvirt_xml/devices/serial.py
+++ virttest/libvirt_xml/devices/serial.py
@@ -10,12 +10,12 @@ class Serial(base.TypedDeviceBase):
 
     __slots__ = base.TypedDeviceBase.__slots__ + ('source_path', 'target_port')
 
-    def __init__(self, virsh_instance=base.virsh):
+    def __init__(self, type_name='pty', virsh_instance=base.base.virsh):
         base.accessors.XMLAttribute('source_path', self, parent_xpath='/',
                                     tag_name='source', attribute='path')
         base.accessors.XMLAttribute('target_port', self, parent_xpath='/',
                                     tag_name='target', attribute='port')
         #TODO: Support 'target_type' and 'target_address'
         #      These need 'address' device/module added
-        super(Serial, self).__init__(device_tag='serial', type_name='pty',
-                                    virsh_instance=virsh_instance)
+        super(Serial, self).__init__(device_tag='serial', type_name=type_name,
+                                     virsh_instance=virsh_instance)
