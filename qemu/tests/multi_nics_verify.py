33ccbee1b51bc2a5e3483f2d57a4d58da00b032f
Feng Yang <fyang@redhat.com>
2013-04-02 08:33:03 +0000

    KVM Test: Add error.context() for multi_nics_verify.py
    Correct utils_test import link
    Move sub function to top of code.
    add error.context support.
    
    
    Signed-off-by: Feng Yang <fyang@redhat.com>
    Acked-by: Qingtang Zhou <qzhou@redhat.com>

--- qemu/tests/multi_nics_verify.py
+++ qemu/tests/multi_nics_verify.py
@@ -3,28 +3,23 @@ from autotest.client.shared import error
 from virttest import utils_test
 
 
+@error.context_aware
 def run_multi_nics_verify(test, params, env):
     """
     Verify guest NIC numbers again whats provided in test config file.
 
     If the guest NICs info does not match whats in the params at first,
     try to fix these by operating the networking config file.
+    1. Boot guest with multi NICs.
+    2. Check whether guest NICs info match with params setting.
+    3. Create configure file for every NIC interface in guest.
+    4. Reboot guest.
+    5. Check whether guest NICs info match with params setting.
 
     @param test: QEMU test object
     @param params: Dictionary with the test parameters
     @param env: Dictionary with test environment.
     """
-    vm = env.get_vm(params["main_vm"])
-    vm.verify_alive()
-    session = vm.wait_for_login(timeout=int(params.get("login_timeout", 360)))
-    # Redirect ifconfig output from guest to log file
-    log_file = os.path.join(test.debugdir, "ifconfig")
-    utils_test.dump_command_output(session, "ifconfig", log_file)
-
-    # Get the ethernet cards number from params
-    nics_num = len(params.objects("nics"))
-    logging.info("[ %s ] NICs card specified in config file" % nics_num)
-
     # A helper function for getting NICs counts from ifconfig output of guest
     def get_nics_list(session):
         s, o = session.get_command_status_output("ifconfig")
@@ -39,8 +34,11 @@ def run_multi_nics_verify(test, params, env):
 
         return nics_list
 
+
     # A helper function for checking NICs number
     def check_nics_num(expect_c, session):
+        txt = "Check whether guest NICs info match with params setting."
+        error.context(txt, logging.info) 
         nics_list = get_nics_list(session)
         actual_c = len(nics_list)
         msg = "Expect NICs nums are: %d\nPractical NICs nums are: %d\n" % \
@@ -51,9 +49,22 @@ def run_multi_nics_verify(test, params, env):
             return (False, msg)
         return (True, msg+'Nics count match')
 
+
+    vm = env.get_vm(params["main_vm"])
+    vm.verify_alive()
+    session = vm.wait_for_login(timeout=int(params.get("login_timeout", 360)))
+    # Redirect ifconfig output from guest to log file
+    log_file = os.path.join(test.debugdir, "ifconfig")
+    utils_test.dump_command_output(session, "ifconfig", log_file)
+
+    # Get the ethernet cards number from params
+    nics_num = len(params.objects("nics"))
+    logging.info("[ %s ] NICs card specified in config file" % nics_num)
+
     # Pre-judgement for the ethernet interface
     logging.debug(check_nics_num(nics_num, session)[1])
-
+    txt = "Create configure file for every NIC interface in guest."
+    error.context(txt, logging.info)
     ifcfg_prefix = "/etc/sysconfig/network-scripts/ifcfg-eth"
     for num in range(nics_num):
         eth_config_path = "".join([ifcfg_prefix, str(num)])
