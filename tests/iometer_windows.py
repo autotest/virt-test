e8fa391284c5b6661ac720470c091da82d8179e2
tian xu <xutian@redhat.com>
2013-04-18 15:34:06 +0000

    KVM Test: enhanced iometer_windows test script
    Windows guest used different letters for cdrom in different guest, this
    commit use 'wmic' to guest cdrom device letter to avoid directory not
    empty issue;
    Iometers run in batch mode with special configure file in Winutils.iso;
    when target volume ID changed, configure file need to update too, else
    test may run on incorrect target device; This commit, reformat configure
    file with new volume ID to ensure test run on correct target volume;
    And fixed some coding style issue;
    
    changes from v1:
        removed duplicate bug ID in git log
    changes from v2:
        fix coding style issue
        raise error if no available letters for new volume;
    changes from v3:
        rename get_cdrom to get_drive to make it more meaningful
    changes from v4:
        reformat patch
    changes from v5:
        fix typo in commit log
    
    
    Signed-off-by: Xu Tian <xutian@redhat.com>

--- tests/iometer_windows.py
+++ tests/iometer_windows.py
@@ -2,7 +2,7 @@
 @author: Golita Yue <gyue@redhat.com>
 @author: Amos Kong <akong@redhat.com>
 """
-import logging, time, re
+import string, logging, time, os, re
 from autotest.client.shared import error
 
 
@@ -20,87 +20,123 @@ def run_iometer_windows(test, params, env):
     @param params: Dictionary with the test parameters
     @param env: Dictionary with test environment.
     """
-    def check_cdrom(timeout):
-        cdrom_chk_cmd = "echo list volume > cmd && echo exit >>"
-        cdrom_chk_cmd += " cmd && diskpart /s cmd"
-        vols = []
-        start_time = time.time()
+    def get_used_ids(session):
+        """
+        return used device letters;
+        """
+        cmd = "wmic logicaldisk get DeviceID"
+        output = session.cmd(cmd, timeout=360)
+        device_ids = re.findall(r'([a-zA-Z]):', output, re.M)
+        if device_ids:
+            device_ids = map(string.upper, device_ids)
+            return set(device_ids)
+        return set()
 
-        while time.time() - start_time < timeout:
-            vols_str = session.cmd(cdrom_chk_cmd)
-            logging.info("vols_str is %s" % vols_str)
 
-            if len(re.findall("CDFS.*CD-ROM", vols_str)) >= 1:
-                vols = re.findall(".*CDFS.*?CD-ROM.*\n", vols_str)
-                logging.info("vols is %s" % vols)
-                break
-        return vols
+    def get_drive(session):
+        """
+        return drive letter of WIN_UTILS drive;
+        """
+        cmd = "wmic datafile where \"FileName='software_install_64' and "
+        cmd += "extension='bat'\" get drive"
+        info = session.cmd(cmd, timeout=600)
+        device = re.search(r'(\w):', info, re.M)
+        if not device:
+            raise error.TestError("WIN_UTILS drive not found...")
+        device = device.group(1)
+        return device.upper()
+
+
+    def get_free_letter(session):
+        """
+        return free letter which can use for new drive;
+        """
+        full_ids = set(string.uppercase)
+        black_list = set (["A", "B", "C"])
+        used_ids = get_used_ids(session)
+        free_ids = (full_ids - used_ids - black_list)
+        if free_ids:
+            return free_ids.pop()
+        else:
+            raise error.TestError("No avaliable letter can use to new volume")
+
+
+    def format_iometer_cfg(session, path, device_id="E"):
+        """
+        reformat Iometer cfg file;
+        """
+        tmp_path = "/tmp/iometer_windows.cfg"
+        vm.copy_files_from(path, tmp_path)
+        if not os.path.exists(tmp_path):
+            raise error.TestError("Iometer configure file not found")
+        cfg = ""
+        with open(tmp_path, "r") as cfg_fd:
+            cfg = cfg_fd.readlines()
+            cfg = "".join(cfg)
+            cfg = re.sub(r"[a-zA-Z]:local", "%s:local" % device_id, cfg)
+        with open(tmp_path, "w") as cfg_fd:
+            cfg_fd.write(cfg)
+        vm.copy_files_to(tmp_path, path)
 
 
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
     timeout = int(params.get("login_timeout", 360))
     session = vm.wait_for_login(timeout=timeout)
-    cmd_timeout = int(params.get("cmd_timeout",1200))
 
-    logging.info("Sleep 120 seconds, and create a partition on second disk")
-    time.sleep(120)
+    # perpare test params
+    cmd_timeout = int(params.get("cmd_timeout", 1200))
+    sleep_before_test = int(params.get("sleep_before_test", 0))
+    iometer_timeout = int(params.get("iometer_timeout", 1200))
 
-    error.context("Creating partition")
+    device_id = get_free_letter(session)
+    cdrom = get_drive(session)
     create_partition_cmd = params.get("create_partition_cmd")
-    (s, o) = session.get_command_status_output(cmd=create_partition_cmd,
-                                                timeout=cmd_timeout)
-    if s != 0:
-        raise error.TestFail("Failed to create partition with error: %s" % o)
-    logging.info("Output of command of create partition on disk: %s" % o)
-
-    # Format the disk
     format_cmd = params.get("format_cmd")
-    error.context("Formating second disk")
-    (s, o) = session.get_command_status_output(cmd=format_cmd,
-                                                timeout=cmd_timeout)
-    if s != 0:
-        raise error.TestFail("Failed to format with error: %s" % o)
-    logging.info("Output of format disk command: %s" % o)
-
-    # Install Iometer
-    init_timeout = int(params.get("init_timeout", "60"))
-    volumes = check_cdrom(init_timeout)
-    vol_utils = re.findall("Volume\s+\d+\s+(\w).*?\d+\s+\w+", volumes[0])[0]
-
     install_iometer_cmd = params.get("iometer_installation_cmd")
-    install_iometer_cmd = re.sub("WIN_UTILS", vol_utils, install_iometer_cmd)
-    error.context("Installing iometer")
+    install_iometer_cmd = re.sub("WIN_UTILS", cdrom, install_iometer_cmd)
+    create_partition_cmd = re.sub("DEVICEID", device_id, create_partition_cmd)
+    format_cmd = re.sub("DEVICEID", device_id , format_cmd)
+    cmd_reg = params.get("iometer_reg")
+    iometer_cfg = params.get("iometer_cfg")
+    guest_path = params.get("guest_path")
+    cmd_run = params.get("iometer_run") % (iometer_cfg, guest_path)
+
+    if sleep_before_test:
+        logging.info("sleep %ss, wait vds service startup", sleep_before_test)
+        time.sleep(sleep_before_test)
+
+    error.context("Format data disk for Iometer test", logging.info)
+    logging.debug("Create partition cmd: %s" % create_partition_cmd)
+    (s, o) = session.get_command_status_output(cmd=create_partition_cmd,
+                                               timeout=cmd_timeout)
+    if s != 0:
+        raise error.TestFail("Create partition failed with error: %s" % o)
+
+    logging.debug("Format partition cmd: %s" % format_cmd)
+    (s, o) = session.get_command_status_output(cmd=format_cmd,
+                                               timeout=cmd_timeout)
+    if s != 0:
+        raise error.TestFail("Format disk failed with error: %s" % o)
+
+    error.context("install iometer app", logging.info)
     (s, o) = session.get_command_status_output(cmd=install_iometer_cmd,
                                                 timeout=cmd_timeout)
-    if s !=0:
-        raise error.TestFail("Failed to install iometer with error: %s" % o)
-    else:
-        logging.info("Complete installation of iometer")
+    if s != 0:
+        raise error.TestFail("Install iometer failed with error: %s" % o)
 
-    # Run Iometer
-    cmd_reg = params.get("iometer_reg")
-    cmd_run = params.get("iometer_run")
-    t = int(params.get("iometer_timeout", 1000))
-    cmd_reg = re.sub("WIN_UTILS", vol_utils, cmd_reg)
-    cmd_run = re.sub("WIN_UTILS", vol_utils, cmd_run)
-
-    error.context("Registering Iometer on guest, timeout %ss" % cmd_timeout)
     (s, o) = session.cmd_status_output(cmd=cmd_reg, timeout=cmd_timeout)
-    if s !=0:
-        raise error.TestFail("Failed to register iometer with error: %s" % o)
-    else:
-        logging.info("Complete iometer register")
+    if s != 0:
+        raise error.TestFail("Register iometer failed with error: %s" % o)
 
-    error.context("Running Iometer command on guest, timeout %ss" % cmd_timeout)
-    (s, o) = session.cmd_status_output(cmd=cmd_run, timeout=t)
-    if s !=0:
-        raise error.TestFail("Failed to run iometer with error: %s" % o)
-    else:
-        logging.info("Completed iometer testing")
-
+    error.context("start iometer app", logging.info)
+    format_iometer_cfg(session, iometer_cfg, device_id)
+    logging.debug("start iometer cmd: %s" % cmd_run)
+    (s, o) = session.cmd_status_output(cmd=cmd_run, timeout=iometer_timeout)
+    if s != 0:
+        raise error.TestFail("Run iometer failed with error: %s" % o)
     guest_path = params.get("guest_path")
-    error.context("Copying result '%s' to host" % guest_path)
-    vm.copy_files_from(guest_path,test.resultsdir)
-
-    session.close()
+    logging.info("Iometer test done, copy result to %s", test.resultsdir)
+    vm.copy_files_from(guest_path, test.resultsdir)
+    if session:
+        session.close()
