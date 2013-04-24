82df2a12236c9c931cb89a9e3ccef18a53fde654
Yiqiao Pu <ypu@redhat.com>
2013-04-24 19:54:37 +0000

    virttest.bootstrap.py: Correct number_variants in get_directory_structure
    The old method of caculate number_variants will give a wrong number if
    there is more than one sub directory in a cfg directory and each sub directory
    has different depth.
    
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>
    Acked-by: Feng Yang <fyang@redhat.com>
    Acked-by: Xiaoqing Wei <xwei@redhat.com>

--- virttest/bootstrap.py
+++ virttest/bootstrap.py
@@ -133,7 +133,7 @@ def get_directory_structure(rootdir, guest_file):
                              (4 * (indent + number_variants - 1) * " "))
             number_variants += 1
         elif indent < previous_indent:
-            number_variants -= 1
+            number_variants = indent
         indent += number_variants
         try:
             base_folder = folders[-1]
