badbf2e64557fd479140756cbe333414287ddd3e
Yunping Zheng <yunzheng@redhat.com>
2013-04-26 14:26:44 +0800

    qemu : add mac_change support windows
    This patch make mac_change case support windows guest, except WinXP

--- tests/mac_change.py
+++ tests/mac_change.py
@@ -1,8 +1,8 @@
-import logging
+import logging, re
 from autotest.client.shared import error
-from virttest import utils_misc, utils_test
+from virttest import utils_misc, utils_net
 
-
+@error.context_aware
 def run_mac_change(test, params, env):
     """
     Change MAC address of guest.
@@ -27,34 +27,71 @@ def run_mac_change(test, params, env):
         new_mac = vm.virtnet.generate_mac_address(0)
         if old_mac != new_mac:
             break
+
+    os_type = params.get("os_type")
+    change_cmd_pattern = params.get("change_cmd")
+
     logging.info("The initial MAC address is %s", old_mac)
-    interface = utils_test.get_linux_ifname(session_serial, old_mac)
+    if os_type == "linux":
+        interface = utils_net.get_linux_ifname(session_serial, old_mac)
+    else:
+        connection_id = utils_net.get_windows_nic_attribute(session,
+                                                             "macaddress",
+                                                             old_mac,
+                                                             "netconnectionid")
+        nic_index = utils_net.get_windows_nic_attribute(session,
+                                                         "netconnectionid",
+                                                         connection_id,
+                                                         "index")
+
     # Start change MAC address
-    logging.info("Changing MAC address to %s", new_mac)
-    change_cmd = ("ifconfig %s down && ifconfig %s hw ether %s && "
-                  "ifconfig %s up" % (interface, interface, new_mac, interface))
-    session_serial.cmd(change_cmd)
+    error.context("Changing MAC address to %s" % new_mac, logging.info)
+    if os_type == "linux":
+        change_cmd = change_cmd_pattern % (interface,
+                                           interface, new_mac, interface)
+    else:
+        change_cmd = change_cmd_pattern % (int(nic_index),
+                                           "".join(new_mac.split(":")))
+    try:
+        session_serial.cmd(change_cmd)
 
-    # Verify whether MAC address was changed to the new one
-    logging.info("Verifying the new mac address")
-    session_serial.cmd("ifconfig | grep -i %s" % new_mac)
+        # Verify whether MAC address was changed to the new one
+        error.context("Verify the new mac address, and restart the network",
+                      logging.info)
+        if os_type == "linux":
+            session_serial.cmd("ifconfig | grep -i %s" % new_mac)
+            logging.info("Mac address change successfully, net restart...")
+            dhclient_cmd = "dhclient -r && dhclient %s" % interface
+            session_serial.sendline(dhclient_cmd)
+        else:
+            utils_net.restart_windows_guest_network(session_serial,
+                                                    connection_id)
 
-    # Restart `dhclient' to regain IP for new mac address
-    logging.info("Restart the network to gain new IP")
-    dhclient_cmd = "dhclient -r && dhclient %s" % interface
-    session_serial.sendline(dhclient_cmd)
+            o = session_serial.cmd("ipconfig /all")
+            if not re.findall("%s" % "-".join(new_mac.split(":")), o, re.I):
+                raise error.TestFail("Guest mac change failed")
+            logging.info("Guest mac have been modified successfully")
 
-    # Re-log into the guest after changing mac address
-    if utils_misc.wait_for(session.is_responsive, 120, 20, 3):
-        # Just warning when failed to see the session become dead,
-        # because there is a little chance the ip does not change.
-        logging.warn("The session is still responsive, settings may fail.")
-    session.close()
+        # Re-log into the guest after changing mac address
+        if utils_misc.wait_for(session.is_responsive, 120, 20, 3):
+            # Just warning when failed to see the session become dead,
+            # because there is a little chance the ip does not change.
+            logging.warn("The session is still responsive, settings may fail.")
+        session.close()
 
-    # Re-log into guest and check if session is responsive
-    logging.info("Re-log into the guest")
-    session = vm.wait_for_login(timeout=timeout)
-    if not session.is_responsive():
-        raise error.TestFail("The new session is not responsive.")
-
-    session.close()
+        # Re-log into guest and check if session is responsive
+        error.context("Re-log into the guest", logging.info)
+        session = vm.wait_for_login(timeout=timeout)
+        if not session.is_responsive():
+            raise error.TestFail("The new session is not responsive.")
+    finally:
+        if os_type == "windows":
+            clean_cmd_pattern = params.get("clean_cmd")
+            clean_cmd = clean_cmd_pattern % int(nic_index)
+            session_serial.cmd(clean_cmd)
+            utils_net.restart_windows_guest_network(session_serial,
+                                                    connection_id)
+            nic = vm.virtnet[0]
+            nic.mac = old_mac
+            vm.virtnet.update_db()
+        session.close()
