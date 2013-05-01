c922cbe18839ff344fff72bb90b032b94f728b9f
Chris Evich <cevich@redhat.com>
2013-05-01 15:19:23 -0400

    virt-libvirt: Character XML bugfix and unittests
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- virttest/libvirt_xml/devices/console.py
+++ virttest/libvirt_xml/devices/console.py
@@ -12,5 +12,5 @@ class Console(CharacterBase):
     __slots__ = CharacterBase.__slots__
 
     def __init__(self, type_name='pty', virsh_instance=base.virsh):
-        super(Serial, self).__init__(device_tag='console', type_name=type_name,
+        super(Console, self).__init__(device_tag='console', type_name=type_name,
                                      virsh_instance=virsh_instance)
