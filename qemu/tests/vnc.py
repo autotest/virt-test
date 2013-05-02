a30d335703903eedcf6c66ff3ec8ce49217152d0
Qingtang Zhou <qzhou@redhat.com>
2013-05-02 22:54:44 +0800

    qemu.tests.vnc: Separate VNC class
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- qemu/tests/vnc.py
+++ qemu/tests/vnc.py
@@ -3,6 +3,85 @@ from autotest.client.shared import error
 from virttest import utils_misc
 from virttest.RFBDes import Des
 
+class VNC(object):
+    def __init__(self, host="localhost", port="5900", rfb_version="3.8"):
+        self.sock = socket.socket()
+        self.sock.settimeout(5)
+        self.sock.connect((host, int(port)))
+        self.rfb_version = rfb_version
+
+
+    def hand_shake(self, password=None):
+        rfb_server_version = self.sock.recv(12)
+        logging.debug("VNC server rfb version: %s" % rfb_server_version)
+        logging.debug("Handshake with rfb protocol version: %s"
+                       % self.rfb_version)
+        rfb_version = "RFB 00%s.00%s\n" % (self.rfb_version.split(".")[0],
+                                          self.rfb_version.split(".")[1])
+        self.sock.send(rfb_version)
+        if self.rfb_version != "3.3":
+            rec = self.sock.recv(1)
+            (auth,) = struct.unpack('!B',rec)
+            if auth == 0:
+                rec = self.sock.recv(4)
+                (reason_len, ) = struct.unpack('!I', rec)
+                reason = self.sock.recv(reason_len)
+                logging.error("Connection failed: %s" % reason)
+                return False
+            else:
+                rec = self.sock.recv(auth)
+                (auth_type,) = struct.unpack('!%sB' % auth, rec)
+                logging.debug("Server support %s security types" % auth_type)
+        else:
+            rec = self.sock.recv(4)
+            (auth_type, ) = struct.unpack('!I',rec)
+            logging.debug("Server support %s security types" % auth_type)
+
+        if auth_type == 0:
+            logging.error("Invalid security types")
+            return False
+        elif auth_type == 1:
+            if password is not None:
+                logging.error("Security types is None")
+                return False
+        elif auth_type == 2:
+            logging.debug("VNC Authentication")
+            if self.rfb_version != "3.3":
+                self.sock.send(struct.pack('!B', 2))
+            rec = self.sock.recv(16)
+            des = Des(password)
+            p = des.crypt(rec)
+            self.sock.send(p)
+            # Security Result check phase
+            rec = self.sock.recv(4)
+            (status, ) = struct.unpack('!I', rec)
+            if status == 1:
+                if self.rfb_version == "3.8":
+                    rec = self.sock.recv(4)
+                    (str_len, ) = struct.unpack('!I', rec)
+                    reason = self.sock.recv(str_len)
+                    logging.debug("Handshaking Failed : %s" % reason)
+                return False
+            elif status == 0:
+                return True
+
+
+    def initialize(self, shared_flag=0):
+        (shared_flag, ) = struct.pack('!B',shared_flag)
+        self.sock.send(shared_flag)
+        rec = self.sock.recv(24)
+        (width, height, pixformat, name_len) = struct.unpack('!HH16sI',rec)
+        (bits_per_pixel, depth, big_endian, true_color,
+         red_max, green_max, blue_max, red_shift, green_shift,
+         blue_shift) = struct.unpack("!BBBBHHHBBBxxx", pixformat)
+        server_name = self.sock.recv(name_len)
+        logging.info("vnc server name: %s" % server_name)
+
+
+    def close(self):
+        self.sock.close()
+
+
 def run_vnc(test, params, env):
     """
     Base test for vnc, mainly focus on handshaking during vnc connection setup.
@@ -24,82 +103,6 @@ def run_vnc(test, params, env):
     """
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
-
-    class VNC(object):
-        def __init__(self, host="localhost", port="5900", rfb_version="3.8"):
-            self.sock = socket.socket()
-            self.sock.settimeout(5)
-            self.sock.connect((host, int(port)))
-            self.rfb_version = rfb_version
-
-        def Handshake(self, password=None):
-            rfb_server_version = self.sock.recv(12)
-            logging.debug("VNC server rfb version: %s" % rfb_server_version)
-            logging.debug("Handshake with rfb protocol version: %s"
-                           % self.rfb_version)
-            rfb_version = "RFB 00%s.00%s\n" % (self.rfb_version.split(".")[0],
-                                              self.rfb_version.split(".")[1])
-            self.sock.send(rfb_version)
-            if self.rfb_version != "3.3":
-                rec = self.sock.recv(1)
-                (auth,) = struct.unpack('!B',rec)
-                if auth == 0:
-                    rec = self.sock.recv(4)
-                    (reason_len, ) = struct.unpack('!I', rec)
-                    reason = self.sock.recv(reason_len)
-                    logging.error("Connection failed: %s" % reason)
-                    return False
-                else:
-                    rec =self.sock.recv(auth)
-                    (auth_type,) = struct.unpack('!%sB' % auth, rec)
-                    logging.debug("Server support %s security types" % auth_type)
-            else:
-                rec = self.sock.recv(4)
-                (auth_type, ) = struct.unpack('!I',rec)
-                logging.debug("Server support %s security types" % auth_type)
-            if auth_type == 0:
-                logging.error("Invalid security types")
-                return False
-            elif auth_type == 1:
-                if password is not None:
-                    logging.error("Security types is None")
-                    return False
-            elif auth_type == 2:
-                logging.debug("VNC Authentication")
-                if self.rfb_version != "3.3":
-                    self.sock.send(struct.pack('!B', 2))
-                rec = self.sock.recv(16)
-                des = Des(password)
-                p = des.crypt(rec)
-                self.sock.send(p)
-                # Security Result check phase
-                rec = self.sock.recv(4)
-                (status, ) = struct.unpack('!I', rec)
-                if status == 1:
-                    if self.rfb_version == "3.8":
-                        rec = self.sock.recv(4)
-                        (str_len, ) = struct.unpack('!I', rec)
-                        reason = self.sock.recv(str_len)
-                        logging.debug("Handshaking Failed : %s" % reason)
-                    return False
-                elif status == 0:
-                    return True
-
-        def initialize(self, shared_flag=0):
-            (shared_flag, ) = struct.pack('!B',shared_flag)
-            self.sock.send(shared_flag)
-            rec = self.sock.recv(24)
-            (width, height, pixformat, name_len) = struct.unpack('!HH16sI',rec)
-            (bits_per_pixel, depth, big_endian, true_color,
-             red_max, green_max, blue_max, red_shift, green_shift,
-             blue_shift) = struct.unpack("!BBBBHHHBBBxxx", pixformat)
-            server_name = self.sock.recv(name_len)
-            logging.info("vnc server name: %s" % server_name)
-
-        def close(self):
-            self.sock.close()
-
-
     port = vm.get_vnc_port()
     passwd_cmd = params.get("change_passwd_cmd",
              "__com.redhat_set_password protocol=vnc,password=%s,expiration=%s")
@@ -113,7 +116,7 @@ def run_vnc(test, params, env):
         vm.monitor.send_args_cmd(passwd_cmd % (password, timeout))
 
         vnc = VNC(port=port, rfb_version=rfb_version)
-        status = vnc.Handshake(password)
+        status = vnc.hand_shake(password)
         vnc.initialize()
         vnc.close()
         if not status:
@@ -125,7 +128,7 @@ def run_vnc(test, params, env):
         logging.info("Waiting for vnc password timeout.")
         time.sleep(timeout + 5)
         vnc = VNC(port=port, rfb_version=rfb_version)
-        status = vnc.Handshake(password)
+        status = vnc.hand_shake(password)
         vnc.close()
         if status:
             # Should not handshake succeffully.
