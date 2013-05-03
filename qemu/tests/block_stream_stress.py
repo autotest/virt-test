2446f5d3c5f87c9735065afe37ba468246e32c21
Xu Tian <xutian@redhat.com>
2013-05-03 12:13:13 -0300

    qemu: Refactor and update block stream tests
    Introduce new block stream test cases:
    
    refactor block stream cancel, set speed to make
    script more readable;
    
    block_stream_simple test include sub-tests:
    
    1) Cancel block stream job aync and sync, AKA
    cancel before/after set block job speed event;
    2) set limited speed when job start and reset
    job speed after job started and query block job
    status used in each test of block stream tests,
    so no special test for it;
    
    Signed-off-by: Xu Tian <xutian@redhat.com>

---
+++ qemu/tests/block_stream_stress.py
@@ -0,0 +1,90 @@
+import time, logging
+from autotest.client.shared import error
+from virttest import utils_misc
+from qemu.tests import blk_stream
+
+class BlockStreamStress(blk_stream.BlockStream):
+
+    def __init__(self, test, params, env, tag):
+        super(BlockStreamStress, self).__init__(test, params, env, tag)
+
+    def parser_test_args(self):
+        """
+        set default values and check core commands has configured;
+        """
+        params = super(BlockStreamStress, self).parser_test_args()
+        for param in ["start_cmd", "stop_cmd", "check_cmd"]:
+            if not params.get(param):
+                raise error.TestFail("%s not configured,please check your"
+                                     "configuration at first")
+        return params
+
+    @error.context_aware
+    def load_stress(self):
+        """
+        load IO/CPU/Memoery stress in guest;
+        """
+        params = self.parser_test_args()
+        cmd = params.get("start_cmd")
+        session = self.get_session()
+        error.context("Load stress in guest(%s)" % cmd, logging.info)
+        session.sendline(cmd)
+        if not self.app_runing():
+            raise error.TestFail("stress app( %s) isn't running" % cmd)
+        # sleep 10s to ensure heavyload.exe make guest under heayload really;
+        time.sleep(10)
+        return None
+
+    @error.context_aware
+    def unload_stress(self):
+        """
+        stop stress app
+        """
+        error.context("stop stress app in guest", logging.info)
+        params = self.parser_test_args()
+        def _unload_stress():
+            session = self.get_session()
+            cmd = params.get("stop_cmd")
+            session.sendline(cmd)
+            if not self.app_runing():
+                return True
+            return False
+
+        stoped = utils_misc.wait_for(_unload_stress, first=2.0,
+                                     text="wait stress app quit",
+                                     step=1.0, timeout=params["wait_timeout"])
+        if not stoped:
+            raise error.TestFail("stress app is still runing")
+
+    def app_runing(self):
+        """
+        check stress app really run in background;
+        """
+        session = self.get_session()
+        params = self.parser_test_args()
+        cmd = params.get("check_cmd")
+        status = session.cmd_status(cmd, timeout=120)
+        return status == 0
+
+
+def run_block_stream_stress(test, params, env):
+    """
+    block_stream_stress test:
+    1). load stress in guest
+    2). stream block device and wait to finished
+    7). quit stress app
+    8). reboot and verify guest can response correctly
+
+    @param test: Kvm test object
+    @param params: Dictionary with the test parameters
+    @param env: Dictionary with test environment.
+    """
+    tag = params.get("source_image", "image1")
+    stress_test = BlockStreamStress(test, params, env, tag)
+    try:
+        stress_test.create_snapshots()
+        stress_test.action_before_start()
+        stress_test.start()
+        stress_test.action_after_finished()
+    finally:
+        stress_test.clean()
