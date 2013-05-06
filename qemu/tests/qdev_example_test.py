b87acb7ddd09eb887c0b128fcd7608df15002137
Lukáš Doktor <ldoktor@redhat.com>
2013-05-06 10:45:06 +0200

    REMOVEME: qemu.tests.qdev_example_test: Add unplug part
    A demonstration of unplug support. Between phase1 and 2 shouldn't be any
    VM recreations. (in case you don't specify allow_hotplugged_vm = no
    in params)
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- qemu/tests/qdev_example_test.py
+++ qemu/tests/qdev_example_test.py
@@ -6,22 +6,33 @@ import logging
 def run_qdev_example_test(test, params, env):   # pylint: disable=W0613
     vm = env.get_vm(params["main_vm"])
     pre = vm.devices.str_short()
-    if params.get('hotplug'):
-        vm.monitor.info("pci")
-        for name in params.get('hotplug').split():
-            devs = vm.devices.images_define_by_params(name,
-                                                params.object_params(name),
-                                                'disk')
-            for dev in devs:
-                ret = vm.devices.hotplug(dev, vm.monitor)
-                if ret is True:
-                    logging.warn('%s added and verified automatically', dev)
-                elif ret is False:
-                    logging.warn('%s verification failed, dev is not added', dev)
-                else:
-                    logging.warn('%s added now I should verify the state', dev)
-                    # I should verify the results here...
-                    vm.devices.hotplug_verified()
-        vm.monitor.info("pci")
+    vm.monitor.info("pci")
+    for name in params.get('hotplug', '').split():
+        devs = vm.devices.images_define_by_params(name,
+                                            params.object_params(name),
+                                            'disk')
+        for dev in devs:
+            ret = vm.devices.hotplug(dev, vm.monitor)
+            if ret is True:
+                logging.warn('%s added and verified automatically', dev)
+            elif ret is False:
+                logging.warn('%s verification failed, dev is not added', dev)
+            else:
+                logging.warn('%s added now I should verify the state', dev)
+                # I should verify the results here...
+                vm.devices.hotplug_verified()
+    middle = vm.devices.str_short()
+    for name in params.get('unplug', '').split():
+        ret = vm.devices.unplug(name, vm.monitor)
+        if ret is True:
+            logging.warn('%s removed and verified automatically', name)
+        elif ret is False:
+            logging.warn('%s verification failed, dev is not removed', name)
+        else:
+            logging.warn('%s removed now I should verify the state', name)
+            # I should verify the results here...
+            vm.devices.hotplug_verified()
+    vm.monitor.info("pci")
     logging.warn(pre)
+    logging.warn(middle)
     logging.warn(vm.devices.str_short())
