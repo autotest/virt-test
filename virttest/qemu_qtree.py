autotest/virt-test

commit 44d4805cbbec9ccd10ec5bb29bbe3fbf62c81177

qemu_qtree: Fix the problem with attached cdroms in multi_disk
Previously cdroms were not taken into account while checking
disk params thus VMs with cdroms failed with additional
devices in qtree but not in params.

Now they are skipped from params detection. As this test is not
focused on cdroms it doesn't check the cdrom params and it doesn't
require the cdrom to be present.

Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

virttest/qemu_qtree.py
@@ -5,7 +5,7 @@ Utility classes and functions to handle KVM Qtree parsing and verification.
 @copyright: 2012 Red Hat Inc.
 """
 import logging, os, re
-import storage, data_dir
+import storage, data_dir, utils_misc
 
 
 OFFSET_PER_LEVEL = 2
@@ -425,7 +425,7 @@ class QtreeDisksContainer(object):
         # host, channel, id, lun, vendor
         _scsis = re.findall(r'Host:\s+(\w+)\s+Channel:\s+(\d+)\s+Id:\s+(\d+)'
                              '\s+Lun:\s+(\d+)\n\s+Vendor:\s+([a-zA-Z0-9_-]+)'
-                             '\s+Model: ', info)
+                             '\s+Model:.*\n.*Type:\s+([a-zA-Z0-9_-]+)', info)
         disks = set()
         # Check only scsi disks
         for disk in self.disks:
@@ -440,8 +440,7 @@ class QtreeDisksContainer(object):
         scsis = set()
         for scsi in _scsis:
             # Ignore IDE disks
-            # TODO: Consider passthrough devices
-            if scsi[4].startswith('QEMU'):
+            if scsi[4] != 'CD-ROM':
                 scsis.add("%d-%d-%d" % (int(scsi[1]), int(scsi[2]),
                                         int(scsi[3])))
             else:
@@ -469,6 +468,16 @@ class QtreeDisksContainer(object):
                 disks[disk.get_qname()] = disk.get_params().copy()
         # We don't have the params name so we need to map file_names instead
         qname = None
+        for name in params.objects('cdroms'):
+            image_name = utils_misc.get_path(data_dir.get_data_dir(),
+                                params.object_params(name).get('cdrom', ''))
+            image_name = os.path.realpath(image_name)
+            for (qname, disk) in disks.iteritems():
+                if disk.get('image_name') == image_name:
+                    break
+            else:
+                continue    # Not /proc/scsi cdrom device
+            disks.pop(qname)
         for name in params.objects('images'):
             current = None
             image_params = params.object_params(name)

commit e7f1bd0c7e9d4d90deae5656194bb205c6c5a875

virttest.qemu_qtree: Fix a bug in check_guests_proc_scsi()
Fix an old bug introduced in 44d4805cbbec9ccd10ec5bb29bbe3fbf62c81177
The checked property offset should be 5.

Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

virttest/qemu_qtree.py
@@ -440,7 +440,7 @@ class QtreeDisksContainer(object):
         scsis = set()
         for scsi in _scsis:
             # Ignore IDE disks
-            if scsi[4] != 'CD-ROM':
+            if scsi[5] != 'CD-ROM':
                 scsis.add("%d-%d-%d" % (int(scsi[1]), int(scsi[2]),
                                         int(scsi[3])))
             else: