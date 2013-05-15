2147bd6a6dbe9979543063ec61472076b71f2e7b
Cleber Rosa <crosa@redhat.com>
2013-05-14 23:37:09 -0300

    common.py: check the autotest client library directory
    When relying on the environment variable $AUTOTEST_PATH to locate
    the autotest libraries, we only check if the variable is set. This
    adds another sanity check, if the client library directory actually
    exists.
    
    This prevents an ImportError exception to be thrown with no clear
    error message.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- libguestfs/common.py
+++ libguestfs/common.py
@@ -27,5 +27,11 @@ except ImportError:
             print("Environment variable $AUTOTEST_PATH not set. "
                   "please set it to a path containing an autotest checkout")
             sys.exit(1)
+        if not os.path.isdir(client_dir):
+            print('Autotest client library directory was not found at: "%s"' %
+                  client_dir)
+            print('Please check if the environment variable "$AUTOTEST_PATH" '
+                  'points to a valid location')
+            sys.exit(1)
         sm = load_setup_modules(client_dir)
 sm.setup(base_path=client_dir, root_module_name="autotest.client")
