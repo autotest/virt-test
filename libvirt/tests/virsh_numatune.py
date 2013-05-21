5a5fd35d3f6e2684b5e17f3db27315410aad9224
Chris Evich <cevich@redhat.com>
2013-05-21 15:45:38 -0400

    virt-libvirt: fix virsh_numatune test bug
    Test was failing when start_vm was "no" but gest was running.
    Test was failing when requested numaset greater than available
    on host.  Test was failing when --config parameter was used
    but guest was not restarted.  Many thanks to Yang Dongsheng
    <yangds.fnst@cn.fujitsu.com> for his help finding and helping
    fix some of these problems.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- libvirt/tests/virsh_numatune.py
+++ libvirt/tests/virsh_numatune.py
@@ -1,7 +1,19 @@
 import re, logging
-from autotest.client.shared import error
+from autotest.client.shared import error, utils
 from virttest import libvirt_vm, libvirt_xml, virsh
 from virttest import utils_cgroup
+
+
+def num_numa_nodes():
+    """
+    Return the number of available numa nodes
+    """
+    output = utils.run('numactl -H').stdout.strip()
+    mobj = re.match(r'available:\s+(\d+)\s+nodes\s+.*', output)
+    if mobj is not None:
+        return int(mobj.group(1))
+    else:
+        return 0
 
 
 def nodeset_parser(nodeset):
@@ -26,7 +38,7 @@ def nodeset_parser(nodeset):
                     tmp = re.split("-", nodeset)
                     hyphens = hyphens + range(int(tmp[0]), int(tmp[-1])+1)
                 elif "^" in nodeset:
-                    tmp = re.split("\^", nodeset)[-1]
+                    tmp = re.split(r"\^", nodeset)[-1]
                     carets.append(int(tmp))
                 else:
                     try:
@@ -59,14 +71,26 @@ def check_numatune_xml(params):
     vm_name = params.get("vms")
     mode = params.get("numa_mode", "")
     nodeset = params.get("numa_nodeset", "")
+    options = params.get("options", "")
+    #--config option will act after vm shutdown.
+    if options == "config":
+        virsh.shutdown(vm_name)
+    #The verification of the numa params should
+    #be done when vm is running.
+    if not virsh.is_alive(vm_name):
+        virsh.start(vm_name)
 
     numa_params = libvirt_xml.VMXML.get_numa_params(vm_name)
     if not numa_params:
-        logging.error("Could not get numa parameters for %s" % vm_name)
+        logging.error("Could not get numa parameters for %s", vm_name)
         return False
 
     mode_from_xml = numa_params['mode']
-    nodeset_from_xml = numa_params['nodeset']
+    #if the placement is auto, there is no nodeset in numa param.
+    try:
+        nodeset_from_xml = numa_params['nodeset']
+    except KeyError():
+        nodeset_from_xml = ""
 
     if mode and mode != mode_from_xml:
         logging.error("To expect %s: %s", mode, mode_from_xml)
@@ -149,7 +173,10 @@ def set_numa_parameter(params):
             raise error.TestFail("Unexpected return code %d" % status)
     elif status_error == "no":
         if status:
-            raise error.TestFail(result.stderr)
+            if len(nodeset_parser(nodeset)) > num_numa_nodes():
+                raise error.TestNAError("Host does not support requested nodeset")
+            else:
+                raise error.TestFail(result.stderr)
         else:
             if check_numatune_xml(params):
                 logging.info(result.stdout)
@@ -183,44 +210,52 @@ def run_virsh_numatune(test, params, env):
     # Run test case
     vm_name = params.get("vms")
     vm = env.get_vm(vm_name)
+    original_vm_xml = libvirt_xml.VMXML.new_from_dumpxml(vm_name)
     status_error = params.get("status_error", "no")
     libvirtd = params.get("libvirtd", "on")
     cgconfig = params.get("cgconfig", "on")
     start_vm = params.get("start_vm", "no")
     change_parameters = params.get("change_parameters", "no")
 
+    # Make sure vm is down if start not requested
+    if start_vm == "no" and vm.is_alive():
+        vm.destroy()
+
     ########## positive and negative testing #########
 
-    if status_error == "no":
-        if change_parameters == "no":
-            get_numa_parameter(params)
-        else:
-            set_numa_parameter(params)
-
-    if cgconfig == "off":
-        # Need to shutdown a running guest before stopping cgconfig service
-        # and will start the guest after restarting libvirtd service
-        if vm.is_alive():
-            vm.destroy()
-        if utils_cgroup.service_cgconfig_control("status"):
-            utils_cgroup.service_cgconfig_control("stop")
-
-    # Refresh libvirtd service to get latest cgconfig service change
-    if libvirtd == "restart":
-        libvirt_vm.service_libvirtd_control("restart")
-
-    # Recover previous running guest
-    if cgconfig == "off" and libvirtd == "restart" \
-        and not vm.is_alive() and start_vm == "yes":
-        vm.start()
-
-    if status_error == "yes":
-        if change_parameters == "no":
-            get_numa_parameter(params)
-        else:
-            set_numa_parameter(params)
-
-    # Recover cgconfig and libvirtd service
-    if not utils_cgroup.service_cgconfig_control("status"):
-        utils_cgroup.service_cgconfig_control("start")
-        libvirt_vm.service_libvirtd_control("restart")
+    try:
+        if status_error == "no":
+            if change_parameters == "no":
+                get_numa_parameter(params)
+            else:
+                set_numa_parameter(params)
+        if cgconfig == "off":
+            # Need to shutdown a running guest before stopping cgconfig service
+            # and will start the guest after restarting libvirtd service
+            if vm.is_alive():
+                vm.destroy()
+            if utils_cgroup.service_cgconfig_control("status"):
+                utils_cgroup.service_cgconfig_control("stop")
+        # Refresh libvirtd service to get latest cgconfig service change
+        if libvirtd == "restart":
+            libvirt_vm.service_libvirtd_control("restart")
+        # Recover previous running guest
+        if cgconfig == "off" and libvirtd == "restart" \
+            and not vm.is_alive() and start_vm == "yes":
+            vm.start()
+        if status_error == "yes":
+            if change_parameters == "no":
+                get_numa_parameter(params)
+            else:
+                set_numa_parameter(params)
+        # Recover cgconfig and libvirtd service
+        if not utils_cgroup.service_cgconfig_control("status"):
+            utils_cgroup.service_cgconfig_control("start")
+            libvirt_vm.service_libvirtd_control("restart")
+    finally:
+        vm.destroy()
+        # Restore guest, first remove existing
+        original_vm_xml.undefine()
+        # Recover based on original XML
+        original_vm_xml.define()
+        # leave vm down, next test will start if needed
