78b418f29e303818a9417266e707be30fb8217e5
Yunping Zheng <yunzheng@redhat.com>
2013-05-24 14:32:28 +0800

    qemu: make nic_promisc support windows guests
    This patch makes nic_promisc test support windows guests.
    
    Signed-off-by: Yunping Zheng <yunzheng@redhat.com>

---
+++ shared/scripts/set_win_promisc.py
@@ -0,0 +1,9 @@
+import socket, time
+HOST = socket.gethostbyname(socket.gethostname())
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+s.bind((HOST, 0))
+
+s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+time.sleep(2)
+s.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+time.sleep(2)
