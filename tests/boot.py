28d6484259414de80f369e46d43d9cb8b99959c9
Suqin Huang <shuang@redhat.com>
2013-05-24 14:39:49 +0800

    qemu.tests.boot: Print test steps
    Print boot steps to logging.info
    
    Signed-off-by: Suqin Huang <shuang@redhat.com>
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>
    Acked-by: Feng Yang <fyang@redhat.com>

--- tests/boot.py
+++ tests/boot.py
@@ -1,4 +1,4 @@
-import time
+import time, logging
 from autotest.client.shared import error
 from virttest import utils_test
 
@@ -6,18 +6,18 @@ from virttest import utils_test
 @error.context_aware
 def run_boot(test, params, env):
     """
-    KVM reboot test:
+    Qemu reboot test:
     1) Log into a guest
     3) Send a reboot command or a system_reset monitor command (optional)
     4) Wait until the guest is up again
     5) Log into the guest to verify it's up again
 
-    @param test: kvm test object
+    @param test: QEMU test object
     @param params: Dictionary with the test parameters
     @param env: Dictionary with test environment.
     """
 
-    error.context("Try to log into guest.")
+    error.context("Try to log into guest.", logging.info)
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
     timeout = float(params.get("login_timeout", 240))
@@ -27,7 +27,7 @@ def run_boot(test, params, env):
         utils_test.service_setup(vm, session, test.virtdir)
 
     if params.get("reboot_method"):
-        error.context("Reboot guest.")
+        error.context("Reboot guest.", logging.info)
         if params["reboot_method"] == "system_reset":
             time.sleep(int(params.get("sleep_before_reset", 10)))
             # Reboot the VM
