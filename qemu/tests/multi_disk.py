autotest/virt-test

commit a1c46d139b79ad7f27ab2dee6f6ff58ac9de65d1

qemu: multi_disk: Disable qtree functions on RHEL5 host
RHEL5 host doesn't provide a qtree monitor command, thus
we should disable qtree check on this kind of host.

Signed-off-by: Qingtang Zhou <qzhou@redhat.com>
Acked-by: Feng Yang <fyang@redhat.com>

qemu/tests/multi_disk.py
@@ -193,24 +193,25 @@ def run_multi_disk(test, params, env):
     re_str = params["re_str"]
     black_list = params["black_list"].split()
 
-    error.context("verifying qtree vs. test params")
-    err = 0
-    qtree = qemu_qtree.QtreeContainer()
-    qtree.parse_info_qtree(vm.monitor.info('qtree'))
-    disks = qemu_qtree.QtreeDisksContainer(qtree.get_nodes())
-    (tmp1, tmp2) = disks.parse_info_block(vm.monitor.info('block'))
-    err += tmp1 + tmp2
-    err += disks.generate_params()
-    err += disks.check_disk_params(params)
-    (tmp1, tmp2, _, _) = disks.check_guests_proc_scsi(
+    if "qtree" in str(vm.monitor.send_args_cmd("help")):
+        error.context("verifying qtree vs. test params")
+        err = 0
+        qtree = qemu_qtree.QtreeContainer()
+        qtree.parse_info_qtree(vm.monitor.info('qtree'))
+        disks = qemu_qtree.QtreeDisksContainer(qtree.get_nodes())
+        (tmp1, tmp2) = disks.parse_info_block(vm.monitor.info('block'))
+        err += tmp1 + tmp2
+        err += disks.generate_params()
+        err += disks.check_disk_params(params)
+        (tmp1, tmp2, _, _) = disks.check_guests_proc_scsi(
                                     session.cmd_output('cat /proc/scsi/scsi'))
-    err += tmp1 + tmp2
+        err += tmp1 + tmp2
 
-    if err:
-        raise error.TestFail("%s errors occurred while verifying qtree vs. "
-                             "params" % err)
-    if params.get('multi_disk_only_qtree') == 'yes':
-        return
+        if err:
+            raise error.TestFail("%s errors occurred while verifying"
+                                 " qtree vs. params" % err)
+        if params.get('multi_disk_only_qtree') == 'yes':
+            return
 
     try:
         if params.get("clean_cmd"):

commit f97ea551a92e2437a8dd022de0ac06e55f5af51e

qemu.tests.multi_disk: Fix github issue #438
This is a bug which was introduced in
a1c46d139b79ad7f27ab2dee6f6ff58ac9de65d1

This is no 'qtree' keyword in the output of any
version of qemu, thus the method to check if
qemu supports qtree in that commit would disable
qtree check for every qemu binary.

This commit tries to run the 'info qtree' command
directly, if everything goes well, it means we
have qtree support.

Reported-by: Mike Qiu <qiudayu@linux.vnet.ibm.com>
Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

qemu/tests/multi_disk.py
@@ -5,7 +5,7 @@ multi_disk test for Autotest framework.
 """
 import logging, re, random, string
 from autotest.client.shared import error, utils
-from virttest import qemu_qtree, env_process
+from virttest import qemu_qtree, env_process, qemu_monitor
 
 _RE_RANGE1 = re.compile(r'range\([ ]*([-]?\d+|n).*\)')
 _RE_RANGE2 = re.compile(r',[ ]*([-]?\d+|n)')
@@ -202,7 +202,12 @@ def run_multi_disk(test, params, env):
     re_str = params["re_str"]
     black_list = params["black_list"].split()
 
-    if "qtree" in str(vm.monitor.human_monitor_cmd("help", debug=False)):
+    have_qtree = True
+    out = vm.monitor.human_monitor_cmd("qtree", debug=False)
+    if "unknown command" in str(out):
+        have_qtree = False
+
+    if have_qtree:
         error.context("Verifying qtree vs. test params")
         err = 0
         qtree = qemu_qtree.QtreeContainer()