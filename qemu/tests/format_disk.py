8a54779a22acb5df5a2c47ad47fc6c130d67f463
Qingtang Zhou <qzhou@redhat.com>
2013-06-04 17:12:49 +0800

    qemu.tests.format_disk: Update the cmd of read/write data
    This patch makes the read/write cmd fully configurable.
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- qemu/tests/format_disk.py
+++ qemu/tests/format_disk.py
@@ -75,17 +75,17 @@ def run_format_disk(test, params, env):
         error.context("Write some random string to test file", logging.info)
         ranstr = utils_misc.generate_random_string(100)
 
-        writefile_cmd = params.get("writefile_cmd")
-        wfilecmd = writefile_cmd + " " + ranstr + " >" + testfile_name
-        s, o = session.cmd_status_output(wfilecmd, timeout=cmd_timeout)
+        writefile_cmd = params["writefile_cmd"]
+        writefile_cmd = writefile_cmd % (ranstr, testfile_name)
+        s, o = session.cmd_status_output(writefile_cmd, timeout=cmd_timeout)
         if s != 0:
             raise error.TestFail("Write to file error: %s" % o)
 
         error.context("Read in the file to see whether content has changed",
                       logging.info)
-        readfile_cmd = params.get("readfile_cmd")
-        rfilecmd = readfile_cmd + " " + testfile_name
-        s, o = session.cmd_status_output(rfilecmd, timeout=cmd_timeout)
+        readfile_cmd = params["readfile_cmd"]
+        readfile_cmd = readfile_cmd % testfile_name
+        s, o = session.cmd_status_output(readfile_cmd, timeout=cmd_timeout)
         if s != 0:
             raise error.TestFail("Read file error: %s" % o)
         if o.strip() != ranstr:
