3609ced61445f8e333c7ac63b2acd24f31214dfb
yangdongsheng <yangds.fnst@cn.fujitsu.com>
2013-06-21 11:25:45 +0800

    Fix dummyvirsh.define() to return CmdResultObject.
    Signed-off-by: yangdongsheng <yangds.fnst@cn.fujitsu.com>

--- virttest/libvirt_xml_unittest.py
+++ virttest/libvirt_xml_unittest.py
@@ -98,6 +98,14 @@ class LibvirtXMLTestBase(unittest.TestCase):
                                                 '%s.xml' % dom_name)
         shutil.copy(file_path, xml_path)
 
+        cmd = "virsh define --file %s" % file_path
+        stdout = ""
+        stderr = stdout
+        exit_status = 0
+        result = utils.CmdResult(cmd, stdout, stderr, exit_status)
+
+        return result
+
     @staticmethod
     def _dumpxml(name, to_file="", **dargs):
         """
