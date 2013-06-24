0286ff1f3394161e4dcf42d6de282feb8b8be034
Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
2013-06-24 14:56:20 +0530

    virsh_cmd: Add virsh domiflist test case
    Step 1: Get the virsh domiflist value.
    Step 2: Check for interface in xml file.
    Step 3: Check for type in xml file.
    Step 4: Check for model inside the guest and xml file.
    Step 5: Check for mac id inside the guest and xml file.
    
    Signed-off-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>

--- virttest/virsh.py
+++ virttest/virsh.py
@@ -2157,7 +2157,7 @@ def domblklist(name, options=None, **dargs):
     return command(cmd, **dargs)
 
 
-def domiflist(name, options='', **dargs):
+def domiflist(name, options='', extra='', **dargs):
     """
     Get the domain network devices
 
@@ -2167,7 +2167,7 @@ def domiflist(name, options='', **dargs):
     @return: CmdResult instance
     """
 
-    return command('domiflist %s %s' % (name, options), **dargs)
+    return command('domiflist %s %s %s' % (name, options, extra), **dargs)
 
 
 def cpu_stats(name, options, **dargs):
