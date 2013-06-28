306c79331026ad693fdba3fbebc2c2063b4f894a
Chris Evich <cevich@redhat.com>
2013-06-28 16:13:53 -0400

    virt-libvirt: Remove disused VM methods
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- virttest/libvirt_vm.py
+++ virttest/libvirt_vm.py
@@ -1122,19 +1122,6 @@ class VM(virt_vm.BaseVM):
             self.connect_uri = dest_uri
         return result
 
-    def attach_device(self, xml_file, extra=""):
-        """
-        Attach a device to VM.
-        """
-        return virsh.attach_device(self.name, xml_file, extra,
-                                   uri=self.connect_uri)
-
-    def detach_device(self, xml_file, extra=""):
-        """
-        Detach a device from VM.
-        """
-        return virsh.detach_device(self.name, xml_file, extra,
-                                   uri=self.connect_uri)
 
     def attach_interface(self, option="", ignore_status=False,
                          debug=False):
