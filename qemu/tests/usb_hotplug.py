eadd7a80c5a8341ada150c3bbc3ead332be2f65c
Qingtang Zhou <qzhou@redhat.com>
2013-07-03 15:11:44 +0800

    qemu.tests.usb: Make usb hotplug test can run multiple times
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- qemu/tests/usb_hotplug.py
+++ qemu/tests/usb_hotplug.py
@@ -10,6 +10,46 @@ def run_usb_hotplug(test, params, env):
     @param params: Dictionary with the test parameters
     @param env: Dictionary with test environment.
     """
+    @error.context_aware
+    def usb_dev_hotplug():
+        error.context("Plugin usb device", logging.info)
+        session.cmd(clear_guest_log_cmd)
+        reply = vm.monitor.cmd(monitor_add)
+        if params.get("usb_negative_test") == "yes":
+            if params["usb_reply_msg"] not in reply:
+                raise error.TestFail("Could not get expected warning"
+                                     " msg in negative test, monitor"
+                                     " returns: '%s'" % reply)
+            return
+
+        monitor_pattern = "Parameter 'driver' expects a driver name"
+        if reply.find(monitor_pattern) != -1:
+            raise error.TestNAError("usb device %s not available" % device)
+
+
+    @error.context_aware
+    def usb_dev_verify():
+        error.context("Verify usb device is pluged on guest", logging.info)
+        time.sleep(sleep_time)
+        session.cmd(udev_refresh_cmd)
+        messages_add = session.cmd(query_syslog_cmd)
+        for line in messages_add.splitlines():
+            logging.debug("[Guest add] %s" % line)
+        if not re.search(match_add, messages_add, re.I):
+            raise error.TestFail("Guest didn't detect plugin")
+
+
+    @error.context_aware
+    def usb_dev_unplug():
+        error.context("Unplug usb device", logging.info)
+        vm.monitor.cmd(monitor_del)
+        time.sleep(sleep_time)
+        messages_del = session.cmd(query_syslog_cmd)
+        for line in messages_del.splitlines():
+            logging.debug("[Guest del] %s" % line)
+        if messages_del.find(match_del) == -1:
+            raise error.TestFail("Guest didn't detect unplug")
+
 
     device = params.object_params("testdev")["usb_type"]
     vendor_id = params["vendor_id"]
@@ -31,34 +71,12 @@ def run_usb_hotplug(test, params, env):
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
     session = vm.wait_for_login()
-    session.cmd(clear_guest_log_cmd)
 
-    error.context("Plugin usb device", logging.info)
-    reply = vm.monitor.cmd(monitor_add)
-    if params.get("usb_negative_test") == "yes":
-        if params["usb_reply_msg"] not in reply:
-            raise error.TestFail("Could not get expected warning msg in"
-                            " negative test, monitor returns: '%s'" % reply)
-        return
-
-    if reply.find("Parameter 'driver' expects a driver name") != -1:
-        raise error.TestNAError("usb device %s not available" % device)
-
-    time.sleep(sleep_time)
-    session.cmd(udev_refresh_cmd)
-    messages_add = session.cmd(query_syslog_cmd)
-    for line in messages_add.splitlines():
-        logging.debug("[Guest add] %s" % line)
-    if not re.search(match_add, messages_add, re.I):
-        raise error.TestFail("Guest didn't detect plugin")
-
-    error.context("Unplug usb device", logging.info)
-    vm.monitor.cmd(monitor_del)
-    time.sleep(sleep_time)
-    messages_del = session.cmd(query_syslog_cmd)
-    for line in messages_del.splitlines():
-        logging.debug("[Guest del] %s" % line)
-    if messages_del.find(match_del) == -1:
-        raise error.TestFail("Guest didn't detect unplug")
+    repeat_times = int(params["usb_repeat_times"])
+    for i in range(repeat_times):
+        error.context("Hotplug for the %i time" % (i + 1), logging.info)
+        usb_dev_hotplug()
+        usb_dev_verify()
+        usb_dev_unplug()
 
     session.close()
