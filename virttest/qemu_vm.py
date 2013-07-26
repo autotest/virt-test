618d10ec519d48f2098d3811211f8977879a9efd
Jiří Župka <jzupka@redhat.com>
2013-07-26 15:41:14 +0200

    virt: Fix bug in migration for spice protocol.
    Signed-off-by: Jiří Župka <jzupka@redhat.com>

--- virttest/qemu_vm.py
+++ virttest/qemu_vm.py
@@ -2838,7 +2838,7 @@ class VM(virt_vm.BaseVM):
             error.context()
 
         try:
-            if (self.params["display"] == "spice" and
+            if (self.params["display"] == "spice" and local and
                 not (protocol == "exec" and
                 (migration_exec_cmd_src and "gzip" in migration_exec_cmd_src))):
                 host_ip = utils_net.get_host_ip_address(self.params)
