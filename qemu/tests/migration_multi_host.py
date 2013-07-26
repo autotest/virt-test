4ab104eef102bdd46e84b77ddee2e770d7f693f8
Jiří Župka <jzupka@redhat.com>
2013-07-26 15:45:29 +0200

    virt: Expands multihost migration test.
    Allow test start pre_sub_test before migration.
    
    Signed-off-by: Jiří Župka <jzupka@redhat.com>

--- qemu/tests/migration_multi_host.py
+++ qemu/tests/migration_multi_host.py
@@ -10,13 +10,21 @@ def run_migration_multi_host(test, params, env):
 
     Migration execution progress is described in documentation
     for migrate method in class MultihostMigration.
+    steps:
+        1) try log to VM if login_before_pre_tests == yes
+        2) before migration start pre_sub_test
+        3) migration
+        4) after migration start post_sub_test
 
     @param test: kvm test object.
     @param params: Dictionary with test parameters.
     @param env: Dictionary with the test environment.
     """
     login_timeout = int(params.get("login_timeout", 360))
-    sub_test = params.get("sub_test")
+    pre_sub_test = params.get("pre_sub_test")
+    post_sub_test = params.get("post_sub_test")
+    pre_sub_test_timeout = int(params.get("pre_sub_test_timeout", "240"))
+    login_before_pre_tests = params.get("login_before_pre_tests", "no")
 
     mig_protocol = params.get("mig_protocol", "tcp")
     mig_type = utils_test.MultihostMigration
@@ -30,11 +38,24 @@ def run_migration_multi_host(test, params, env):
     dsthost = params["hosts"][1]
     is_src = params["hostid"] == srchost
 
+    if is_src:  #is destination
+        if pre_sub_test:
+            if login_before_pre_tests == "yes":
+                vm = env.get_vm(vms[0])
+                vm.wait_for_login(timeout=login_timeout)
+            error.context("Run sub test '%s' before migration on src"
+                           % pre_sub_test, logging.info)
+            utils_test.run_virt_sub_test(test, params, env, pre_sub_test)
+
+
     mig = mig_type(test, params, env, False)
+    mig._hosts_barrier([srchost, dsthost],
+                       {'src': srchost, 'dst': dsthost, "vms": vms[0]},
+                       "sync", pre_sub_test_timeout)
     mig.migrate_wait([vms[0]], srchost, dsthost)
 
     if not is_src:  #is destination
-        if sub_test:
-            error.context("Run sub test '%s' after checking"
-                          " clock resolution" % sub_test, logging.info)
-            utils_test.run_virt_sub_test(test, params, env, sub_test)
+        if post_sub_test:
+            error.context("Run sub test '%s' after migration on dst"
+                           % post_sub_test, logging.info)
+            utils_test.run_virt_sub_test(test, params, env, post_sub_test)
