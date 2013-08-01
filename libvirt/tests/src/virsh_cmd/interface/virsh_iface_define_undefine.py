377a6f4fa0410a99bd8c11a1ea7185f729d1215a
sanjeevpatro@linux.vnet.ibm.com <sanjeevp@linux.vnet.ibm.com>
2013-08-01 12:42:46 +0530

    virsh_iface_define_undefine.py

--- libvirt/tests/src/virsh_cmd/interface/virsh_iface_define_undefine.py
+++ libvirt/tests/src/virsh_cmd/interface/virsh_iface_define_undefine.py
@@ -3,7 +3,6 @@ import os,logging, fileinput, re, os.path
 from autotest.client.shared import utils,error
 from virttest import virsh, libvirt_vm,iface
 from virttest.libvirt_xml import vm_xml
-from virttest.iface import *
 """
 Test Case:
 If a network script is available then, for virsh it is defined or else 
@@ -33,67 +32,74 @@ def run_virsh_iface_define_undefine(test, params, env):
     def chk_virsh_define(opt):
         err_cnt=0
         virsh.iface_define("tmp-%s.xml" %opt, ignore_status=True)
-        if is_scr_avail(opt) is 'no' or avail_vir_iface(opt) is 'no':
-            logging.debug("iface define failed for defined iface %s after undefine"%opt)
+        logging.debug("Running virsh iface define for undefined iface %s"%opt)
+        if iface.is_scr_avail(opt) is False or iface.avail_vir_iface(opt) is False:
+            logging.debug("iface define failed for undefined iface %s"%opt)
             err_cnt += 1
         else:
-            if chk_mac_vir_iface(opt) is 'FAIL':
-                logging.debug("mac address is not correct in virsh")
+            if iface.chk_mac_vir_iface(opt) is False:
+                logging.debug("mac address is not correct in virsh for %s"%opt)
                 err_cnt +=1
+            else:
+                logging.debug("iface define is passed for undefined iface %s"%opt)
         if err_cnt > 0:
-            return 'FAIL'
+            return False
     def chk_virsh_undefine(opt):
         err_cnt=0
         virsh.iface_undefine("%s" %opt, ignore_status=True)
-        if is_scr_avail(opt) is 'yes' or avail_vir_iface(opt) is 'yes':
+        logging.debug("Running virsh iface undefine for defined iface %s"%opt)
+        if iface.is_scr_avail(opt) or iface.avail_vir_iface(opt):
             logging.debug("iface undefine failed for defined iface %s"%opt)
             err_cnt += 1
+        else: 
+            logging.debug("iface undefine passed for defined iface %s"%opt)
         if err_cnt > 0:
-            return 'FAIL'
+            return False
     def check_virsh_define_undefine(opt):
         error_cnt=0
-        if avail_vir_iface(opt) is 'yes':
-            edit_iface_xml(opt)
-            if is_scr_avail(opt) is 'yes':
-                network_scripts_backup(opt) 
-                if chk_virsh_define(opt) is 'FAIL': 
+        if iface.avail_vir_iface(opt):
+            iface.edit_iface_xml(opt)
+            if iface.is_scr_avail(opt):
+                iface.network_scripts_backup(opt) 
+                if chk_virsh_define(opt) is False: 
                     error_cnt+=1
-                if chk_virsh_undefine(opt) is 'FAIL':
+                if chk_virsh_undefine(opt) is False:
                     error_cnt+=1
-                network_scripts_restore(opt) 
+                iface.network_scripts_restore(opt) 
             else:
                 logging.debug("list shows %s as defined though absence of script"%opt)
                 error_cnt += 1
-            destroy_iface_xml(opt)
+            iface.destroy_iface_xml(opt)
         else:
-            create_iface_xml(opt)
-            if is_scr_avail(opt) is 'no':
-                if chk_virsh_define(opt) is 'FAIL':
+            iface.create_iface_xml(opt)
+            if iface.is_scr_avail(opt) is False:
+                if chk_virsh_define(opt) is False:
                     error_cnt+=1
-                if chk_virsh_undefine(opt) is 'FAIL':
+                if chk_virsh_undefine(opt) is False:
                     error_cnt+=1
             else:
                 logging.debug("list did not define iface %s though presnece of script "%opt)
                 error_cnt += 1
-            destroy_iface_xml(opt)
+            iface.destroy_iface_xml(opt)
         if error_cnt > 0:
-            return 'FAIL'
+            return False
         else:
-            return 'PASS'
+            return True
     
     
     def check_virsh_define_undefine_all():
         error=0
-        for ind_iface in input_ifaces():
-            if check_virsh_define_undefine(ind_iface) is 'FAIL':
+        for ind_iface in iface.input_ifaces():
+            logging.debug("Define/Undefine test would be run on")
+            logging.debug("following interfaces")
+            logging.debug("%s"%iface.input_ifaces())
+            if check_virsh_define_undefine(ind_iface) is False:
                 logging.debug("iface define undefine is unsuccessful for iface %s"%ind_iface)
                 error += 1
             else:
                 logging.debug("iface define undefine is successful for iface %s"%ind_iface)
         if error > 0:
-            return 'FAIL'
+            return False
         else:
-            return 'PASS'
-    
+            return True
     logging.debug("%s" %check_virsh_define_undefine_all())
-    #print input_ifaces()
