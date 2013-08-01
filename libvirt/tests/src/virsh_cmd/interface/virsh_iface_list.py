bde598c4368aff39c3e4cb857fc2b58afc4c4fdb
sanjeevpatro@linux.vnet.ibm.com <sanjeevp@linux.vnet.ibm.com>
2013-08-01 12:42:47 +0530

    virsh_iface_list.py

--- libvirt/tests/src/virsh_cmd/interface/virsh_iface_list.py
+++ libvirt/tests/src/virsh_cmd/interface/virsh_iface_list.py
@@ -3,7 +3,6 @@ import os,logging, fileinput, re, os.path
 from autotest.client.shared import utils,error
 from virttest import virsh, libvirt_vm,iface
 from virttest.libvirt_xml import vm_xml
-from virttest.iface import *
 """
 Test case:
 Verify virsh iface-list. The interface which are having network scripts
@@ -23,14 +22,14 @@ The interface whose network scripts are availble
 def run_virsh_iface_list(test, params, env):
     def check_virsh_list_all():
         error=0
-        for ind_iface in input_ifaces():
-            if is_scr_avail(ind_iface) == 'yes':
-                if avail_vir_iface(ind_iface) == 'yes':
-                    if chk_state_vir_iface(ind_iface) == 'FAIL': 
+        for ind_iface in iface.input_ifaces():
+            if iface.is_scr_avail(ind_iface):
+                if iface.avail_vir_iface(ind_iface):
+                    if iface.chk_state_vir_iface(ind_iface) == False: 
                         logging.debug("virsh list --all shows wrong")
                         logging.debug("for the state of %s"%ind_iface)
                         error += 1
-                    if chk_mac_vir_iface(ind_iface) == 'FAIL':
+                    if iface.chk_mac_vir_iface(ind_iface) == False:
                         logging.debug("virsh list --all shows")
                         logging.debug("wrong for the mac of %s"%ind_iface)
                         error += 1
@@ -39,26 +38,26 @@ def run_virsh_iface_list(test, params, env):
                     logging.debug("not show iface %s"%ind_iface)
                     error += 1
             else:
-                if avail_vir_iface(ind_iface) == 'yes':
+                if iface.avail_vir_iface(ind_iface):
                     logging.debug("virsh iface-list --all should")
                     logging.debug("not show iface %s"%ind_iface)
                     error += 1
         if error > 0:
-            return 'FAIL'
+            return False
         else:
-            return 'PASS'
+            return True
     
     
     def check_virsh_list_active():
         error=0
-        for ind_iface in input_ifaces():
-            if is_scr_avail(ind_iface) == 'yes' and is_up(ind_iface)=='yes' :
-                if avail_vir_iface_active(ind_iface) == 'yes':
-                    if chk_state_vir_iface_active(ind_iface) == 'FAIL':
+        for ind_iface in iface.input_ifaces():
+            if iface.is_scr_avail(ind_iface) and iface.is_up(ind_iface):
+                if iface.avail_vir_iface_active(ind_iface):
+                    if iface.chk_state_vir_iface_active(ind_iface) == False:
                         logging.debug("virsh list shows wrong")
                         logging.debug("for the state of %s"%ind_iface)
                         error += 1
-                    if chk_mac_vir_iface_active(ind_iface) == 'FAIL':
+                    if iface.chk_mac_vir_iface_active(ind_iface) == False:
                         logging.debug("virsh list  shows")
                         logging.debug("wrong for the mac of %s"%ind_iface)
                         error += 1
@@ -68,25 +67,25 @@ def run_virsh_iface_list(test, params, env):
 
                     error += 1
             else:
-                if avail_vir_iface_active(ind_iface) == 'yes':
+                if iface.avail_vir_iface_active(ind_iface):
                     logging.debug("virsh iface-list should")
                     logging.debug("not show iface %s"%ind_iface)
                     error += 1
         if error > 0:
-            return 'FAIL'
+            return False
         else:
-            return 'PASS'
+            return True
     
     def check_virsh_list_inactive():
         error=0
-        for ind_iface in input_ifaces():
-            if is_scr_avail(ind_iface) == 'yes'  and is_up(ind_iface)=='no':
-                if avail_vir_iface_inactive(ind_iface) == 'yes':
-                    if chk_state_vir_iface(ind_iface) == 'FAIL':
+        for ind_iface in iface.input_ifaces():
+            if iface.is_scr_avail(ind_iface)  and iface.is_up(ind_iface)==False:
+                if iface.avail_vir_iface_inactive(ind_iface):
+                    if iface.chk_state_vir_iface(ind_iface) == False:
                         logging.debug("virsh list --inactive shows wrong")
                         logging.debug("for the state of %s"%ind_iface)
                         error += 1
-                    if chk_mac_vir_iface(ind_iface) == 'FAIL':
+                    if iface.chk_mac_vir_iface(ind_iface) == False:
                         logging.debug("virsh list --inactive shows")
                         logging.debug("wrong for the mac of %s"%ind_iface)
                         error += 1
@@ -95,15 +94,15 @@ def run_virsh_iface_list(test, params, env):
                     logging.debug("not show iface %s"%ind_iface)
                     error += 1
             else:
-                if avail_vir_iface_inactive(ind_iface) == 'yes':
+                if iface.avail_vir_iface_inactive(ind_iface):
                     logging.debug("virsh iface-list --inactive should")
                     logging.debug("not show iface %s"%ind_iface)
 
                     error += 1
         if error > 0:
-            return 'FAIL'
+            return False
         else:
-            return 'PASS'
+            return True
     
     options_ref = params.get("iface_list_option","");
     if options_ref == '--all':
