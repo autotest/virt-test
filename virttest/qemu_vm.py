1c2eb90747394ea5352a6262b8ebf2ecf87103f9
Feng Yang <fyang@redhat.com>
2013-09-10 17:08:20 +0800

    virttest.qemu_vm: Support device_driver parameter in pci_assignable related device.
    It used to set which device we want to use in device assignment test
    
    Signed-off-by: Feng Yang <fyang@redhat.com>

--- virttest/qemu_vm.py
+++ virttest/qemu_vm.py
@@ -633,9 +633,9 @@ class VM(virt_vm.BaseVM):
         def add_uuid(devices, uuid):
             return " -uuid '%s'" % uuid
 
-        def add_pcidevice(devices, host, params):
-            if devices.has_device('pci-assign'):
-                dev = QDevice('pci-assign', parent_bus={'type': 'pci'})
+        def add_pcidevice(devices, host, params, device_driver="pci-assign"):
+            if devices.has_device(device_driver):
+                dev = QDevice(device_driver, parent_bus={'type': 'pci'})
             else:
                 dev = qemu_devices.QCustomDevice('pcidevice',
                                                  parent_bus={'type': 'pci'})
@@ -1283,8 +1283,10 @@ class VM(virt_vm.BaseVM):
                 # TODO: Is every NIC a PCI device?
                 devices.insert(StrDev("NET-%s" % nettype, cmdline=cmd))
             else:
+                device_driver = nic_params.get("device_driver", "pci-assign")
                 pci_id = vm.pa_pci_ids[iov]
-                add_pcidevice(devices, pci_id, params=nic_params)
+                add_pcidevice(devices, pci_id, params=nic_params,
+                              device_driver=device_driver)
                 iov += 1
 
         mem = params.get("mem")
@@ -1785,6 +1787,8 @@ class VM(virt_vm.BaseVM):
                 nic_params = params.object_params(nic.nic_name)
                 pa_type = nic_params.get("pci_assignable")
                 if pa_type and pa_type != "no":
+                    device_driver = nic_params.get("device_driver",
+                                                    "pci-assign")
                     if mac not in nic:
                         self.virtnet.generate_mac_address(nic["nic_name"])
                     mac = nic["mac"]
@@ -1795,7 +1799,8 @@ class VM(virt_vm.BaseVM):
                             host_set_flag=params.get("host_setup_flag"),
                             kvm_params=params.get("kvm_default"),
                             vf_filter_re=params.get("vf_filter_re"),
-                            pf_filter_re=params.get("pf_filter_re"))
+                            pf_filter_re=params.get("pf_filter_re"),
+                            device_driver=device_driver)
                     # Virtual Functions (VF) assignable devices
                     if pa_type == "vf":
                         self.pci_assignable.add_device(device_type=pa_type,
