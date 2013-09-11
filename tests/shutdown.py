3908640d9794e13bd40af7bbe6d671d5695e514a
Feng Yang <fyang@redhat.com>
2013-09-11 15:19:46 +0800

    tests.shutdown: Support shutdown guest for multi times.
    Signed-off-by: Feng Yang <fyang@redhat.com>

--- tests/shutdown.py
+++ tests/shutdown.py
@@ -1,7 +1,7 @@
 import time
 import logging
 from autotest.client.shared import error
-from virttest import utils_misc
+from virttest import utils_misc, env_process
 
 
 @error.context_aware
@@ -17,28 +17,33 @@ def run_shutdown(test, params, env):
     :param params: Dictionary with the test parameters
     :param env: Dictionary with test environment
     """
-    vm = env.get_vm(params["main_vm"])
-    vm.verify_alive()
     timeout = int(params.get("login_timeout", 360))
-    session = vm.wait_for_login(timeout=timeout)
+    shutdown_count = int(params.get("shutdown_count", 1))
+    shutdown_method = params.get("shutdown_method", "shell")
+    sleep_time = float(params.get("sleep_before_powerdown", 10))
+    shutdown_command = params.get("shutdown_command")
 
-    try:
-        error.base_context("shutting down the VM", logging.info)
-        if params.get("shutdown_method") == "shell":
+    for i in xrange(shutdown_count):
+        vm = env.get_vm(params["main_vm"])
+        vm.verify_alive()
+        session = vm.wait_for_login(timeout=timeout)
+        error.base_context("shutting down the VM %s/%s" % (i + 1,
+                                                           shutdown_count),
+                                                           logging.info)
+        if shutdown_method == "shell":
             # Send a shutdown command to the guest's shell
-            session.sendline(vm.get_params().get("shutdown_command"))
+            session.sendline(shutdown_command)
             error.context("waiting VM to go down (shutdown shell cmd)",
                           logging.info)
-        elif params.get("shutdown_method") == "system_powerdown":
+        elif shutdown_method == "system_powerdown":
             # Sleep for a while -- give the guest a chance to finish booting
-            time.sleep(float(params.get("sleep_before_powerdown", 10)))
+            time.sleep(sleep_time)
             # Send a system_powerdown monitor command
             vm.monitor.cmd("system_powerdown")
             error.context("waiting VM to go down "
                           "(system_powerdown monitor cmd)", logging.info)
 
-        if not utils_misc.wait_for(vm.is_dead, 240, 0, 1):
+        if not utils_misc.wait_for(vm.is_dead, 360, 0, 1):
             raise error.TestFail("Guest refuses to go down")
-
-    finally:
-        session.close()
+        if i < shutdown_count - 1:
+            env_process.preprocess_vm(test, params, env, params["main_vm"])
