f05f2f65285ec3ff3e4c72a10070011a75859e14
Yanbing Du <ydu@redhat.com>
2013-09-11 16:17:26 +0800

    Add test for virsh autostart
    Signed-off-by: Yanbing Du <ydu@redhat.com>

--- virttest/virsh.py
+++ virttest/virsh.py
@@ -2359,3 +2359,15 @@ def domiftune(name, interface, options=None, inbound=None,
         cmd += " --%s" % options
 
     return command(cmd, **dargs)
+
+
+def autostart(name, options, **dargs):
+    """
+    Autostart a domain
+
+    @return: cmdresult object.
+    """
+    cmd = ("autostart %s %s" % (name, options))
+    CmdResult = command(cmd, **dargs)
+
+    return CmdResult
