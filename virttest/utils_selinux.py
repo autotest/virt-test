b0b698e52328c015b22eaaefe39993660778b305
Chris Evich <cevich@redhat.com>
2013-09-18 12:49:29 -0400

    virt: Enhance utils_selinux
    * Made MLS range patern matching optional in get_context_from_str(context)
      since this information isn't always provided/available.
    
    * Added function to extract only the context type from a context string
      since many selinux commands only work with a type anyway
    
    * Added get/set/del functions to manipulate default file contexts on system
    
    * Added search function to look up a type based on a path
    
    * Added unittests for new functions
    
    * Added special exception if semanage tool doesn't exist
    
    * Added functions and unittests for getting/verifying/setting
      default contexts on specified path.
    
    * Added function to apply default contexts onto file/dir
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- virttest/utils_selinux.py
+++ virttest/utils_selinux.py
@@ -2,10 +2,8 @@
 selinux test utility functions.
 """
 
-import logging
-import re
+import logging, re, os.path
 from autotest.client import utils
-
 
 class SelinuxError(Exception):
 
@@ -13,7 +11,6 @@ class SelinuxError(Exception):
     Error selinux utility functions.
     """
     pass
-
 
 class SeCmdError(SelinuxError):
 
@@ -30,9 +27,23 @@ class SeCmdError(SelinuxError):
         return str("Execute command %s failed.\n"
                    "Detail: %s .\n" % (self.cmd, self.detail))
 
+class SemanageError(SelinuxError):
+    """
+    Error when semanage binary is not found
+    """
+
+    def __str__(self):
+        return ("The semanage command is not available, "
+                "please install policycoreutils "
+                "or equivilent for your platform.")
+
+class RestoreconError(SelinuxError):
+
+    def __str__(self):
+        return ("Output from the restorecon command"
+                "does not match the expected format")
 
 STATUS_LIST = ['enforcing', 'permissive', 'disabled']
-
 
 def get_status():
     """
@@ -56,7 +67,6 @@ def get_status():
 
     raise SelinuxError("result of 'getenforce' (%s)is not expected."
                        % result.stdout)
-
 
 def set_status(status):
     """
@@ -95,7 +105,6 @@ def set_status(status):
 
     logging.debug("Set status of selinux to %s success.", status)
 
-
 def is_disabled():
     """
     Return True if the selinux is disabled.
@@ -106,27 +115,36 @@ def is_disabled():
     else:
         return False
 
-
 def is_not_disabled():
     """
     Return True if the selinux is not disabled.
     """
     return not is_disabled()
 
-
-def get_context_from_str(string):
+def get_context_from_str(context):
     """
-    Get the context in a string.
+    Get the context in a context.
 
-    :raise SelinuxError: if there is no context in string.
+    :raise SelinuxError: if there is no context in context.
     """
-    context_pattern = r"[a-z,_]*_u:[a-z,_]*_r:[a-z,_]*_t:[s,\-,0-9,:[c,\,,0-9]*]*"
-    if re.search(context_pattern, string):
-        context_list = re.findall(context_pattern, string)
+    context_pattern = (r"[a-z,_]*_u:[a-z,_]*_r:[a-z,_]*_t"
+                       # non-greedy/non-group match on optional MLS range
+                       r"(?:\:[s,\-,0-9,:[c,\,,0-9]*]*)?")
+    if re.search(context_pattern, context):
+        context_list = re.findall(context_pattern, context)
         return context_list[0]
 
-    raise SelinuxError("There is no context in %s." % string)
+    raise SelinuxError("There is no context in %s." % context)
 
+def get_type_from_context(context):
+    """
+    Return just the type component of a full context string
+    """
+    # Raise exception if not a context string
+    get_context_from_str(context)
+    type_pattern = (r"[a-z,_]*_u:[a-z,_]*_r:([a-z,_]*_t)"
+                    r"(?:\:[s,\-,0-9,:[c,\,,0-9]*]*)?")
+    return re.search(type_pattern, context).group(1)
 
 def get_context_of_file(filename):
     """
@@ -141,7 +159,6 @@ def get_context_of_file(filename):
 
     output = result.stdout
     return get_context_from_str(output)
-
 
 def set_context_of_file(filename, context):
     """
@@ -167,7 +184,6 @@ def set_context_of_file(filename, context):
 
     logging.debug("Set context of %s success.", filename)
 
-
 def get_context_of_process(pid):
     """
     Get context of process.
@@ -178,3 +194,209 @@ def get_context_of_process(pid):
 
     output = attr_file.read()
     return get_context_from_str(output)
+
+# Force uniform handling if semanage not found (used in unittests)
+def _no_semanage(cmdresult):
+    if cmdresult.exit_status == 127:
+        if cmdresult.stdout.lower().count('command not found'):
+            raise SemanageError()
+
+def get_defcon(local=False):
+    """
+    Return list of dictionaries containing SELinux default file context types
+
+    :param local: Only return locally modified default contexts
+    :return: list of dictionaries of default context attributes
+    """
+    if local:
+        result = utils.run("semanage fcontext --list -C", ignore_status=True)
+    else:
+        result = utils.run("semanage fcontext --list", ignore_status=True)
+    _no_semanage(result)
+    if result.exit_status != 0:
+        raise SeCmdError(cmd, result.stderr)
+    result_list = result.stdout.strip().split('\n')
+    # Need to process top-down instead of bottom-up
+    result_list.reverse()
+    first_line = result_list.pop()
+    # First column name has a space in it
+    column_names = [name.strip().lower().replace(' ', '_')
+                    for name in first_line.split('  ')
+                    if len(name) > 0]
+    # Shorten first column name
+    column_names[0] = column_names[0].replace("selinux_", "")
+    fcontexts = []
+    for line in result_list:
+        if len(line) < 1: # skip blank lines
+            continue
+        column_data = [name.strip()
+                       for name in line.split('  ')
+                       if len(name) > 0]
+        # Enumerating data raises exception if no column_names match
+        fcontext = dict([(column_names[idx], data)
+                         for idx, data in enumerate(column_data)])
+        # find/set functions only accept type, not full context string
+        fcontext['context'] = get_type_from_context(fcontext['context'])
+        fcontexts.append(fcontext)
+    return fcontexts
+
+def find_defcon_idx(defcon, pathname):
+    """
+    Returns the index into defcon where pathname matches or None
+    """
+    # Default context path regexes only work on cannonical paths
+    pathname = os.path.realpath(pathname)
+    for default_context in defcon:
+        if bool(re.search(default_context['fcontext'], pathname)):
+            return defcon.index(default_context)
+    return None
+
+def find_defcon(defcon, pathname):
+    """
+    Returns the context type of first match to pathname or None
+    """
+    # Default context path regexes only work on cannonical paths
+    pathname = os.path.realpath(pathname)
+    idx = find_defcon_idx(defcon, pathname)
+    if idx is not None:
+        return get_type_from_context(defcon[idx]['context'])
+    else:
+        return None
+
+def find_pathregex(defcon, pathname):
+    """
+    Returns the regular expression in defcon matching pathname
+    """
+    # Default context path regexes only work on cannonical paths
+    pathname = os.path.realpath(pathname)
+    idx = find_defcon_idx(defcon, pathname)
+    if idx is not None:
+        return defcon[idx]['fcontext']
+    else:
+        return None
+
+<<<<<<< HEAD
+def set_defcon(context, pathregex):
+=======
+
+def set_defcon(context_type, pathregex):
+>>>>>>> 5590f6a... fixup! virt: Enhance utils_selinux
+    """
+    Set the default context of a file/path in local SELinux policy
+
+    :param context_type: The selinux context (only type is used)
+    :param pathregex: Pathname regex e.g. r"/foo/bar/baz(/.*)?"
+    :raise SelinuxError: if semanage command not found
+    :raise SeCmdError: if semanage exits non-zero
+    """
+    cmd = ("semanage fcontext --add -t %s '%s'" % (context_type, pathregex))
+    result = utils.run(cmd, ignore_status=True)
+    _no_semanage(result)
+    if result.exit_status != 0:
+        raise SeCmdError(cmd, result.stderr)
+
+
+def del_defcon(context_type, pathregex):
+    """
+    Remove the default local SELinux policy type for a file/path
+
+    :param context: The selinux context (only type is used)
+    :pramm pathregex: Pathname regex e.g. r"/foo/bar/baz(/.*)?"
+    :raise SelinuxError: if semanage command not found
+    :raise SeCmdError: if semanage exits non-zero
+    """
+    cmd = ("semanage fcontext --delete -t %s '%s'" % (context_type, pathregex))
+    result = utils.run(cmd, ignore_status=True)
+    _no_semanage(result)
+    if result.exit_status != 0:
+        raise SeCmdError(cmd, result.stderr)
+
+# Process pathname/dirdesc in uniform way for all defcon functions + unittests
+def _run_restorecon(pathname, dirdesc, readonly=True):
+    cmd = 'restorecon -v'
+    if dirdesc:
+        cmd += 'R'
+    if readonly:
+        cmd += 'n'
+    cmd += ' "%s"' % pathname
+    # Always returns 0, even if contexts wrong
+    return utils.run(cmd).stdout.strip()
+
+def verify_defcon(pathname, dirdesc=False):
+    """
+    Verify contexts of pathspec (and/or below, if dirdesc) match default
+
+    :param pathname: Absolute path to file, directory, or symlink
+    :param dirdesc: True to descend into sub-directories
+    :return: True if all components match default contexts
+    :note: By default DOES NOT follow symlinks
+    """
+    # Default context path regexes only work on cannonical paths
+    changes = _run_restorecon(pathname, dirdesc)
+    if changes.count('restorecon reset'):
+        return False
+    else:
+        return True
+
+# Provide uniform formatting for diff and apply functions
+
+def _format_changes(changes):
+    result = []
+    if changes: # Empty string or None - return empty list
+        # Could be many changes, need efficient line searching
+        regex = re.compile('^restorecon reset (.+) context (.+)->(.+)')
+        for change_line in changes.split('\n'):
+            mobj = regex.search(change_line)
+            if mobj is None:
+                raise RestoreconError()
+            pathname = mobj.group(1)
+            from_con = mobj.group(2)
+            to_con = mobj.group(3)
+            result.append( (pathname, from_con, to_con) )
+    return result
+
+def diff_defcon(pathname, dirdesc=False):
+    """
+    Return a list of tuple(pathname, from, to) for current & default contexts
+
+    :param pathname: Absolute path to file, directory, or symlink
+    :param dirdesc: True to descend into sub-directories
+    :return: List of tuple(pathname, from context, to context)
+    """
+    return _format_changes(_run_restorecon(pathname, dirdesc))
+
+def apply_defcon(pathname, dirdesc=False):
+    """
+    Apply default contexts to pathname, possibly descending into sub-dirs also.
+
+    :param pathname: Absolute path to file, directory, or symlink
+    :param dirdesc: True to descend into sub-directories
+    :return: List of changes applied tuple(pathname, from context, to context)
+    """
+    return _format_changes(_run_restorecon(pathname, dirdesc, readonly=False))
+
+
+def transmogrify_usr_local(pathregex):
+    """
+    Replace usr/local/something with usr/(local/)?something
+    """
+    # Whoa! don't mess with short path regex's
+    if len(pathregex) < 3:
+        return pathregex
+    if pathregex.count('usr/local'):
+        pathregex = pathregex.replace('usr/local/', r'usr/(local/)?')
+    return pathregex
+
+
+def transmogrify_sub_dirs(pathregex):
+    """
+    Append '(/.*)?' regex to end of pathregex to optionally match all subdirs
+    """
+    # Whoa! don't mess with short path regex's
+    if len(pathregex) < 3:
+        return pathregex
+    # Doesn't work with path having trailing slash
+    if pathregex.endswith('/'):
+        pathregex = pathregex[0:-1]
+    return pathregex + r'(/.*)?'
+
