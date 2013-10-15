6fa20ffb18177132da053a4369ed59abac933124
Yiqiao Pu <ypu@redhat.com>
2013-10-15 18:09:40 +0800

    virttest: Record the kernel crash message in debug file
    Record the kernel crash message in debug file instead of putting it
    in the __str__(). With this change the crash call trace will only shows
    in debug log file but not in status.
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

--- virttest/virt_vm.py
+++ virttest/virt_vm.py
@@ -147,10 +147,11 @@ class VMDeadKernelCrashError(VMError):
 
     def __init__(self, kernel_crash):
         VMError.__init__(self, kernel_crash)
-        self.kernel_crash = kernel_crash
+        logging.debug(kernel_crash)
 
     def __str__(self):
-        return ("VM is dead due to a kernel crash:\n%s" % self.kernel_crash)
+        return ("VM is dead due to a kernel crash, "
+                "see debug/serial log for details")
 
 
 class VMInvalidInstructionCode(VMError):
