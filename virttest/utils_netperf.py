affbaa97369981b97c77b3f98e6bc424da4b0351
Yunping Zheng <yunzheng@redhat.com>
2013-10-25 15:41:36 +0800

    qemu.test: add netperf stress test
    This patch add netperf stress test for nic test
    
    Signed-off-by: Yunping Zheng <yunzheng@redhat.com>

--- virttest/utils_netperf.py
+++ virttest/utils_netperf.py
@@ -225,7 +225,8 @@ class NetperfClient(NetperfPackage):
         else:
             self.client_dir = self.remote_path
 
-    def start(self, server_address, test_option="", timeout=1200):
+    def start(self, server_address, test_option="", timeout=1200,
+              cmd_prefix=""):
         """
         Run netperf test
 
@@ -234,8 +235,8 @@ class NetperfClient(NetperfPackage):
         :param timeout: Netperf test timeout(-l)
         :return: return test result
         """
-        netperf_cmd = "%s -H %s %s " % (self.client_dir, server_address,
-                                        test_option)
+        netperf_cmd = "%s %s -H %s %s " % (cmd_prefix, self.client_dir,
+                                           server_address, test_option)
         logging.debug("Start netperf with cmd: '%s'" % netperf_cmd)
         (status, output) = self.session.cmd_status_output(netperf_cmd,
                                                           timeout=timeout)
@@ -244,7 +245,8 @@ class NetperfClient(NetperfPackage):
         self.result = output
         return self.result
 
-    def bg_start(self, server_address, test_option="", session_num=1):
+    def bg_start(self, server_address, test_option="", session_num=1,
+                 cmd_prefix=""):
         """
         Run netperf background, for stress test, Only support linux now
         Have no output
@@ -256,8 +258,8 @@ class NetperfClient(NetperfPackage):
         if self.client == "nc":
             raise NetperfTestError("Currently only support linux client")
 
-        netperf_cmd = "%s -H %s %s " % (self.client_dir, server_address,
-                                        test_option)
+        netperf_cmd = "%s %s -H %s %s " % (cmd_prefix, self.client_dir,
+                                           server_address, test_option)
         logging.debug("Start %s sessions netperf background with cmd: '%s'" %
                       (session_num, netperf_cmd))
         for _ in xrange(int(session_num)):
