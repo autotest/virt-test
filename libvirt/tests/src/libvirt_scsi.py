83d96a05d5953109cbf4602ad8f0f89d2bc0137d
Dongsheng Yang <yangds.fnst@cn.fujitsu.com>
2013-11-07 10:26:02 -0500

    libvirt-tests: use new Disk class in libvirt xml.
    Signed-off-by: Dongsheng Yang <yangds.fnst@cn.fujitsu.com>

--- libvirt/tests/src/libvirt_scsi.py
+++ libvirt/tests/src/libvirt_scsi.py
@@ -11,12 +11,12 @@ from virttest import virt_vm, qemu_storage
 from virttest.libvirt_xml.vm_xml import VMXML
 from virttest.libvirt_xml.devices.disk import Disk
 from virttest.libvirt_xml.devices.controller import Controller
+from virttest import data_dir
 
 
 def run_libvirt_scsi(test, params, env):
     #Get variables.
     status_error = ('yes' == params.get("status_error", 'no'))
-    add_controller = ('yes' == params.get("libvirt_scsi_controller", 'yes'))
     img_type = ('yes' == params.get("libvirt_scsi_img_type", "no"))
     cdrom_type = ('yes' == params.get("libvirt_scsi_cdrom_type", "no"))
     partition_type = ('yes' == params.get("libvirt_scsi_partition_type", "no"))
@@ -43,41 +43,41 @@ def run_libvirt_scsi(test, params, env):
         vmxml.add_device(scsi_controller)
     # Add disk with bus of scsi into vmxml.
     if img_type:
-        img_disk = Disk("disk")
-        img_disk.type = "file"
-        img_disk.device = "disk"
-        img_disk.target_dev = "vde"
-        img_disk.target_bus = "scsi"
         # Init a QemuImg instance.
         img_name = "libvirt_scsi"
         params['image_name'] = img_name
-        image = qemu_storage.QemuImg(params, test.tmpdir, img_name)
+        image = qemu_storage.QemuImg(params, data_dir.get_tmp_dir(), img_name)
         # Create a image.
-        img_path, result = image.create(params)
-        img_disk.source = img_path
+        img_path, _ = image.create(params)
+        img_disk = Disk(type_name="file")
+        img_disk.device = "disk"
+        img_disk.source = img_disk.new_disk_source(
+            **{'attrs': {'file': img_path}})
+        img_disk.target = {'dev': "vde",
+                           'bus': "scsi"}
         vmxml.add_device(img_disk)
     if cdrom_type:
-        cdrom_disk = Disk("disk")
-        cdrom_disk.type = "file"
-        cdrom_disk.device = "cdrom"
-        cdrom_disk.target_dev = "vdf"
-        cdrom_disk.target_bus = "scsi"
         # Init a CdromDisk instance.
-        cdrom_path = os.path.join(test.tmpdir, "libvirt_scsi")
-        cdrom = CdromDisk(cdrom_path, test.tmpdir)
+        cdrom_path = os.path.join(data_dir.get_tmp_dir(), "libvirt_scsi")
+        cdrom = CdromDisk(cdrom_path, data_dir.get_tmp_dir())
         cdrom.close()
-        cdrom_disk.source = cdrom_path
+        cdrom_disk = Disk(type_name="file")
+        cdrom_disk.device = "cdrom"
+        cdrom_disk.target = {'dev': "vdf",
+                             'bus': "scsi"}
+        cdrom_disk.source = cdrom_disk.new_disk_source(
+            **{'attrs': {'file': cdrom_path}})
         vmxml.add_device(cdrom_disk)
     if partition_type:
         if partition.count("ENTER.YOUR"):
             raise error.TestNAError("Partition for partition test"
                                     "is not configured.")
-        partition_disk = Disk("disk")
-        partition_disk.type = "block"
+        partition_disk = Disk(type_name="block")
         partition_disk.device = "disk"
-        partition_disk.target_dev = "vdg"
-        partition_disk.target_bus = "scsi"
-        partition_disk.source = partition
+        partition_disk.target = {'dev': "vdg",
+                                 'bus': "scsi"}
+        partition_disk.source = partition_disk.new_disk_source(
+            **{'attrs': {'dev': partition}})
         vmxml.add_device(partition_disk)
     # sync the vmxml with VM.
     vmxml.sync()
