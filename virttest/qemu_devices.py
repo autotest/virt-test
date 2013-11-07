e0dd0fb8d572973d67c3cb84ce9f67ab5287e3d7
Lukáš Doktor <ldoktor@redhat.com>
2013-11-07 13:08:12 +0100

    virttest.qemu_devices: Fix cdrom filename
    CDROM filenames doesn't use suffixes. Use image_raw_device to
    get the correct image name.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- virttest/qemu_devices.py
+++ virttest/qemu_devices.py
@@ -2957,6 +2957,7 @@ class DevContainer(object):
             image_params['image_name'] = os.path.join(data_dir.get_data_dir(),
                                                       image_params.get('cdrom')
                                                       )
+            image_params['image_raw_device'] = 'yes'
         cd_format = image_params.get('cd_format')
         if cd_format is None or cd_format is 'ide':
             if not self.get_buses({'atype': 'ide'}):
