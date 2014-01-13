9b2317cbf2e3c9cc8596da7bd1a5ed4e0d607709
Amos Kong <akong@redhat.com>
2014-01-13 14:10:49 +0800

    netperf: count interrupt for each queue
    When we enable multiple queue, there are multiple virtio
    input/output interrupts in guest.
    
    This patch counts interrupt for each queue, and output
    a summary in result.
    
    Signed-off-by: Amos Kong <akong@redhat.com>

--- generic/tests/netperf.py
+++ generic/tests/netperf.py
@@ -263,8 +263,14 @@ def start_test(server, server_ctl, host, clients, resultsdir, l=60,
 
     record_list = ['size', 'sessions', 'throughput', 'trans.rate', 'CPU',
                    'thr_per_CPU', 'rx_pkts', 'tx_pkts', 'rx_byts', 'tx_byts',
-                   're_pkts', 'rx_intr', 'tx_intr', 'io_exit', 'irq_inj',
-                   'tpkt_per_exit', 'rpkt_per_irq']
+                   're_pkts']
+    for i in range(int(params.get("queues", 0))):
+        record_list.append('rx_intr_%s' % i)
+    record_list.append('rx_intr_sum')
+    for i in range(int(params.get("queues", 0))):
+        record_list.append('tx_intr_%s' % i)
+    record_list.append('tx_intr_sum')
+    record_list += ['io_exit', 'irq_inj', 'tpkt_per_exit', 'rpkt_per_irq']
     base = params.get("format_base", "12")
     fbase = params.get("format_fbase", "2")
 
@@ -417,13 +423,18 @@ def launch_client(sessions, server, server_ctl, host, clients, l, nf_args,
 
     def count_interrupt(name):
         """
-        :param name: the name of interrupt, such as "virtio0-input"
+        Get a list of interrut number for each queue
+
+        @param name: the name of interrupt, such as "virtio0-input"
         """
-        intr = 0
+        sum = 0
+        intr = []
         stat = ssh_cmd(server_ctl, "cat /proc/interrupts |grep %s" % name)
-        stat = stat.strip().split("\n")[-1]
-        for cpu in range(int(ncpu)):
-            intr += int(stat.split()[cpu + 1])
+        for i in stat.strip().split("\n"):
+            for cpu in range(int(ncpu)):
+                sum += int(i.split()[cpu + 1])
+            intr.append(sum)
+            sum = 0
         return intr
 
     def get_state():
@@ -449,10 +460,22 @@ def launch_client(sessions, server, server_ctl, host, clients, l, nf_args,
         try:
             nrx_intr = count_interrupt("virtio.-input")
             ntx_intr = count_interrupt("virtio.-output")
-            state_list.append('rx_intr')
-            state_list.append(nrx_intr)
-            state_list.append('tx_intr')
-            state_list.append(ntx_intr)
+            sum = 0
+            for i in range(len(nrx_intr)):
+                state_list.append('rx_intr_%s' % i)
+                state_list.append(nrx_intr[i])
+                sum += nrx_intr[i]
+            state_list.append('rx_intr_sum')
+            state_list.append(sum)
+
+            sum = 0
+            for i in range(len(ntx_intr)):
+                state_list.append('tx_intr_%s' % i)
+                state_list.append(ntx_intr[i])
+                sum += ntx_intr[i]
+            state_list.append('tx_intr_sum')
+            state_list.append(sum)
+
         except IndexError:
             ninit = count_interrupt("virtio.")
             state_list.append('intr')
