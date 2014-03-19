98b0c427ea70ad2f6da5104af3f3db591123084e
Li Yang <liyang.fnst@cn.fujitsu.com>
2014-03-19 13:58:12 -0300

    virttest.utils_test.libvirt: Add a 'session' param to mk_part()
    So we can use the same function to create a partition
    on remote systems.
    
    Signed-off-by: Li Yang <liyang.fnst@cn.fujitsu.com>

--- virttest/utils_test/libvirt.py
+++ virttest/utils_test/libvirt.py
@@ -488,14 +488,18 @@ def pci_label_from_address(address_dict, radix=10):
     return pci_label
 
 
-def mk_part(disk, size="100M"):
+def mk_part(disk, size="100M", session=None):
     """
     Create a partition for disk
     """
-    cmd = "parted -s %s mklabel msdos" % disk
-    utils.run(cmd)
-    cmd = "parted -s %s mkpart primary ext4 0 %s" % (disk, size)
-    utils.run(cmd)
+    mklabel_cmd = "parted -s %s mklabel msdos" % disk
+    mkpart_cmd = "parted -s %s mkpart primary ext4 0 %s" % (disk, size)
+    if session:
+        session.cmd(mklabel_cmd)
+        session.cmd(mkpart_cmd)
+    else:
+        utils.run(mklabel_cmd)
+        utils.run(mkpart_cmd)
 
 
 def check_actived_pool(pool_name):
