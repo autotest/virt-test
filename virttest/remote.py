74ed95eb940c6feadaa47a422ad93665533cc59d
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-03-26 12:29:07 -0300

    Revert "Merge pull request #1544 from kongove/disable-ssh-debug"
    This reverts commit da68c016be1d0f27d7d269949b8b4072a8cda471, reversing
    changes made to a9cce5c525271af65e0ce6354c70a8983417a6e6.

--- virttest/remote.py
+++ virttest/remote.py
@@ -216,7 +216,6 @@ def remote_login(client, host, port, username, password, prompt, linesep="\n",
         host = "%s%%%s" % (host, interface)
     if client == "ssh":
         cmd = ("ssh -o UserKnownHostsFile=/dev/null "
-               "-E /dev/null "
                "-o StrictHostKeyChecking=no "
                "-o PreferredAuthentications=password -vvv -p %s %s@%s" %
                (port, username, host))
