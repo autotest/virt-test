ec92fff112eeb47e7cb12dc08333c3b255725288
Yu Mingfei <yumingfei@cn.fujitsu.com>
2014-07-31 14:57:18 +0800

    libvirt_xml.devices: Add slots for channel.
    Signed-off-by: Yu Mingfei <yumingfei@cn.fujitsu.com>

--- virttest/libvirt_xml/devices/channel.py
+++ virttest/libvirt_xml/devices/channel.py
@@ -5,14 +5,19 @@ http://libvirt.org/formatdomain.html#elementCharSerial
 """
 
 from virttest.libvirt_xml import base
+from virttest.libvirt_xml import accessors
 from virttest.libvirt_xml.devices.character import CharacterBase
 
 
 class Channel(CharacterBase):
 
-    __slots__ = []
+    __slots__ = ('source', 'target',)
 
     def __init__(self, type_name='unix', virsh_instance=base.virsh):
+        accessors.XMLElementDict('source', self, parent_xpath='/',
+                                 tag_name='source')
+        accessors.XMLElementDict('target', self, parent_xpath='/',
+                                 tag_name='target')
         super(
             Channel, self).__init__(device_tag='channel', type_name=type_name,
                                     virsh_instance=virsh_instance)
