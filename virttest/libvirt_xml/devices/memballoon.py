404d40b92d826e8ec826aae2d732ba201687247b
Yanbing Du <ydu@redhat.com>
2014-10-15 19:36:28 +0800

    libvirt_xml: Implement memballoon xml
    Signed-off-by: Yanbing Du <ydu@redhat.com>

--- virttest/libvirt_xml/devices/memballoon.py
+++ virttest/libvirt_xml/devices/memballoon.py
@@ -4,10 +4,18 @@ memballoon device support class(es)
 http://libvirt.org/formatdomain.html#elementsMemBalloon
 """
 
+from virttest.libvirt_xml import accessors
 from virttest.libvirt_xml.devices import base
 
 
 class Memballoon(base.UntypedDeviceBase):
-    # TODO: Write this class
-    __metaclass__ = base.StubDeviceMeta
-    _device_tag = 'memballoon'
+
+    __slots__ = ('model', 'stats_period')
+
+    def __init__(self, virsh_instance=base.base.virsh):
+        accessors.XMLAttribute('model', self, parent_xpath='/',
+                               tag_name='memballoon', attribute='model')
+        accessors.XMLAttribute('stats_period', self, parent_xpath='/',
+                               tag_name='stats', attribute='period')
+        super(Memballoon, self).__init__(device_tag='memballoon',
+                                         virsh_instance=virsh_instance)
