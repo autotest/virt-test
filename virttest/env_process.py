0699d5559b353bc6a6b18241149fc0ab0e16fbef
Feng Yang <fyang@redhat.com>
2014-11-17 15:48:24 +0800

    virt: Make VM create timeout configurable.
    Now we use fix CREATE_TIME = 20s.
    But some time this timeout is too short. So
    make it configurable.
    
    Signed-off-by: Feng Yang <fyang@redhat.com>

--- virttest/env_process.py
+++ virttest/env_process.py
@@ -163,11 +163,13 @@ def preprocess_vm(test, params, env, name):
             # Start the VM (or restart it if it's already up)
             if params.get("reuse_previous_config", "no") == "no":
                 vm.create(name, params, test.bindir,
+                          timeout=int(params.get("vm_create_timeout", 20)),
                           migration_mode=params.get("migration_mode"),
                           migration_fd=params.get("migration_fd"),
                           migration_exec_cmd=params.get("migration_exec_cmd_dst"))
             else:
-                vm.create(migration_mode=params.get("migration_mode"),
+                vm.create(timeout=int(params.get("vm_create_timeout", 20)),
+                          migration_mode=params.get("migration_mode"),
                           migration_fd=params.get("migration_fd"),
                           migration_exec_cmd=params.get("migration_exec_cmd_dst"))
     elif not vm.is_alive():    # VM is dead and won't be started, update params
