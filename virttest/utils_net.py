autotest/virt-test

commit 6e6e88de12f05234a5ea548be36c23ba2ad55ee9

Fix verify dhcp lease failed issue
1.) make timeout work for verify_ip_address_ownership
2.) update address_cache only catched 'DHCPACK' packet

Signed-off-by: Xu Tian <xutian@redhat.com>

virttest/utils_net.py
@@ -2622,7 +2622,7 @@ def parse_arp():
     return ret
 
 
-def verify_ip_address_ownership(ip, macs, timeout=10.0):
+def verify_ip_address_ownership(ip, macs, timeout=60.0):
     """
     Use arping and the ARP cache to make sure a given IP address belongs to one
     of the given MAC addresses.
@@ -2631,28 +2631,35 @@ def verify_ip_address_ownership(ip, macs, timeout=10.0):
     :param macs: A list or tuple of MAC addresses.
     :return: True if ip is assigned to a MAC address in macs.
     """
-    ip_map = parse_arp()
-    for mac in macs:
-        if ip_map.get(mac) == ip:
-            return True
+    def __arping(regex, arping_cmd, ip):
+        # Compile a regex that matches the given IP address and any of the
+        # given MAC addresses from arping output
+        ip_map = parse_arp()
+        for mac in macs:
+            if ip_map.get(mac) == ip:
+                return True
+        o = commands.getoutput(arping_cmd)
+        if not regex.search(o):
+            logging.debug("Verify arping result failed: %s" % o)
+            return False
+        return True
 
-    # Compile a regex that matches the given IP address and any of the given
-    # MAC addresses
-    mac_regex = "|".join("(%s)" % mac for mac in macs)
-    regex = re.compile(r"\b%s\b.*\b(%s)\b" % (ip, mac_regex), re.IGNORECASE)
-
-    # Get the name of the bridge device for arping
-    o = commands.getoutput("%s route get %s" %
-                           (utils_misc.find_command("ip"), ip))
-    dev = re.findall(r"dev\s+\S+", o, re.IGNORECASE)
+    # Get the name of the bridge device for ip route cache
+    ip_cmd = utils_misc.find_command("ip")
+    ip_cmd = "%s route get %s" % (ip_cmd, ip)
+    o = commands.getoutput(ip_cmd)
+    dev = re.findall(r"dev\s+\S+", o, re.I)
     if not dev:
+        logging.debug("No dev in route table to %s: %s" % (ip, o))
         return False
     dev = dev[0].split()[-1]
-
-    # Send an ARP request
-    o = commands.getoutput("%s -f -c 3 -I %s %s" %
-                           (utils_misc.find_command("arping"), dev, ip))
-    return bool(regex.search(o))
+    mac_regex = "|".join("(%s)" % mac for mac in macs)
+    regex = re.compile(r"\b%s\b.*\b(%s)\b" % (ip, mac_regex), re.I)
+    arping_bin = utils_misc.find_command("arping")
+    arping_cmd = "%s -f -c 3 -I %s %s" % (arping_bin, dev, ip)
+    ret = utils.wait_for(lambda: __arping(regex, arping_cmd, ip),
+                         timeout=timeout)
+    return bool(ret)
 
 
 def generate_mac_address_simple():
@@ -2769,7 +2776,7 @@ def get_linux_ifname(session, mac_address=""):
     def _process_output(cmd, reg_pattern):
         sys_ifname = ["lo", "sit0"]
         try:
-            output = session.cmd(cmd)
+            output = session.cmd_output_safe(cmd)
             ifname_list = re.findall(reg_pattern, output, re.I)
             if not ifname_list:
                 return None

virttest/utils_env.py
@@ -3,8 +3,6 @@ import UserDict
 import os
 import logging
 import re
-import time
-
 import utils_misc
 import virt_vm
 import aexpect
@@ -46,43 +44,54 @@ def lock_safe(function):
 
 @lock_safe
 def _update_address_cache(env, line):
-    if re.search("Your.IP", line, re.IGNORECASE):
-        matches = re.findall(r"\d*\.\d*\.\d*\.\d*", line)
-        if matches:
-            env["address_cache"]["last_seen"] = matches[0]
+    """
+    Update mac <-> ip releation ship dict when we got a dhcpack packet.
+    """
+    address_cache = env["address_cache"]
+    # counter to record how may line searched
+    count = address_cache.get("line_count", 0) + 1
+    address_cache["line_count"] = count
+    # Container to save missed dhcpack packet IP
+    ip_pool = address_cache.get("ip_pool", set())
+    address_cache["ip_pool"] = ip_pool
+    matches = re.search(r"Your.IP\s+(\S+)", line, re.I)
+    if matches:
+        # Counter line num. when match IP
+        address_cache["count_ip"] = count
+        ip = matches.group(1)
+        ip_pool.add(ip)
+        address_cache["ip_pool"] = ip_pool
+        if ip != address_cache.get("last_seen_ip"):
+            address_cache["last_seen_ip"] = ip
+        return
 
-    if re.search("Client.Ethernet.Address", line, re.IGNORECASE):
-        matches = re.findall(r"\w*:\w*:\w*:\w*:\w*:\w*", line)
-        if matches and env["address_cache"].get("last_seen"):
-            mac_address = matches[0].lower()
-            last_time = env["address_cache"].get("time_%s" % mac_address, 0)
-            last_ip = env["address_cache"].get("last_seen")
-            cached_ip = env["address_cache"].get(mac_address)
+    matches = re.search(r"Client.Ethernet.Address\s+(\S+)", line, re.I)
+    if matches:
+        mac = matches.group(1).lower()
+        # Counter line num. when match MAC
+        address_cache["count_mac"] = count
+        if mac != address_cache.get("last_seen_mac"):
+            address_cache["last_seen_mac"] = mac
+        return
 
-            if (time.time() - last_time > 5 or cached_ip != last_ip):
-                logging.debug("(address cache) DHCP lease OK: %s --> %s",
-                              mac_address, env["address_cache"].get("last_seen"))
-
-            env["address_cache"][mac_address] = env["address_cache"].get("last_seen")
-            env["address_cache"]["time_%s" % mac_address] = time.time()
-            del env["address_cache"]["last_seen"]
-        elif matches:
-            env["address_cache"]["last_seen_mac"] = matches[0]
-
-    if re.search("Requested.IP", line, re.IGNORECASE):
-        matches = matches = re.findall(r"\d*\.\d*\.\d*\.\d*", line)
-        if matches and env["address_cache"].get("last_seen_mac"):
-            ip_address = matches[0]
-            mac_address = env["address_cache"].get("last_seen_mac")
-            last_time = env["address_cache"].get("time_%s" % mac_address, 0)
-
-            if time.time() - last_time > 10:
-                logging.debug("(address cache) DHCP lease OK: %s --> %s",
-                              mac_address, ip_address)
-
-            env["address_cache"][mac_address] = ip_address
-            env["address_cache"]["time_%s" % mac_address] = time.time()
-            del env["address_cache"]["last_seen_mac"]
+    if re.search(r"DHCP.Message.*:\s+ACK", line, re.I):
+        if (address_cache.get('last_seen_mac') and
+                address_cache.get('last_seen_ip')):
+            mac = address_cache['last_seen_mac']
+            ip = address_cache['last_seen_ip']
+            # Check is match packet is a dhcpack packet
+            if address_cache["count_mac"] - address_cache["count_ip"] == 3:
+                address_cache[mac] = ip
+                ip_pool.discard(ip)
+                address_cache["ip_pool"] = ip_pool
+                del address_cache["line_count"]
+                del address_cache["count_ip"]
+                del address_cache["count_mac"]
+                del address_cache["last_seen_mac"]
+                del address_cache["last_seen_ip"]
+                logging.info("tcpdump updated address_cache" +
+                             "(%s <-> %s)" % (mac, ip))
+        return
 
     # ipv6 address cache:
     mac_ipv6_reg = r"client-ID.*?([0-9a-fA-F]{12})\).*IA_ADDR (.*) pltime"
@@ -95,6 +104,7 @@ def _update_address_cache(env, line):
             logging.debug("(address cache) DHCPV6 lease OK: %s --> %s",
                           mac_address, request_ip)
             env["address_cache"]["%s_6" % mac_address] = request_ip
+        return
 
     if re.search("dhcp6 (reply|advertise)", line, re.IGNORECASE):
         ipv6_mac_reg = "IA_ADDR (.*) pltime.*client-ID.*?([0-9a-fA-F]{12})\)"
@@ -106,6 +116,7 @@ def _update_address_cache(env, line):
             logging.debug("(address cache) DHCPV6 lease OK: %s --> %s",
                           mac_address, allocate_ip)
             env["address_cache"]["%s_6" % mac_address] = allocate_ip
+        return
 
 
 def _tcpdump_handler(env, filename, line):

virttest/virt_vm.py
@@ -13,6 +13,7 @@ import aexpect
 import traceback
 import ppm_utils
 import data_dir
+import copy
 
 
 class VMError(Exception):
@@ -645,19 +646,22 @@ class BaseVM(object):
         if not nic.has_key('mac'):
             raise VMMACAddressMissingError(index)
         if self.ip_version == "ipv4":
-            # Get the IP address from arp cache, try upper and lower case
-            arp_ip = self.address_cache.get(nic.mac.upper())
+            # Get the IP address from arp cache
+            arp_ip = self.address_cache.get(nic.mac.lower())
             if not arp_ip:
-                arp_ip = self.address_cache.get(nic.mac.lower())
-
-            if not arp_ip and os.geteuid() != 0:
+                arp_ip = self.address_cache.get(nic.mac.upper())
+            if not arp_ip or os.geteuid() != 0:
                 # For non-root, tcpdump won't work for finding IP address,
-                # try arp
+                # or IP missed in address_cache, try to find it from arp table.
+                arping_bin = utils_misc.find_command("arping")
+                # Check IP not got DHCPACK packet to ensure no ip/mac
+                # table missed in address_cache.
+                for ip in copy.deepcopy(self.address_cache.get("ip_pool")):
+                    arping_cmd = "%s -f -c 5 " % arping_bin
+                    arping_cmd += "-I %s %s" % (nic.netdst, ip)
+                    utils.system(arping_cmd, ignore_status=True)
                 ip_map = utils_net.parse_arp()
                 arp_ip = ip_map.get(nic.mac.lower())
-                if arp_ip:
-                    self.address_cache[nic.mac.lower()] = arp_ip
-
             if not arp_ip:
                 raise VMIPAddressMissingError(nic.mac)
 
@@ -683,6 +687,9 @@ class BaseVM(object):
 
             logging.debug('Found/Verified IP %s for VM %s NIC %s',
                           arp_ip, self.name, str(index))
+            # Update address_cache since ip/mac releation ship verify pass
+            self.address_cache[nic.mac.lower()] = arp_ip
+            self.address_cache["ip_pool"].discard(arp_ip)
             return arp_ip
 
         elif self.ip_version == "ipv6":

commit b227e78eac2c91a8973bbb3416d13589cdc8597f

utils_net.py: Fixed wrong module reference (utils -> utils_misc)
There was a call 'utils.wait_for()' added in the commit 6e6e88de which
caused an error as the module 'utils' has no attributes 'wait_for()'.
The function 'wait_for()' is implemented in the module 'utils_misc'.

virttest/utils_net.py
@@ -2665,7 +2665,7 @@ def verify_ip_address_ownership(ip, macs, timeout=60.0):
     regex = re.compile(r"\b%s\b.*\b(%s)\b" % (ip, mac_regex), re.I)
     arping_bin = utils_misc.find_command("arping")
     arping_cmd = "%s -f -c 3 -I %s %s" % (arping_bin, dev, ip)
-    ret = utils.wait_for(lambda: __arping(regex, arping_cmd, ip),
+    ret = utils_misc.wait_for(lambda: __arping(regex, arping_cmd, ip),
                          timeout=timeout)
     return bool(ret)
 