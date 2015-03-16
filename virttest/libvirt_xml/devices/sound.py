b9a8af4fcabc7c60809a1b8fbafcfe8fb2018bcd
Alex Jia <ajia@redhat.com>
2015-03-16 15:27:41 +0800

    virttest.libvirt_xml.devices: update sound XML
    Signed-off-by: Alex Jia <ajia@redhat.com>

--- virttest/libvirt_xml/devices/sound.py
+++ virttest/libvirt_xml/devices/sound.py
@@ -4,10 +4,25 @@ sound device support class(es)
 http://libvirt.org/formatdomain.html#elementsSound
 """
 
+from virttest.libvirt_xml import accessors
 from virttest.libvirt_xml.devices import base
 
 
 class Sound(base.UntypedDeviceBase):
-    # TODO: Write this class
-    __metaclass__ = base.StubDeviceMeta
-    _device_tag = 'sound'
+
+    __slots__ = ('model_type', 'codec_type', 'address')
+
+    def __init__(self, virsh_instance=base.base.virsh):
+        accessors.XMLAttribute('model_type', self,
+                               parent_xpath='/',
+                               tag_name='sound',
+                               attribute='model')
+        accessors.XMLAttribute('codec_type', self,
+                               parent_xpath='/',
+                               tag_name='codec',
+                               attribute='type')
+        accessors.XMLElementDict('address', self,
+                                 parent_xpath='/',
+                                 tag_name='address')
+        super(Sound, self).__init__(device_tag='sound',
+                                    virsh_instance=virsh_instance)
