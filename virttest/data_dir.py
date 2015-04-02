f6b39c8961ef982d6cf8fa752c72fdd318b200a5
Yiqiao Pu <ypu@redhat.com>
2015-04-02 17:59:11 +0800

    virttest: Update data_dir to make it can works with python 2.4
    There is a bug in python 2.4 which make the get_modules of inspect
    raise TypeError for class object ModuleWrapper in versionable_class.
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

--- virttest/data_dir.py
+++ virttest/data_dir.py
@@ -182,10 +182,13 @@ def get_deps_dir(target=None):
     # Get the frame that called this function
     frame = inspect.stack()[1]
     # This is the module that called the function
-    module = inspect.getmodule(frame[0])
     # With the module path, we can keep searching with a parent dir with 'deps'
     # in it, which should be the correct deps directory.
-    path = os.path.dirname(module.__file__)
+    try:
+        module = inspect.getmodule(frame[0])
+        path = os.path.dirname(module.__file__)
+    except TypeError:
+        path = os.path.dirname(frame[1])
     nesting_limit = 10
     for index in xrange(nesting_limit):
         files = os.listdir(path)
