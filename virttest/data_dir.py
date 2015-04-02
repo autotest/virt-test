ff00bbc689aa4854ed6ab92c850c5620023152e8
Yiqiao Pu <ypu@redhat.com>
2015-04-02 19:30:44 +0800

    virttest: Update get_deps_dir to find the deps under shared
    Update the function as the original one can not find the deps dir under
    shared in virt-test repo.
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

--- virttest/data_dir.py
+++ virttest/data_dir.py
@@ -192,6 +192,11 @@ def get_deps_dir(target=None):
     nesting_limit = 10
     for index in xrange(nesting_limit):
         files = os.listdir(path)
+        origin_path = ""
+        if 'shared' in files:
+            origin_path = path
+            path = os.path.join(path, 'shared')
+            files = os.listdir(path)
         if 'deps' in files:
             deps = os.path.join(path, 'deps')
             if target:
@@ -202,6 +207,8 @@ def get_deps_dir(target=None):
         if '.git' in os.listdir(path):
             raise MissingDepsDirError("Could not find specified deps dir for "
                                       "git repo %s" % path)
+        if origin_path:
+            path = origin_path
         path = os.path.dirname(path)
     raise MissingDepsDirError("Could not find specified deps dir after "
                               "looking %s parent directories" %
